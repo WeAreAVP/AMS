afe4db04d543b23bb1e9b77e3f4d0bdcc290502f
Nouman Tayyab <nouman@geekschicago.com>
2012-10-15 17:33:56 +0500

    changes in sphinx

--- application/config/sphinxsearch.php
+++ application/config/sphinxsearch.php
@@ -1,6 +1,7 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['server']          = array('10.17.55.170', 9312);
+//$config['server']          = array('10.17.55.170', 9312);
+$config['server']          = array('localhost', 9312);
 $config['connect_timeout'] = 1000;
 $config['array_result']    = false;
 $config['max_query_time']  = 1000;

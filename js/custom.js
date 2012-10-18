8eb2cebfe306984110ac3477ac69b16023bc59e4
aliraza001 <ali.raza@pureloigcs.net>
2012-10-18 13:28:43 +0500

    remove js error

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+function rand (min, max) {
+  // +   bugfixed by: Onno Marsman
+  // *     example 1: rand(1, 1);
+  // *     returns 1: 1
+  var argc = arguments.length;
+  if (argc === 0) {
+    min = 0;
+    max = 2147483647;
+  } else if (argc === 1) {
+    throw new Error('Warning: rand() expects exactly 2 parameters, 1 given');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+

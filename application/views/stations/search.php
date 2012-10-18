cb288661cc0a290efcbf91ab1777027c03ab0017
aliraza001 <ali.raza@pureloigcs.net>
2012-10-18 14:35:14 +0500

    remove js error

--- application/views/stations/search.php
+++ application/views/stations/search.php
@@ -154,7 +154,7 @@ if(!$is_ajax){?>
 			{
 				var random_id=rand(0,1000365);
 				var search_id=$('#search_keyword').val()+random_id;
-				$('#tokens').append('<div class="btn-img" id="'+search_id+'" ><span class="search_keys">'+$('#search_keyword').val()+'</span><span class="btn-close-img" onlclick="remove_keword(\''+search_id+'\')"></span></div>');
+				$('#tokens').append('<div class="btn-img" id="'+search_id+'" ><span class="search_keys">'+$('#search_keyword').val()+'</span><span class="btn-close-img" onclick="remove_keword(\''+search_id+'\')"></span></div>');
 			}
 			$('#search_keyword').val('');
 		

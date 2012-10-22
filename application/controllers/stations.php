cc448dae98b4c5867c85721b53072151168374d4
Nouman Tayyab <nouman@geekschicago.com>
2012-10-22 14:37:59 +0500

    station issue fixed

--- application/controllers/stations.php
+++ application/controllers/stations.php
@@ -41,6 +41,7 @@ class Stations extends MY_Controller {
         } else {
             $records = $this->sphinx->search_stations($param);
             $data['stations'] = $records['records'];
+            echo '<pre>';print_r($data['stations']);exit;
         }
         if (isAjax()) {
             $data['is_ajax'] = true;

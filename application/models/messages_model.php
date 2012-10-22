127d96c5975bcc0d54daa1ea666be7dbaf690f09
aliraza001 <ali.raza@pureloigcs.net>
2012-10-22 23:04:04 +0500

    Working on messages alert

---
+++ application/models/messages_model.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * station Model.
+ *
+ * @package    AMS
+ * @subpackage email_template_model
+ * @author     Ali Raza
+ */
+class Messages_Model extends CI_Model {
+
+    /**
+     * constructor. set table name amd prefix
+     * 
+     */
+    function __construct() {
+        parent::__construct();
+        $this->_prefix = '';
+        $this->_table = 'messages';
+    }
+
+    /**
+     * Get Messages By Sender Id 
+     * 
+     * @return rows object 
+    */
+		function get_msg_by_sender_id($sender_id)
+		{
+			$this->db->select("*");
+			$this->db->from($this->_table);
+			$this->db->where("sender_id",$sender_id);
+			$res=$this->db->get();
+			if(isset($res) && !empty($res))
+				return $res->result();
+			return false;
+		}
+		 /**
+     * Get Messages By Receiver Id 
+     * 
+     * @return rows object 
+    */
+		function get_msg_by_receiver_id($receiver_id)
+		{
+			$this->db->select("*");
+			$this->db->from($this->_table);
+			$this->db->where("receiver_id",$receiver_id);
+			$res=$this->db->get();
+			if(isset($res) && !empty($res))
+				return $res->result();
+			return false;
+		}
+		
+    /**
+     * Add Email Message
+     * 
+     * @return insert id
+     */
+		function add_msg($data)
+		{
+			$this->db->insert($this->_table,$data);
+			return $this->db->insert_id();
+		}
+		 /**
+     * update Message by id
+     * 
+     * @return bool
+     */
+		function update_msg_by_id($msg_id,$data)
+		{
+			$this->db->where("id",$msg_id);
+			$this->db->update($this->_table,$data);
+			return $this->db->affected_rows() > 0;
+		}
+		 /**
+     * Get Messages for inbox with filter array perm so key will be table field and value will be search text
+     * 
+     * @return rows object 
+    */
+		function get_inbox_msgs($receiver_id,$where='')
+		{
+			
+			$this->db->select("*");
+			$this->db->from($this->_table);
+			$this->db->where("receiver_id",$receiver_id);
+			$this->db->where("receiver_folder","inbox");
+			if(!empty($where))
+			{
+				foreach($where as $key=>$value)
+				{
+					$this->db->where($key,$value);
+				}
+			}
+			$res=$this->db->get();
+			if(isset($res) && !empty($res))
+				return $res->result();
+			return false;
+		}
+		/**
+     * Get Messages for sent with filter array perm so key will be table field and value will be search text
+     * 
+     * @return rows object 
+    */
+		function get_sent_msgs($sender_id)
+		{
+			$this->db->select("*");
+			$this->db->from($this->_table);
+			$this->db->where("sender_id",$sender_id);
+			$this->db->where("sender_folder","sent");
+			if(!empty($where))
+			{
+				foreach($where as $key=>$value)
+				{
+					$this->db->where($key,$value);
+				}
+			}
+			$res=$this->db->get();
+			if(isset($res) && !empty($res))
+				return $res->result();
+			return false;
+		}
+		/**
+     * Get Unread Messages Count
+     * 
+     * @return rows object 
+    */
+		function get_unread_msgs_count()
+		{
+			$this->db->select("COUNT(id) as total");
+			$this->db->from($this->_table);
+			$this->db->where("receiver_id",$receiver_id);
+			$this->db->where("receiver_folder","inbox");
+			$this->db->where("msg_status","unread");
+			$this->db->group_by("receiver_id");
+			$res=$this->db->get();
+			if(isset($res) && !empty($res))
+			{
+				$cnt=$res->row();
+				if($cnt->total>0)
+					return $cnt->total;
+			}
+			return 0;
+		}
+}
---
+++ application/views/messages/inbox.php
@@ -0,0 +1,102 @@
+<?php
+if (!$is_ajax) {
+    $stations = array(
+        'name' => 'stations',
+        'id' => 'stations',
+        'function' => 'onchange="filter_inbox();"',
+    );
+    $message_type = array(
+        'name' => 'message_type',
+        'id' => 'message_type',
+        'function' => 'onchange="filter_inbox();"',
+    );
+		$select[]='Select';
+		foreach($this->config->item('messages_type') as $msg_type)
+		{
+			$select[]=$msg_type;
+		}
+    ?>
+    <br/>
+    <div class="row-fluid">
+			<div class="span3">
+				<a href="javascript:;" class="btn btn-large" >Compose Message</a>
+			</div>
+		</div>
+    <div class="row-fluid">
+        <div class="span3">
+            <div id="search_bar">
+                <b><h4>Folders</h4></b>
+                <div style="padding: 8px;color: white;background: none repeat scroll 0% 0% rgb(0, 152, 214);" ><a href="<?php echo site_url('messages/inbox')?>" >Inbox</a></div>
+                <div style="padding: 8px;" >	<a href="<?php echo site_url('messages/sent')?>" >Sent</a></div>
+                
+                <div>
+                    <?php echo form_label('Stations', $stations['id']); ?>
+                </div>
+                <div>
+                    <?php echo form_dropdown($stations['id'], array('' => 'Select'), array(), $stations['function'] . 'id="' . $stations['id'] . '"'); ?>
+                </div>
+                <div>
+                    <?php echo form_label('Message Type', $message_type['id']); ?>
+                </div>
+                <div>
+                    <?php echo form_dropdown($message_type['id'], $select, array(), $message_type['function'] . 'id="' . $message_type['id'] . '"'); ?>
+                </div>
+            </div>
+        </div>
+       
+        <div  class="span9">
+            <div class="alert" style="margin-bottom: 0px; margin-top: 0px;display: none;" id="success_message"></div>
+            <div style="overflow: scroll;height: 600px;" >
+                <table class="tablesorter table table-bordered" id="station_table">
+                    <thead>
+                        <tr>
+                            <td><span style="float:left;min-width:50px;">From</span></td>
+                            <td><span style="float:left;min-width:80px;">Subject</span></td>
+                            <th><span style="float:left;min-width:90px;">Date</span></td>
+                        </tr>
+                    </thead>
+                    <tbody id="append_record"><?php
+                    }
+                    if (count($results) > 0) {
+                        foreach ($results as $row) {
+                            ?>
+                            <tr>
+                              <td><?php echo $data->sender_id; ?></td>
+                              <td><?php echo $data->subject; ?></td>
+                              <td><?php echo date("Y-m-d",strtotime($data->created_at)); ?></td>
+                           </tr>
+                            <?php
+                        }
+                    } else {
+                        ?>
+                        <tr><td colspan="11" style="text-align: center;"><b>No Message Found.</b></td></tr>
+                    <?php } ?>
+                    <?php if (!$is_ajax) { ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+      <script type="text/javascript">
+       function filter_inbox(){
+           var stations=$('#stations').val();
+           var message_type=$('#message_type').val();
+            $.ajax({
+                type: 'POST', 
+                url: site_url+'messages/inbox',
+                data:{stations:stations,message_type:message_type},
+                dataType: 'json',
+                cache: false,
+                success: function (result) {
+	                 $('#append_record').html(result);
+                }
+            });
+        }
+    </script>
+
+    <?php
+} else {
+    exit();
+    ?>
+    <script type="text/javascript"> $("#station_table").tablesorter();</script>
+<?php } ?>

abec2567ad8d76bee456af1db9a6cccad1e27c2b
aliraza001 <ali.raza@pureloigcs.net>
2012-10-23 19:14:14 +0500

    Read message changes

--- application/views/messages/read_msg.php
+++ application/views/messages/read_msg.php
@@ -8,9 +8,9 @@ if(isset($result) && !empty($result) && isset($result[0]))
        <h3 id="userLabel"><?php echo $row->subject;?></h3>
     </div>
     <div class="modal-body">
-				<div><strong><?php echo date("F d, Y",strtotime($rrow->created_at));?></strong></div>
-        <div><strong>From: <?php $row->full_name;?></strong></div>
-        <div><strong>Subject: <?php $row->subject;?></strong></div>
+				<div><strong><?php echo date("F d, Y",strtotime($row->created_at));?></strong></div>
+        <div><strong>From: <?php echo $row->full_name;?></strong></div>
+        <div><strong>Subject: <?php echo $row->subject;?></strong></div>
         <br/><br/>
         <?php 
 				if(isset($row->msg_extras) && $row->msg_extras!=NULL){

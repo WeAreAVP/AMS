6755664d0940a6d4c9877079fd8a5cd049d1ee0a
Nouman Tayyab <nouman@geekschicago.com>
2012-10-25 12:18:29 +0500

    table freeze

--- application/views/layouts/main_layout.php
+++ application/views/layouts/main_layout.php
@@ -8,7 +8,7 @@
         echo link_js('jquery-ui-1.9.0.custom.js');
         echo link_js('jquery.tablesorter.js');
         echo link_js('bootstrap/bootstrap.js');
-        echo link_js('jquery.freezetablecolumns.1.1');
+        echo link_js('jquery.freezetablecolumns.1.1.js');
 
         echo link_js('custom.js');
         echo link_tag("css/tableSorter.css");

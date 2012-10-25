8e715ffd649b521ed4ccf794909499b7a508754b
Nouman Tayyab <nouman@geekschicago.com>
2012-10-25 12:35:52 +0500

    table freeze

--- application/views/layouts/main_layout.php
+++ application/views/layouts/main_layout.php
@@ -46,7 +46,7 @@
                 $("[rel=tooltip]").tooltip();
                 $("#station_table").tablesorter();
                  $('#station_table').freezeTableColumns({
-            width:       500,   // required
+            width:       400,   // required
             height:      500,   // required
             numFrozen:   2,     // optional
 //            frozenWidth: 150,   // optional

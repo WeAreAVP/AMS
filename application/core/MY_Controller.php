b6952f826e910fda4187e41562d5bc3e027896ed
Nouman Tayyab <nouman@geekschicago.com>
2012-11-01 12:37:46 +0500

    My controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,38 +3,36 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller
-{
+class MY_Controller extends CI_Controller {
 
     /**
      * constructor. Load layout,Model,Library and helpers
      * 
      */
     public $total_unread;
-		public $user_id;
-		public $role_id;
-		public $is_station_user;
-		public $station_id;
-    function __construct()
-		{
-    	parent::__construct();
-      if (!$this->dx_auth->is_logged_in())
-			{
-      	redirect('auth/login');
-      }
-			$this->load->model('messages_model', 'msgs');
+    public $user_id;
+    public $role_id;
+    public $is_station_user;
+    public $station_id;
+
+    function __construct() {
+        parent::__construct();
+        if (!$this->dx_auth->is_logged_in()) {
+            redirect('auth/login');
+        }
+        $this->load->model('messages_model', 'msgs');
     }
-		function _assing_user_info()
-		{
-			$this->user_id = $this->dx_auth->get_user_id();
-			$this->role_id = $this->dx_auth->get_role_id();
-			$this->is_station_user = $this->dx_auth->is_station_user();
-			if($this->is_station_user)
-			{
-				$this->station_id = $this->dx_auth->get_station_id();;
-			}
-			$this->total_unread = $this->msgs->get_unread_msgs_count($this->user_id);
-		}
+
+    function _assing_user_info() {
+        $this->user_id = $this->dx_auth->get_user_id();
+        $this->role_id = $this->dx_auth->get_role_id();
+        $this->is_station_user = $this->dx_auth->is_station_user();
+        if ($this->is_station_user) {
+            $this->station_id = $this->dx_auth->get_station_id();
+            ;
+        }
+        $this->total_unread = $this->msgs->get_unread_msgs_count($this->user_id);
+    }
 
 }
 

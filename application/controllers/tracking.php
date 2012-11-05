224b86cadc30dbea43d27c88b00c505dcfb40421
Nouman Tayyab <nouman@geekschicago.com>
2012-11-05 14:08:18 +0500

    station tracking mail

--- application/controllers/tracking.php
+++ application/controllers/tracking.php
@@ -18,7 +18,7 @@ class Tracking extends MY_Controller {
         error_reporting(E_ALL);
         error_reporting(1);
         $this->layout = 'main_layout.php';
-        $this->load->model('station_model', 'station');
+
         $this->load->model('tracking_model', 'tracking');
     }
 
@@ -49,7 +49,9 @@ class Tracking extends MY_Controller {
                     'station_id' => $data['station_id'],
                 );
 
-                $this->tracking->insert_record($record);
+                $inserted_id = $this->tracking->insert_record($record);
+                $tracking_info = $this->tracking->get_by_id($inserted_id);
+                $this->shipment_tracking_email($tracking_info);
                 echo 'done';
                 exit;
             } else {
@@ -88,6 +90,8 @@ class Tracking extends MY_Controller {
                 );
 
                 $this->tracking->update_record($tracking_id, $record);
+                $tracking_info = $this->tracking->get_by_id($tracking_id);
+                $this->shipment_tracking_email($tracking_info);
                 echo 'done';
                 exit;
             } else {
@@ -113,6 +117,45 @@ class Tracking extends MY_Controller {
         redirect('stations/detail/' . $staion_id, 'location');
     }
 
-}
+    /**
+     * Send Email on Add/Edit station tracking info
+     * 
+     * @param array $record
+     * @return boolean 
+     */
+    function shipment_tracking_email($record) {
 
+        $template = '_Tracking_Ship_Date';
+        $template_data = $this->email_template->get_template_by_sys_id($template);
+
+        if (isset($template_data) && !empty($template_data)) {
+            $station_details = $this->station_model->get_station_by_id($record->station_id);
+            $subject = $template_data->subject;
+
+            $replacebale['ship_date'] = $record->ship_date;
+            $replacebale['ship_via'] = $record->ship_via;
+            $replacebale['tracking_no'] = $record->tracking_no;
+            $replacebale['no_box_shipped'] = $record->no_box_shipped;
+            $replacebale['station_name'] = isset($station_details->station_name) ? $station_details->station_name : '';
+
+
+            if ($this->config->item('demo') == true) {
+                $to_email = $this->config->item('to_email');
+                $from_email = $this->config->item('from_email');
+                $replacebale['user_name'] = 'AMS';
+            } else {
+                $to_email = $station_details->contact_email;
+                $from_email = $this->user_detail->email;
+                $replacebale['user_name'] = $this->user_detail->first_name . ' ' . $this->user_detail->last_name;
+            }
+            $replacebale['inform_to'] = 'ssapienza@cpb.org';
+            $this->emailtemplates->sent_now = true;
+            $email_queue_id = $this->emailtemplates->queue_email($template, $to_email, $replacebale);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
 ?>

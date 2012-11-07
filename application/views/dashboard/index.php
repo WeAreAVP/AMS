ffa2d777922b2523ca13b6dacf338fa55df5ce28
Nouman Tayyab <nouman@geekschicago.com>
2012-11-07 13:41:57 +0500

    dashboard changes

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,6 +1,13 @@
 <div style="margin: 0px -20px;">
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#container" data-toggle="tab">Total Digitized Assets</a></li>
+    <li><a href="#scheduled" data-toggle="tab">Total Scheduled Assets</a></li>
 
-  <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="container" style="min-width: 400px; height: 550px; margin: 0 auto"></div>
+    <div class="tab-pane" id="scheduled" style="min-width: 400px; height: 550px; margin: 0 auto">working on it</div>
+  </div>
 </div>
 
 <script type="text/javascript">
@@ -23,7 +30,8 @@
         title: {
           style: {
             color: '#000000',
-            font: 'Verdana, sans-serif'
+            font: 'Verdana, sans-serif',
+            
           }
         }
       },
@@ -94,46 +102,40 @@
           '1/8 inch audio cassette',
           'Audio cassette',
           'CD',
-          'Guangzhou',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Shenzhen',
-          'Istanbul'
+          'CD-R',
+          'CD-ROM',
+          'CD-RW',
+          'DAT',
+          'Betacam Digital (Digi Beta)',
+          'DVCAM',
+          'DVCAM: Sony',
+          'DVCPRO',
+          'DVD data disc',
+          'DVD-R',
+          'DVD+R',
+          'DVD',
+          'Hi8',
+          'Microcassette',
+          'Mini-cassette',
+          'Minidisc',
+          'MiniDV',
+          'DVC',
+          'S-VHS',
+          'Sony IMX tape',
+          'MPEG IMX',
+          'VHS',
+          'LP Record',
+          'EP Record',
+          'LP Record (45)',
+          'Others'
+          
         ],
         gridLineWidth: 0,
         labels: {
           rotation: -90,
           align: 'right',
           style: {
-            fontSize: '13px',
+            fontSize: '11px',
             fontFamily: 'Verdana, sans-serif'
           }
         },
@@ -141,11 +143,14 @@
         tickWidth:0
       },
       yAxis: {
+        tickInterval:100,
         gridLineWidth: 0,
         min: 0,
+        max:1000,
         title: {
           text: ''
-        }
+        },
+        
       },
       legend: {
         enabled: false
@@ -153,7 +158,7 @@
       tooltip: {
         formatter: function() {
           return '<b>'+ this.x +'</b><br/>'+
-             Highcharts.numberFormat(this.y, 1)+' Digitized';
+            Highcharts.numberFormat(this.y, 0)+' Digitized';
         }
       },
       series: [{
@@ -161,10 +166,11 @@
           pointWidth: 10,
           borderWidth: 0,
           
-          data: [34, 21, 20, 20, 19, 19, 19, 18, 18,
-            17, 16, 15, 14, 14, 13, 12, 12, 11,
-            101, 109,1,42,62,92,2,3,5,7,12,
-            10,12,14,12,192,72,42,22,52,82,112,13,162,106,18,142,62,72,102,92,82],
+          data: [341, 211, 210, 201, 191, 119, 192, 218, 128,127,
+            126, 315, 414, 514, 613, 712, 812, 511,101, 109,
+            165,342,262,192,442,143,235,347,312,610,
+            712,514,212,192,456,567,765,432,345,123,
+            123,456,789],
           dataLabels: {
             enabled: true,
             rotation: 0,

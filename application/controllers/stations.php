01f7bec2b00cb31a3fb9c385f9284de723b70202
Nouman Tayyab <nouman@geekschicago.com>
2012-11-12 15:52:56 +0500

    password change

--- application/controllers/stations.php
+++ application/controllers/stations.php
@@ -157,7 +157,7 @@ class Stations extends MY_Controller {
     
     $oSpreadSheet = new Zend_Gdata_Spreadsheets();
     $service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
-    $client = Zend_Gdata_ClientLogin::getHttpClient('nouman@geekschicago.com', 'trishamrita', $service);
+    $client = Zend_Gdata_ClientLogin::getHttpClient('purelogicsy@gmail.com', 'purelogics123', $service);
     $list = $oSpreadSheet->getSpreadsheetFeed();
 
     var_dump($list);

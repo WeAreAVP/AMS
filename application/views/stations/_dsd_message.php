55963d928522bbb6a5b513282d3d52ad0f3e1b1b
Nouman Tayyab <nouman@geekschicago.com>
2012-11-16 15:15:00 +0500

    dsd messages

--- application/views/stations/_dsd_message.php
+++ application/views/stations/_dsd_message.php
@@ -17,70 +17,7 @@
 </div>
 
 <script type="text/javascript">
-  function sendMessage(){
-      var stations=new Array();
-      $('input[name="station[]"]:checked').each(function(index,a){
-        stations[index]=$(this).val();
-      });
-      if(stations.length>0){
-        $.ajax({
-          type: 'POST', 
-          url: site_url+'stations/get_dsd_stations',
-          data:{id:stations},
-          dataType: 'json',
-          cache: false,
-          success: function (result) {
-            if(result.success==true){
-              var station_name='';
-              var compare_start_date=0;
-                        
-                        
-                        
-              var start_date=false;
-              for(cnt in result.records){
-                if(cnt==0){
-                  start_date=result.records[cnt].start_date;
-                }
-                if(cnt>=result.records.length-1){
-                  if(start_date==result.records[cnt].start_date && compare_start_date==0){
-                    compare_start_date=0;
-                  }
-                  else{
-                    compare_start_date=1; 
-                  }
-                          
-                         
-                          
-                }
-                                                                                                                                                
-                if(cnt==result.records.length-1)
-                  station_name+=result.records[cnt].station_name;
-                else
-                  station_name+=result.records[cnt].station_name+',';
-              }
-              if(compare_start_date==0 && start_date!=0){
-                $('#start_date').val(start_date);
-                console.log(start_date);
-              }
-              else if(compare_start_date==0 && start_date==0){
-                $('#start_date').val('');
-                console.log('empty date');
-              }
-              else{
-                console.log('conflicting dates');
-              }
-              $('#DSDLabel').html(station_name);
-              $('#showDSDPopUp').trigger('click');
-            }
-            else{
-              console.log(result);
-            }
-                                                                                                                                        
-          }
-        });
-      }
-                
-    }
+
     
 </script>
 

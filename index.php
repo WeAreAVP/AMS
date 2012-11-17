c9f6d8b4b3f09f0c644ad4f5bd1870081c12e925
Nouman Tayyab <nouman@geekschicago.com>
2012-11-17 15:20:22 +0500

    envirnment changes completed

--- index.php
+++ index.php
@@ -22,20 +22,6 @@
 
 define('ENVIRONMENT', getenv('ENVIRONMENT'));
 
-
-//$serverName = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : gethostname();
-//
-//
-//if (strpos($serverName, 'iserver.purelogics.info') !== FALSE) {
-//  define('ENVIRONMENT', 'testing');
-//} else if (strpos($serverName, 'ec2-54-242-106-197.compute-1.amazonaws.com') !== FALSE) {
-//  define('ENVIRONMENT', 'development');
-//} else if (strpos($serverName, 'ec2-174-129-139-234.compute-1.amazonaws.com') !== FALSE) {
-//  define('ENVIRONMENT', 'qatesting');
-//} else {
-//  define('ENVIRONMENT', 'production');
-//}
-
 /**
  * Setting the default timezone
  *  
@@ -51,22 +37,25 @@ date_default_timezone_set('America/New_York');
  * By default development will show errors but testing and live will hide them.
  */
 
-if (defined('ENVIRONMENT')) {
-  switch (ENVIRONMENT) {
-    case 'development':
-    case 'testing':
-    case 'qatesting':
-      error_reporting(E_ALL);
-      ini_set('display_errors', 1);
-      break;
+if (defined('ENVIRONMENT'))
+{
+    switch (ENVIRONMENT)
+    {
+        case 'development':
+        case 'testing':
+        case 'qatesting':
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+            break;
 
-    case 'production':
-      error_reporting(0);
-      break;
+        case 'production':
+            error_reporting(0);
+            ini_set('display_errors', 0);
+            break;
 
-    default:
-      exit('The application environment is not set correctly.');
-  }
+        default:
+            exit('The application environment is not set correctly.');
+    }
 }
 
 /*
@@ -153,20 +142,23 @@ $application_folder = 'application';
  */
 
 // Set the current directory correctly for CLI requests
-if (defined('STDIN')) {
-  chdir(dirname(__FILE__));
+if (defined('STDIN'))
+{
+    chdir(dirname(__FILE__));
 }
 
-if (realpath($system_path) !== FALSE) {
-  $system_path = realpath($system_path) . '/';
+if (realpath($system_path) !== FALSE)
+{
+    $system_path = realpath($system_path) . '/';
 }
 
 // ensure there's a trailing slash
 $system_path = rtrim($system_path, '/') . '/';
 
 // Is the system path correct?
-if (!is_dir($system_path)) {
-  exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: " . pathinfo(__FILE__, PATHINFO_BASENAME));
+if (!is_dir($system_path))
+{
+    exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: " . pathinfo(__FILE__, PATHINFO_BASENAME));
 }
 
 /*
@@ -193,14 +185,17 @@ define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
 
 // The path to the "application" folder
-if (is_dir($application_folder)) {
-  define('APPPATH', $application_folder . '/');
-} else {
-  if (!is_dir(BASEPATH . $application_folder . '/')) {
-    exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: " . SELF);
-  }
+if (is_dir($application_folder))
+{
+    define('APPPATH', $application_folder . '/');
+} else
+{
+    if (!is_dir(BASEPATH . $application_folder . '/'))
+    {
+        exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: " . SELF);
+    }
 
-  define('APPPATH', BASEPATH . $application_folder . '/');
+    define('APPPATH', BASEPATH . $application_folder . '/');
 }
 
 /*

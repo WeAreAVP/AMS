dd8834ea95a4df7e0445973c6146e980c374a356
Nouman Tayyab <nouman@geekschicago.com>
2012-11-20 12:40:29 +0500

    unit testing

--- application/controllers/tests/citest.php
+++ application/controllers/tests/citest.php
@@ -1,18 +1,61 @@
 <?php
 
 if (!defined('BASEPATH'))
-  exit('No direct script access allowed');
+    exit('No direct script access allowed');
 
 class Citest extends CIUnit_TestCase
 {
-	public function index(){
-       
-    
-    
-    
 
-    echo $this->unit->report();
+    public function index()
+    {
+
+        $this->users();
+
+$out = output();
+		
+		// Check if the content is OK
+		$this->assertSame(0, preg_match('/(error|notice)/i', $out));
+
+//        echo $this->unit->report();
     }
+
+    public function users()
+    {
+        $this->load->model('dx_auth/users', 'users');
+        $this->load->model('dx_auth/user_profile', 'user_profile');
+        $this->load->model('dx_auth/roles', 'roles');
+        $this->load->model('station_model');
+        $data['current_role'] = $currentRoleID = $this->role_id;
+        $data['is_ajax'] = false;
+        $roles = $this->roles->get_roles_list($currentRoleID)->result();
+        $params = null;
+        if (isAjax())
+        {
+            $data['is_ajax'] = true;
+            $params = array('station_id' => $this->input->post('station_id'), 'role_id' => $this->input->post('role_id'));
+        }
+
+        $data['users'] = $this->users->get_users($currentRoleID, $params)->result();
+        $data['roles'][''] = 'Select';
+        $data['stations'][''] = 'Select';
+        foreach ($roles as $value)
+        {
+            $data['roles'][$value->id] = $value->name;
+        }
+        $stations = $this->station_model->get_all();
+
+        foreach ($stations as $value)
+        {
+            $data['stations'][$value->id] = $value->station_name;
+        }
+        if (isAjax())
+        {
+            echo $this->load->view('settings/user', $data, TRUE);
+            exit;
+        }
+        $this->load->view('settings/user', $data);
+    }
+
 }
 
 /* End of file welcome.php */

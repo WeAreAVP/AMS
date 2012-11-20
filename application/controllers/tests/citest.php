c71e82ab3423aa1281599e0cc3269f0560494c73
Nouman Tayyab <nouman@geekschicago.com>
2012-11-20 12:15:55 +0500

    unit testing

--- application/controllers/tests/citest.php
+++ application/controllers/tests/citest.php
@@ -3,30 +3,25 @@
 if (!defined('BASEPATH'))
   exit('No direct script access allowed');
 
-class Citest extends CI_Controller {
-
-  /**
-   * Index Page for this controller.
-   *
-   * Maps to the following URL
-   * 		http://example.com/index.php/welcome
-   * 	- or -  
-   * 		http://example.com/index.php/welcome/index
-   * 	- or -
-   * Since this controller is set as the default controller in 
-   * config/routes.php, it's displayed at http://example.com/
-   *
-   * So any other public methods not prefixed with an underscore will
-   * map to /index.php/welcome/<method_name>
-   * @see http://codeigniter.com/user_guide/general/urls.html
-   */
-  public function index() {
-
-    
-
-    #echo $this->unit->report();
-  }
-
+class SomeControllerTest extends CIUnit_TestCase
+{
+	public function setUp()
+	{
+		// Set the tested controller
+		$this->CI = set_controller('welcome');
+	}
+	
+	public function testWelcomeController()
+	{
+		// Call the controllers method
+		$this->CI->index();
+		
+		// Fetch the buffered output
+		$out = output();
+		
+		// Check if the content is OK
+		$this->assertSame(0, preg_match('/(error|notice)/i', $out));
+	}
 }
 
 /* End of file welcome.php */

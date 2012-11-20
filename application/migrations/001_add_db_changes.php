fe25be428e019a3a52d3cc37009fced00b1b8e6f
Nouman Tayyab <nouman@geekschicago.com>
2012-11-20 16:24:08 +0500

    migration

--- application/migrations/001_add_db_changes.php
+++ application/migrations/001_add_db_changes.php
@@ -17,15 +17,62 @@ class Migration_Add_Db_Changes extends CI_Migration
 
     public function up()
     {
+        $this->dbforge->add_field('id');
+        $this->dbforge->add_field(array(
+            'folder_path' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '500',
+                'null' => FALSE,
+            ),
+            'created_at' => array(
+                'type' => 'DATETIME',
+                'null' => FALSE,
+            ),
+            'data_type' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '200',
+                'null' => TRUE,
+            ),
+        ));
+        $this->dbforge->create_table('data_folders');
         $fields = array(
-            'my_name VARCHAR(50) DEFAULT NULL',
+            'event_note TEXT DEFAULT NULL',
         );
-        $this->dbforge->add_column('user_profile', $fields);
+
+        $this->dbforge->add_column('events', $fields);
+
+        $this->dbforge->add_field('id');
+        $this->dbforge->add_field(array(
+            'assets_id' => array(
+                'type' => 'INT',
+                'constraint' => '11',
+                'null' => FALSE,
+            ),
+            'extension_element' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '45',
+                'null' => TRUE,
+            ),
+            'extension_value' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '45',
+                'null' => FALSE,
+            ),
+            'extension_authority' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '45',
+                'null' => TRUE,
+            ),
+        ));
+
+        $this->dbforge->create_table('extensions');
     }
 
     public function down()
     {
-        $this->dbforge->drop_column('user_profile', 'my_name');
+        $this->dbforge->drop_table('data_folders');
+        $this->dbforge->drop_column('events', 'event_note');
+        $this->dbforge->drop_table('extensions');
     }
 
 }

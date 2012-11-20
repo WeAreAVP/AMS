614ce29cee273a3701646f378e23fa8da8f94496
Nouman Tayyab <nouman@geekschicago.com>
2012-11-20 12:37:36 +0500

    unit testing

--- application/controllers/tests/citest.php
+++ application/controllers/tests/citest.php
@@ -6,10 +6,10 @@ if (!defined('BASEPATH'))
 class Citest extends CIUnit_TestCase
 {
 	public function index(){
-        $this->test_no_additional_headers();
-    $this->test_x_forwarded_for();
-    $this->test_client_ip();
-    $this->test_x_forwarded_for_and_client_ip();
+       
+    
+    
+    
 
     echo $this->unit->report();
     }

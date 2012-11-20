9ea9c8009c0a8bc0da06fa20d7650dfae49e4ad2
Nouman Tayyab <nouman@geekschicago.com>
2012-11-20 12:29:05 +0500

    unit testing

--- application/controllers/tests/citest.php
+++ application/controllers/tests/citest.php
@@ -3,25 +3,16 @@
 if (!defined('BASEPATH'))
   exit('No direct script access allowed');
 
-class Citest extends CIUnit_TestCase
+class SomeControllerTest extends CIUnit_TestCase
 {
-	public function setUp()
-	{
-		// Set the tested controller
-		$this->CI = set_controller('welcome');
-	}
-	
-	public function testWelcomeController()
-	{
-		// Call the controllers method
-		$this->CI->index();
-		
-		// Fetch the buffered output
-		$out = output();
-		
-		// Check if the content is OK
-		$this->assertSame(0, preg_match('/(error|notice)/i', $out));
-	}
+	public function index(){
+        $this->test_no_additional_headers();
+    $this->test_x_forwarded_for();
+    $this->test_client_ip();
+    $this->test_x_forwarded_for_and_client_ip();
+
+    echo $this->unit->report();
+    }
 }
 
 /* End of file welcome.php */

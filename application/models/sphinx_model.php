6ba07b21ffca34cefaf8b5e3e8f9fd8337c85968
Nouman Tayyab <nouman@geekschicago.com>
2012-11-22 17:27:11 +0500

    instantiation record list

--- application/models/sphinx_model.php
+++ application/models/sphinx_model.php
@@ -106,8 +106,8 @@ class Sphinx_Model extends CI_Model
         $this->sphinxsearch->set_array_result(true);
         $this->sphinxsearch->set_match_mode($mode);
         $this->sphinxsearch->set_connect_timeout(120);
-//        if ($limit)
-//            $this->sphinxsearch->set_limits((int) $offset, (int) $limit, ( $limit > 1000 ) ? $limit : 1000 );
+        if ($limit)
+            $this->sphinxsearch->set_limits((int) $offset, (int) $limit, ( $limit > 1000 ) ? $limit : 1000 );
 
 
         $res = $this->sphinxsearch->query($params['search'], 'instantiations_list');

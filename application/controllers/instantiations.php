e9cf30f6530cd4eb7b26b1c2341cb61187fe8725
Nouman Tayyab <nouman@geekschicago.com>
2012-11-22 12:09:52 +0500

    Instantiations controller addd

---
+++ application/controllers/instantiations.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Instantiations Tracking Controller
+ * 
+ * @package		AMS
+ * @subpackage	Tracking Controller
+ * @category	Controllers
+ * @author		Nouman Tayyab <nouman@geekschicago.com>
+ */
+class Instantiations extends MY_Controller
+{
+
+    /**
+     * Constructor
+     * 
+     * Load the layout and Instantiations model
+     * 
+     */
+    function __construct()
+    {
+        parent::__construct();
+        $this->layout = 'main_layout.php';
+        $this->load->model('instantiations_model', 'instantiation');
+    }
+    /**
+     * 
+     * 
+     */
+    public function index()
+    {
+        
+    }
+    public function detail()
+    {
+        
+    }
+
+    
+
+   
+   
+
+}
+
+// END Tracking Controller
+
+/* End of file instantiations.php */
+/* Location: ./application/controllers/instantiations.php */

67b391e313e08fa2414d63288e686b0325a529bd
Nouman Tayyab <nouman@geekschicago.com>
2012-11-22 14:51:07 +0500

    instantiation record list

--- application/views/instantiations/index.php
+++ application/views/instantiations/index.php
@@ -16,19 +16,19 @@
         </div>
     </div>
     <div  class="span9">
-        <div style="overflow: auto;height: 600px;" id="instantiation-main">
-            <table class="tablesorter table table-bordered" style="width: 850px;overflow: auto;">
+        <div style="overflow: auto;height: 600px;width: 850px;" id="instantiation-main">
+            <table class="tablesorter table table-bordered">
                 <thead>
                     <tr>
-                        <th style="width: 50px;">Asset ID</th>
-                        <th style="width: 75px;">Organization</th>
-                        <th style="width: 150px;">Asset Title</th>
-                        <th style="width: 90px;">Instantiation ID</th>
-                        <th style="width: 75px;">Source Date</th>
-                        <th style="width: 75px;">Date Type</th>
-                        <th style="width: 75px;">Format Type</th>
-                        <th style="width: 75px;">File size</th>
-                        <th style="width: 90px;">Unit of measure</th>
+                        <th style="width: 60px;">Asset ID</th>
+                        <th style="width: 90px;">Organization</th>
+                        <th style="width: 250px;">Asset Title</th>
+                        <th style="width: 120px;">Instantiation ID</th>
+                        <th style="width: 95px;">Source Date</th>
+                        <th style="width: 95px;">Date Type</th>
+                        <th style="width: 85px;">Format Type</th>
+                        <th style="width: 95px;">File size</th>
+                        <th style="width: 120px;">Unit of measure</th>
                         <th style="width: 75px;">Duration</th>
                         <th style="width: 75px;">Colors</th>
                         <th style="width: 75px;">Language</th>

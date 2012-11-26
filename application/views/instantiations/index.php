6d49457c0c0e9f3350d6f38aeaebf713beaa40c1
Nouman Tayyab <nouman@geekschicago.com>
2012-11-26 12:14:16 +0500

    instantiations pagination

--- application/views/instantiations/index.php
+++ application/views/instantiations/index.php
@@ -17,7 +17,7 @@
     </div>
     <div  class="span9">
         <div style="text-align: right;width: 860px;">
-            <div style="margin-right: 10px;"><strong>1 - <?php echo $count; ?></strong> of <strong><?php echo $total; ?></strong></div>
+            <strong>1 - <?php echo $count; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
             <?php echo $this->pagination->create_links(); ?>
         </div>
         <div style="overflow: auto;width:865px;" id="instantiation-main">
@@ -75,7 +75,7 @@
         </div>
 
         <div style="text-align: right;width: 860px;">
-            <div style="margin-right: 10px;"><strong>1 - <?php echo $count; ?></strong> of <strong><?php echo $total; ?></strong></div>
+            <strong>1 - <?php echo $count; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
             <?php echo $this->pagination->create_links(); ?>
         </div>
     </div>

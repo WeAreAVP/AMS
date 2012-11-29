6606193c57b2b73bb0e245200782581e2239aac7
Nouman Tayyab <nouman@geekschicago.com>
2012-11-29 19:31:16 +0500

    faceted search

--- js/jquery.blockUI.js
+++ js/jquery.blockUI.js
@@ -141,7 +141,7 @@
 			forceIframe: false,
 
 			// z-index for the blocking overlay
-			baseZ: 1000,
+			baseZ: 99999,
 
 			// set these to true to have the message automatically centered
 			centerX: true, // <-- only effects element blocking (page block controlled via css above)

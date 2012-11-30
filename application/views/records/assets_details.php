3a6a72ecd569a2cbead338d2e6f1a32c29460b8b
aliraza001 <ali.raza@pureloigcs.net>
2012-11-30 20:55:58 +0500

    Sphinx configration changes for Facet search

--- application/views/records/assets_details.php
+++ application/views/records/assets_details.php
@@ -9,12 +9,13 @@
         <?php 
 		if(isset($asset_instantiations['records']) && !empty($asset_instantiations['records']))
 		{?>
-	        <div class="container-sidebar"><?php
+           <div class="container-sidebar"><?php
               	foreach($asset_instantiations['records'] as $asset_instantiation)
 				{?>
-            		<h4><a href="<?php echo site_url('instantiations/detail/'.$asset_instantiation->id)?>"><?php echo $asset_instantiation->instantiation_identifier?></a></h4>
+            		<h4><a href="<?php echo site_url('instantiations/detail/'.$asset_instantiation->id)?>"><?php echo $asset_instantiation->guid_identifier?></a></h4>
                     <?php 
-					echo (isset($asset_instantiation->instantiation_identifier) && ($asset_instantiation->instantiation_identifier!=NULL))?"Source: ".$asset_instantiation->instantiation_identifier.'<br/>':'';
+					echo (isset($asset_instantiation->asset_title) && ($asset_instantiation->asset_title!=NULL))?"Title: ".$asset_instantiation->asset_title.'<br/>':'';
+					echo (isset($asset_instantiation->instantiation_identifier) && ($asset_instantiation->instantiation_identifier!=NULL))?"Instantiation ID: ".$asset_instantiation->instantiation_identifier.'<br/>':'';
 					echo (isset($asset_instantiation->format_name) && ($asset_instantiation->format_name!=NULL) )?"Format: ".$asset_instantiation->format_name.'<br/>':'';
 					echo (isset($asset_instantiation->generation) && ($asset_instantiation->generation!=NULL) ) ?"Generation: ".$asset_instantiation->generation.'<br/>':'';
 					echo ($asset_instantiation->actual_duration>0)?"Actual Duration: ".duration($asset_instantiation->actual_duration).'<br/>':'';

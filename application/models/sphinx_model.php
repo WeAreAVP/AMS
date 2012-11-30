8f552f82e36093fcde5f91af167dc0d7d8bd3d63
Nouman Tayyab <nouman@geekschicago.com>
2012-11-30 12:41:49 +0500

    facet search

--- application/models/sphinx_model.php
+++ application/models/sphinx_model.php
@@ -161,12 +161,12 @@ class Sphinx_Model extends CI_Model
         if (isset($this->session->userdata['physical_format']) && $this->session->userdata['physical_format'] != '')
         {
             $physical_format = str_replace("|||", "' | '", trim($this->session->userdata['physical_format']));
-            $where .=" @format_type '$physical_format'";
+            $where .=" @format_name '$physical_format'";
         }
         if (isset($this->session->userdata['digital_format']) && $this->session->userdata['digital_format'] != '')
         {
             $digital_format= str_replace("|||", "' | '", trim($this->session->userdata['digital_format']));
-            $where .=" @format_type '$digital_format'";
+            $where .=" @format_name '$digital_format'";
         }
         if (isset($this->session->userdata['generation']) && $this->session->userdata['generation'] != '')
         {

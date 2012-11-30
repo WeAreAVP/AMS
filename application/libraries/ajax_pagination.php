706f58e7e36400543bc6626d4e3eabdfff5008d8
Nouman Tayyab <nouman@geekschicago.com>
2012-11-30 14:08:29 +0500

    facet search

--- application/libraries/ajax_pagination.php
+++ application/libraries/ajax_pagination.php
@@ -368,11 +368,10 @@ class Ajax_pagination {
     function my_link_to_remote($text, $method, $pars=array())
 	{
         if($pars !== NULL):
-        	$final_perm= json_encode($pars);
-			$click= $method.'(\'escape('.$final_perm.')\');';
-            
+        	$final_perm=json_encode($pars);
+			$click= $method.'(\''.$final_perm.'\');';
         	//onclick='new Ajax.Updater('$div','$url',{method: 'post', parameters:{'.$par.'}, evalScripts:true}); return false;'
-            $html = '<a  href="javascript:;" onclick="'.$click.'" >'.$text.'</a>';
+            $html = '<a  href="javascript:;" onclick="escape('.$click.')" >'.$text.'</a>';
         else:
       	  $html = "<a href='javascript:;'  onclick='".$method."(); return false;'>$text</a>";
         endif;

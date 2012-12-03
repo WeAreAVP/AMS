0b0a53dd145a6cdeb94e3c8023a43403a5b32020
Nouman Tayyab <nouman@geekschicago.com>
2012-12-03 18:55:58 +0500

    instantiation detail

--- application/views/layouts/main_layout.php
+++ application/views/layouts/main_layout.php
@@ -151,12 +151,12 @@
 
 <?php } ?>
 <?php
-if (is_route_method(array('records' => array('index', 'flagged'), 'instantiations' => array('index', 'detail'))))
+if (is_route_method(array('records' => array('index', 'flagged'), 'instantiations' => array('index'))))
 {
     ?>
                     <ul class="nav nav-tabs">
                         <li class="<?php echo active_anchor('records', array('index', 'flagged')); ?>"><a href="<?php echo site_url('records/index'); ?>">Assets</a></li>
-                        <li class="<?php echo active_anchor('instantiations', array('index')); ?>"><a href="<?php echo site_url('instantiations/index'); ?>">Instantiations</a></li>
+                        <li class="<?php echo active_anchor('instantiations', array('index', 'detail')); ?>"><a href="<?php echo site_url('instantiations/index'); ?>">Instantiations</a></li>
 
                     </ul>
 

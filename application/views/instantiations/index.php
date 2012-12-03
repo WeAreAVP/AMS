6ca4341ff70b6102f2399f3a4b0c08658256d7b8
Nouman Tayyab <nouman@geekschicago.com>
2012-12-03 16:40:07 +0500

    search

--- application/views/instantiations/index.php
+++ application/views/instantiations/index.php
@@ -20,12 +20,13 @@ if (!$isAjax)
                         <th><span style="float:left;min-width: 100px;">Organization</span></th>
                         <th><span style="float:left;min-width: 250px;">Asset Title</span></th>
                         <th><span style="float:left;min-width: 100px;">Instantiation ID</span></th>
-                        <th><span style="float:left;min-width: 90px;">Source Date</span></th>
-                        <th><span style="float:left;min-width: 90px;">Date Type</span></th>
+                        <th><span style="float:left;min-width: 100px;">Instantiation ID Source</span></th>
                         <th><span style="float:left;min-width: 90px;">Format Type</span></th>
+                        <th><span style="float:left;min-width: 70px;">Duration</span></th>
+                        <th><span style="float:left;min-width: 90px;">Date</span></th>
+                        <th><span style="float:left;min-width: 90px;">Date Type</span></th>
                         <th><span style="float:left;min-width: 90px;">File size</span></th>
                         <th><span style="float:left;min-width: 100px;">Unit of measure</span></th>
-                        <th><span style="float:left;min-width: 70px;">Duration</span></th>
                         <th><span style="float:left;min-width: 70px;">Colors</span></th>
                         <th><span style="float:left;min-width: 70px;">Language</span></th>
                     </tr>
@@ -41,13 +42,14 @@ if (!$isAjax)
 <!--                                <td><?php echo $value->assets_id; ?></td>-->
                                 <td><?php echo $value->organization; ?></td>
                                 <td><a href="<?php echo site_url('instantiations/detail/'.$value->id); ?>"><?php echo $value->asset_title; ?></a></td>
-                                <td><?php echo $value->id; ?></td>
+                                <td><?php echo $value->instantiation_identifier; ?></td>
+                                <td><?php echo $value->instantiation_source; ?></td>
+                                <td><?php echo $value->format_type; ?></td>
+                                <td><?php echo ($value->actual_duration) ? $value->actual_duration : 'N/A'; ?></td>
                                 <td><?php echo ($value->instantiation_date == 0) ? 'No Source Date' : date('Y-m-d', $value->instantiation_date); ?></td>
                                 <td><?php echo $value->date_type; ?></td>
-                                <td><?php echo $value->format_type; ?></td>
                                 <td><?php echo ($value->file_size == 0) ? 'N/A' : $value->file_size; ?></td>
                                 <td><?php echo ($value->file_size_unit_of_measure) ? $value->file_size_unit_of_measure : 'N/A'; ?></td>
-                                <td><?php echo ($value->actual_duration) ? $value->actual_duration : 'N/A'; ?></td>
                                 <td><?php echo ($value->color) ? $value->color : 'N/A'; ?></td>
                                 <td><?php echo ($value->language) ? $value->language : 'N/A'; ?></td>
 

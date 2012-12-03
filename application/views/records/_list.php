d0efc61010de06c5f859fdbe9c8011ac448c5e8d
Nouman Tayyab <nouman@geekschicago.com>
2012-12-03 18:34:56 +0500

    instantiation detail

--- application/views/records/_list.php
+++ application/views/records/_list.php
@@ -7,26 +7,26 @@
         if (isset($asset_instantiations['records']) && !empty($asset_instantiations['records']))
         {
             ?>
-            <div class="container-sidebar"><?php
-        foreach ($asset_instantiations['records'] as $asset_instantiation)
-        {
-            if ($asset_instantiation->id == $inst_id)
-                $style = 'background-color:#1797D2;color:white;';
-            else
-                $style = '';
+            <?php
+            foreach ($asset_instantiations['records'] as $asset_instantiation)
+            {
+                if ($asset_instantiation->id == $inst_id)
+                    $style = 'background-color:#1797D2;color:white;';
+                else
+                    $style = '';
                 ?>
-                    <div style="<?php echo $style; ?>"><h4><a style="<?php echo $style; ?>" href="<?php echo site_url('instantiations/detail/' . $asset_instantiation->id) ?>"><?php echo $asset_details->guid_identifier ?></a></h4>
-                        <?php
-                        echo (isset($asset_instantiation->asset_title) && ($asset_instantiation->asset_title != NULL)) ? "Title: " . $asset_instantiation->asset_title . '<br/>' : '';
-                        echo (isset($asset_instantiation->instantiation_identifier) && ($asset_instantiation->instantiation_identifier != NULL)) ? "Instantiation ID: " . $asset_instantiation->instantiation_identifier . '<br/>' : '';
-                        echo (isset($asset_instantiation->format_name) && ($asset_instantiation->format_name != NULL) ) ? "Format: " . $asset_instantiation->format_name . '<br/>' : '';
-                        echo (isset($asset_instantiation->generation) && ($asset_instantiation->generation != NULL) ) ? "Generation: " . $asset_instantiation->generation . '<br/>' : '';
-                        echo ($asset_instantiation->actual_duration > 0) ? "Actual Duration: " . duration($asset_instantiation->actual_duration) . '<br/>' : '';
-                        echo ($asset_instantiation->projected_duration > 0) ? "Projected Duration: " . duration($asset_instantiation->projected_duration) . '<br/>' : '';
-                        echo '</div>'
-                        ?><?php }
-                    ?>
-                </div>
-            <?php } ?>
-        </div><!--end of sidebar-->
+                <div class="container-sidebar" style="<?php echo $style; ?>"><h4><a style="<?php echo $style; ?>" href="<?php echo site_url('instantiations/detail/' . $asset_instantiation->id) ?>"><?php echo $asset_details->guid_identifier ?></a></h4>
+                    <?php
+                    echo (isset($asset_instantiation->asset_title) && ($asset_instantiation->asset_title != NULL)) ? "Title: " . $asset_instantiation->asset_title . '<br/>' : '';
+                    echo (isset($asset_instantiation->instantiation_identifier) && ($asset_instantiation->instantiation_identifier != NULL)) ? "Instantiation ID: " . $asset_instantiation->instantiation_identifier . '<br/>' : '';
+                    echo (isset($asset_instantiation->format_name) && ($asset_instantiation->format_name != NULL) ) ? "Format: " . $asset_instantiation->format_name . '<br/>' : '';
+                    echo (isset($asset_instantiation->generation) && ($asset_instantiation->generation != NULL) ) ? "Generation: " . $asset_instantiation->generation . '<br/>' : '';
+                    echo ($asset_instantiation->actual_duration > 0) ? "Actual Duration: " . duration($asset_instantiation->actual_duration) . '<br/>' : '';
+                    echo ($asset_instantiation->projected_duration > 0) ? "Projected Duration: " . duration($asset_instantiation->projected_duration) . '<br/>' : '';
+                    echo '</div>'
+                    ?><?php }
+                ?>
+            </div>
+        <?php } ?>
+
     </div>

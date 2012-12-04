f0eac6583487b67b13bd0701ab4b88e6454f6248
Nouman Tayyab <nouman@geekschicago.com>
2012-12-04 13:56:30 +0500

    facet search

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -833,16 +833,16 @@
                     if(customColumnName!=''){
                         customColumnName= customColumnName.split(' ');
                         if(customColumnName.length>1){
-                            searchString='@'+customColumnName[0]+' |||'+$('#search').val()+'|||';
-                            searchString +='@'+customColumnName[1]+' |||'+$('#search').val()+'|||';
+                            searchString=' @'+customColumnName[0]+' |||'+$('#search').val()+'|||';
+                            searchString +=' @'+customColumnName[1]+' |||'+$('#search').val()+'|||';
                         }
                         else{
-                            searchString='@'+customColumnName[0]+' |||'+$('#search').val()+'|||';
+                            searchString=' @'+customColumnName[0]+' |||'+$('#search').val()+'|||';
                         }
                         
                     }
                     else{
-                        searchString+='|||'+$('#search').val();
+                        searchString+=' |||'+$('#search').val();
                     }
                     
                     $('#keyword_field_main_search').val(searchString);

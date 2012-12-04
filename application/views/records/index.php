6fe913e3b2319216fc6a69cd5ec1eea637eb0853
aliraza001 <ali.raza@pureloigcs.net>
2012-12-04 19:49:18 +0500

    Some cron level checks

--- application/views/records/index.php
+++ application/views/records/index.php
@@ -106,15 +106,15 @@ if (!$isAjax)
             <tr>
              <td style="vertical-align:middle;font-weight:bold"><i class="icon-flag "></i></td>
              <th style="vertical-align:middle;font-weight:bold;">AA GUID</th>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Local ID</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Titles</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Titles Type</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Titles Ref</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Titles Source</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Description</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Description Type</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Subjects</td>
-             <td style="vertical-align:middle;font-weight:bold;width:30%">Subjects Ref</td>
+             <td style="vertical-align:middle;font-weight:bold;">Local ID</td>
+             <td style="vertical-align:middle;font-weight:bold;" width="200">Titles</td>
+             <td style="vertical-align:middle;font-weight:bold;" >Titles Type</td>
+             <td style="vertical-align:middle;font-weight:bold;">Titles Ref</td>
+             <td style="vertical-align:middle;font-weight:bold;">Titles Source</td>
+             <td style="vertical-align:middle;font-weight:bold;" width="200">Description</td>
+             <td style="vertical-align:middle;font-weight:bold;">Description Type</td>
+             <td style="vertical-align:middle;font-weight:bold;">Subjects</td>
+             <td style="vertical-align:middle;font-weight:bold;">Subjects Ref</td>
              <td style="vertical-align:middle;font-weight:bold;">Subjects Source</td>
              <td style="vertical-align:middle;font-weight:bold;">Genre</td>
              <td style="vertical-align:middle;font-weight:bold;">Genre Source</td>

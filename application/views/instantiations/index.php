3e44b14a3482d87d06995f3316e38da58beb8cf0
Nouman Tayyab <nouman@geekschicago.com>
2012-12-05 16:36:25 +0500

    instantiation result

--- application/views/instantiations/index.php
+++ application/views/instantiations/index.php
@@ -4,74 +4,83 @@ if (!$isAjax)
     ?>
     <div class="row-fluid">
         <div class="span3">
-           <?php $this->load->view('instantiations/_facet_search'); ?>
+            <?php $this->load->view('instantiations/_facet_search'); ?>
         </div>
         <div  class="span9" id="data_container">
         <?php } ?>
-        <div style="text-align: right;width: 860px;">
-            <strong><?php echo $start; ?> - <?php echo $end; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
-            <?php echo $this->ajax_pagination->create_links(); ?>
-        </div>
-        <div style="overflow: auto;width:865px;" id="instantiation-main">
-            <table class="tablesorter table-freeze-custom table-bordered freeze-my-column" id="instantiation_table" style="margin-top:0px;margin-left: 1px;">
-                <thead>
-                    <tr>
-<!--                        <th><span style="float:left;min-width: 80px;">Asset ID</span></th>-->
-                        <th><span style="float:left;min-width: 100px;">Organization</span></th>
-                        <th><span style="float:left;min-width: 250px;">Asset Title</span></th>
-                        <th><span style="float:left;min-width: 100px;">Instantiation ID</span></th>
-                        <th><span style="float:left;min-width: 130px;">Instantiation ID Source</span></th>
-                        <th><span style="float:left;min-width: 90px;">Format Type</span></th>
-                        <th><span style="float:left;min-width: 70px;">Duration</span></th>
-                        <th><span style="float:left;min-width: 90px;">Date</span></th>
-                        <th><span style="float:left;min-width: 90px;">Date Type</span></th>
-                        <th><span style="float:left;min-width: 90px;">File size</span></th>
-                        <th><span style="float:left;min-width: 100px;">Unit of measure</span></th>
-                        <th><span style="float:left;min-width: 70px;">Colors</span></th>
-                        <th><span style="float:left;min-width: 70px;">Language</span></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    if (count($records) > 0)
-                    {
-                        foreach ($records as $key => $value)
+        <?php
+        if (count($records) > 0)
+        {
+            ?>
+            <div style="text-align: right;width: 860px;">
+                <strong><?php echo $start; ?> - <?php echo $end; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
+                <?php echo $this->ajax_pagination->create_links(); ?>
+            </div>
+            <div style="overflow: auto;width:865px;" id="instantiation-main">
+                <table class="tablesorter table-freeze-custom table-bordered freeze-my-column" id="instantiation_table" style="margin-top:0px;margin-left: 1px;">
+                    <thead>
+                        <tr>
+    <!--                        <th><span style="float:left;min-width: 80px;">Asset ID</span></th>-->
+                            <th><span style="float:left;min-width: 100px;">Organization</span></th>
+                            <th><span style="float:left;min-width: 250px;">Asset Title</span></th>
+                            <th><span style="float:left;min-width: 100px;">Instantiation ID</span></th>
+                            <th><span style="float:left;min-width: 130px;">Instantiation ID Source</span></th>
+                            <th><span style="float:left;min-width: 90px;">Format Type</span></th>
+                            <th><span style="float:left;min-width: 70px;">Duration</span></th>
+                            <th><span style="float:left;min-width: 90px;">Date</span></th>
+                            <th><span style="float:left;min-width: 90px;">Date Type</span></th>
+                            <th><span style="float:left;min-width: 90px;">File size</span></th>
+                            <th><span style="float:left;min-width: 100px;">Unit of measure</span></th>
+                            <th><span style="float:left;min-width: 70px;">Colors</span></th>
+                            <th><span style="float:left;min-width: 70px;">Language</span></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if (count($records) > 0)
+                        {
+                            foreach ($records as $key => $value)
+                            {
+                                ?>
+                                <tr>
+            <!--                                <td><?php echo $value->assets_id; ?></td>-->
+                                    <td><?php echo $value->organization; ?></td>
+                                    <td><a href="<?php echo site_url('instantiations/detail/' . $value->id); ?>"><?php echo $value->asset_title; ?></a></td>
+                                    <td><?php echo $value->instantiation_identifier; ?></td>
+                                    <td><?php echo $value->instantiation_source; ?></td>
+                                    <td><?php echo $value->format_type; ?></td>
+                                    <td><?php echo ($value->actual_duration) ? $value->actual_duration : 'N/A'; ?></td>
+                                    <td><?php echo ($value->instantiation_date == 0) ? 'No Source Date' : date('Y-m-d', $value->instantiation_date); ?></td>
+                                    <td><?php echo $value->date_type; ?></td>
+                                    <td><?php echo ($value->file_size == 0) ? 'N/A' : $value->file_size; ?></td>
+                                    <td><?php echo ($value->file_size_unit_of_measure) ? $value->file_size_unit_of_measure : 'N/A'; ?></td>
+                                    <td><?php echo ($value->color) ? $value->color : 'N/A'; ?></td>
+                                    <td><?php echo ($value->language) ? $value->language : 'N/A'; ?></td>
+
+                                </tr>
+                                <?php
+                            }
+                        } else
                         {
                             ?>
                             <tr>
-<!--                                <td><?php echo $value->assets_id; ?></td>-->
-                                <td><?php echo $value->organization; ?></td>
-                                <td><a href="<?php echo site_url('instantiations/detail/'.$value->id); ?>"><?php echo $value->asset_title; ?></a></td>
-                                <td><?php echo $value->instantiation_identifier; ?></td>
-                                <td><?php echo $value->instantiation_source; ?></td>
-                                <td><?php echo $value->format_type; ?></td>
-                                <td><?php echo ($value->actual_duration) ? $value->actual_duration : 'N/A'; ?></td>
-                                <td><?php echo ($value->instantiation_date == 0) ? 'No Source Date' : date('Y-m-d', $value->instantiation_date); ?></td>
-                                <td><?php echo $value->date_type; ?></td>
-                                <td><?php echo ($value->file_size == 0) ? 'N/A' : $value->file_size; ?></td>
-                                <td><?php echo ($value->file_size_unit_of_measure) ? $value->file_size_unit_of_measure : 'N/A'; ?></td>
-                                <td><?php echo ($value->color) ? $value->color : 'N/A'; ?></td>
-                                <td><?php echo ($value->language) ? $value->language : 'N/A'; ?></td>
-
+                                <td colspan="12" style="text-align: center;">No instantiation record found.</td>
                             </tr>
-                            <?php
-                        }
-                    } else
-                    {
-                        ?>
-                        <tr>
-                            <td colspan="12" style="text-align: center;">No instantiation record found.</td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
+                        <?php } ?>
+                    </tbody>
 
-            </table>
-        </div>
+                </table>
+            </div>
 
-        <div style="text-align: right;width: 860px;">
-            <strong><?php echo $start; ?> - <?php echo $end; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
-            <?php echo $this->ajax_pagination->create_links(); ?>
-        </div>
+            <div style="text-align: right;width: 860px;">
+                <strong><?php echo $start; ?> - <?php echo $end; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
+                <?php echo $this->ajax_pagination->create_links(); ?>
+            </div>
+        <?php } else
+        { ?>
+            <div  style="text-align: center;width: 860px;">No instantiation record found.</div>
+            <?php }
+        ?>
         <?php
         if (!$isAjax)
         {
@@ -79,10 +88,10 @@ if (!$isAjax)
         </div>
     </div>
     <script type="text/javascript">
-               
-                
+                           
+                            
         $(function() {
-    			 
+                			 
             $('#instantiation_table').freezeTableColumns({
                 width:       860,   // required
                 height:      600,   // required
@@ -91,6 +100,6 @@ if (!$isAjax)
                 clearWidths: true  // optional
             });//freezeTableColumns
         });
-        
+                    
     </script>
 <?php } ?>

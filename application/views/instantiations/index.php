50add63061b2ffb25a873c22af6e1f8c95f1c24d
Nouman Tayyab <nouman@geekschicago.com>
2012-12-06 12:58:53 +0500

    add the plugin

--- application/views/instantiations/index.php
+++ application/views/instantiations/index.php
@@ -1,26 +1,26 @@
 <style type="text/css">
-    .tablewrapper{ width: 500px; height: 500px; overflow: auto; border: solid 2px #999; }
+    .tablewrapper{ width: 860; height: 500px; overflow: auto; border: solid 2px #999; }
 
-			table{ width: 900px; font-size: 11px; }
-			table th, 
-			table td{ padding: 5px 5px 5px 10px; min-width: 80px;}
-			table th{ background-color: #f8f8f8; text-align: left; }
-			table tr.bg0 td{ background-color: #fff; }
-			table tr.bg1 td{ background-color: #f4f4f4; }
-			/* pt specific styles: ptfixed, ptdragover, ptdraghandle, ptshowhide */
-			table tr th.ptfixed{ background-color: #F1F8FA; border-bottom: solid 1px #aaa; opacity: 0.95; filter: alpha(95); }
-			table tr.bg0 td.ptfixed{ background-color: #eee; border-right: solid 1px #aaa;  opacity: 0.95; filter: alpha(95); }
-			table tr.bg1 td.ptfixed{ background-color: #e4e4e4; border-right: solid 1px #aaa;  opacity: 0.95; filter: alpha(95); }
-			table .ptdragover{ border-left: dashed 2px #A04334; padding-left: 8px; }
-			table th .ptdraghandle{ text-decoration: none; font-weight: bold; font-size: 14px; padding: 0 4px; cursor: move; }
-			table th .ptshowhide{ text-decoration: none; font-weight: bold; font-size: 14px; padding: 0 4px; }
-            
-            ul#tableController{ float: right; padding: 0; width: 175px; }
-			ul#tableController li{ list-style: none; margin: 5px 0; padding: 5px 5px; color: #777; border-bottom: solid 2px #eee; }
-			ul#tableController li[data-ptcolumnvisible='true']{ color: #000; border-bottom: solid 2px #aaa; }
-			ul#tableController li.ptdragover{ border: dashed 2px #A04334; padding: 3px 3px 5px 3px; }
-			ul#tableController li .ptdraghandle{ text-decoration: none; font-weight: bold; padding: 0 4px; font-size: 14px; cursor: move; float: right;}
-			ul#tableController li .ptshowhide{ text-decoration: none; font-weight: bold; padding: 0 4px; font-size: 14px;  }
+    table{ width: 900px; font-size: 11px; }
+    table th, 
+    table td{ padding: 5px 5px 5px 10px; min-width: 80px;}
+    table th{ background-color: white; text-align: left; }
+    table tr.bg0 td{ background-color: #fff; }
+    table tr.bg1 td{ background-color: #f4f4f4; }
+    /* pt specific styles: ptfixed, ptdragover, ptdraghandle, ptshowhide */
+    table tr th.ptfixed{ background-color: #F1F8FA; border-bottom: solid 1px #aaa; opacity: 0.95; filter: alpha(95); }
+    table tr.bg0 td.ptfixed{ background-color: #eee; border-right: solid 1px #aaa;  opacity: 0.95; filter: alpha(95); }
+    table tr.bg1 td.ptfixed{ background-color: #e4e4e4; border-right: solid 1px #aaa;  opacity: 0.95; filter: alpha(95); }
+    table .ptdragover{ border-left: dashed 2px #A04334; padding-left: 8px; }
+    table th .ptdraghandle{ text-decoration: none; font-weight: bold; font-size: 14px; padding: 0 4px; cursor: move; }
+    table th .ptshowhide{ text-decoration: none; font-weight: bold; font-size: 14px; padding: 0 4px; }
+
+    ul#tableController{ float: right; padding: 0; width: 175px; }
+    ul#tableController li{ list-style: none; margin: 5px 0; padding: 5px 5px; color: #777; border-bottom: solid 2px #eee; }
+    ul#tableController li[data-ptcolumnvisible='true']{ color: #000; border-bottom: solid 2px #aaa; }
+    ul#tableController li.ptdragover{ border: dashed 2px #A04334; padding: 3px 3px 5px 3px; }
+    ul#tableController li .ptdraghandle{ text-decoration: none; font-weight: bold; padding: 0 4px; font-size: 14px; cursor: move; float: right;}
+    ul#tableController li .ptshowhide{ text-decoration: none; font-weight: bold; padding: 0 4px; font-size: 14px;  }
 </style>
 <?php
 if (!$isAjax)
@@ -28,7 +28,7 @@ if (!$isAjax)
     ?>
     <div class="row-fluid">
         <div class="span3">
-    <?php $this->load->view('instantiations/_facet_search'); ?>
+            <?php $this->load->view('instantiations/_facet_search'); ?>
         </div>
         <div  class="span9" id="data_container">
         <?php } ?>
@@ -38,7 +38,7 @@ if (!$isAjax)
             ?>
             <div style="text-align: right;width: 860px;">
                 <strong><?php echo $start; ?> - <?php echo $end; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
-    <?php echo $this->ajax_pagination->create_links(); ?>
+                <?php echo $this->ajax_pagination->create_links(); ?>
             </div>
             <div id="instantiation-main" class="tablewrapper">
     <!--                <table class="tablesorter table-freeze-custom table-bordered freeze-my-column" id="instantiation_table" style="margin-top:0px;margin-left: 1px;">-->
@@ -86,18 +86,18 @@ if (!$isAjax)
                     </tbody>
                 </table>
                 <div style="display: none;">
-                <pre class="code example" data-lllanguage="js">$pt = $('#instantiation_table').powertable({
-	fixedColumns: ['nomination'],
-	fixedRows: [0],
-	moveDisabled: ['nomination'],
-	showHideDisabled: ['nomination']
-});</pre>
-                    </div>
+                    <pre class="code example" data-lllanguage="js">$pt = $('#instantiation_table').powertable({
+    	fixedColumns: ['nomination'],
+    	fixedRows: [0],
+    	moveDisabled: ['nomination'],
+    	showHideDisabled: ['nomination']
+    });</pre>
+                </div>
             </div>
 
             <div style="text-align: right;width: 860px;">
                 <strong><?php echo $start; ?> - <?php echo $end; ?></strong> of <strong style="margin-right: 10px;"><?php echo $total; ?></strong>
-            <?php echo $this->ajax_pagination->create_links(); ?>
+                <?php echo $this->ajax_pagination->create_links(); ?>
             </div>
             <?php
         } else
@@ -113,14 +113,14 @@ if (!$isAjax)
         </div>
     </div>
     <script type="text/javascript">
-                	$(document).ready(function(){
-				$('pre.example').each(function(){
-					eval($(this).text());
-				});
+        $(document).ready(function(){
+            $('pre.example').each(function(){
+                eval($(this).text());
+            });
 
-				$('pre').litelighter({  });
-			});                                   
-                                                    
+            $('pre').litelighter({  });
+        });                                   
+                                                        
         //        $(function() {
         //                        			 
         //            $('.freeze-my-column').freezeTableColumns({
@@ -131,6 +131,6 @@ if (!$isAjax)
         //                clearWidths: false  // optional
         //            });//freezeTableColumns
         //        });
-                                            
+                                                
     </script>
 <?php } ?>

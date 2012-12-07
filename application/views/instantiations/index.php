69cca7015221ab6ab5138369c62f44bfacc0bc07
Nouman Tayyab <nouman@geekschicago.com>
2012-12-07 18:42:25 +0500

    column reoder

--- application/views/instantiations/index.php
+++ application/views/instantiations/index.php
@@ -148,18 +148,18 @@ if (!$isAjax)
                         reOrderDropDown(columnArray);
                     }
                 },
-                                                                                                            
+                                                                                                                
                 'bPaginate':false,
                 'bInfo':false,
                 'bFilter': false,
                 "bSort": false,
                 "sScrollY": 400,
                 "sScrollX": "100%"
-                                                                                                            
+                                                                                                                
             });
             if(frozen>0)
                 new FixedColumns( oTable );
-                                                                                    
+                                                                                        
             $.extend( $.fn.dataTableExt.oStdClasses, {
                 "sWrapper": "dataTables_wrapper form-inline"
             } );
@@ -200,10 +200,38 @@ if (!$isAjax)
         }
         function freezeColumns(count){
             frozen=count;
-            new FixedColumns(null);
             new FixedColumns( oTable, {
-                "iLeftColumns": count
-            });
+                "iLeftColumns": count,
+                "fnDrawCallback": function ( left, right ) {
+                    var that = this, groupVal = null, matches = 0, heights = [], index = -1;
+    			
+                    /* Get the heights of the cells and remove redundant ones */
+                    $('tbody tr td', left.body).each( function ( i ) {
+                        var currVal = this.innerHTML;
+    				
+                        /* Reset values on new cell data. */
+                        if (currVal != groupVal) {
+                            groupVal = currVal;
+                            index++;
+                            heights[index] = 0;
+                            matches = 0;
+                        } else  {
+                            matches++;
+                        }
+    				
+                        heights[ index ] += $(this.parentNode).height();
+                        if ( currVal == groupVal && matches > 0 ) {
+                            this.parentNode.parentNode.removeChild(this.parentNode);
+                        }
+                    } );
+
+                    /* Now set the height of the cells which remain, from the summed heights */
+                    $('tbody tr td', left.body).each( function ( i ) {
+                        that.fnSetRowHeight( this.parentNode, heights[ i ] );
+                    } );
+                }
+            } );
+            
         }
     </script>
 <?php } ?>

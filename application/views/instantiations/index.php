701bde3376828c820f75d229f058209b669f6ab4
Nouman Tayyab <nouman@geekschicago.com>
2012-12-07 16:41:17 +0500

    column reoder

--- application/views/instantiations/index.php
+++ application/views/instantiations/index.php
@@ -131,10 +131,11 @@ if (!$isAjax)
         orderString='';
         $(function() {
             oTable = $('#instantiation_table').dataTable({
-                "sDom": 'RC<"clear">lfrtip',
-//                "aoColumnDefs": [
-//                    //                    { "bVisible": false, "aTargets": [ 1 ] }
-//                ],
+//                "sDom": 'RC<"clear">lfrtip',
+                "sDom": 'Rlfrtip',
+                "aoColumnDefs": [
+                    //                    { "bVisible": false, "aTargets": [ 1 ] }
+                ],
                 "oColReorder": {
                     "aiOrder": [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
                     "iFixedColumns": 1,

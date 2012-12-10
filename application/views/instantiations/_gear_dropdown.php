99be9ff579097a937d45bc93b35c4a1750d6637b
aliraza001 <ali.raza@pureloigcs.net>
2012-12-10 20:31:25 +0500

    Apply checks to maintain last state

--- application/views/instantiations/_gear_dropdown.php
+++ application/views/instantiations/_gear_dropdown.php
@@ -113,7 +113,7 @@
             "bSort": false,
             "sScrollY": 400,
             "sScrollX": "100%",
-	        "sScrollXInner": "120%",	
+	        "sScrollXInner": "200%",	
 			"bScrollCollapse": true,
 //        "bScrollInfinite": true,
             "bDeferRender": true,

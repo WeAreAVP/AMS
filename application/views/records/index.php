8eff16e6058759f1ad6c5a6af8067d395fc2c8e7
aliraza001 <ali.raza@pureloigcs.net>
2012-12-10 20:54:36 +0500

    Apply checks to maintain last state

--- application/views/records/index.php
+++ application/views/records/index.php
@@ -101,8 +101,8 @@ if (!$isAjax)
 		}?>
    </table>
   </div>
-   <div style="width: 865px;overflow: hidden;display:<?php if($current_tab=='full_table'){ echo 'block';}else{echo "none"; }?>;" id="full_table_view" >
-   	<table class="table table-bordered" id="listing_table" style="margin-top:0px;margin-left: 1px;margin-bottom: 0px;"  ><?php 
+   <div style="display:<?php if($current_tab=='full_table'){ echo 'block';}else{echo "none"; }?>;" id="full_table_view" >
+   	<table id="listing_table" ><?php 
 		if(isset($records) && ($total>0))
 		{
 			?>

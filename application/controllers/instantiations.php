edf71d62509d157d575cf17de0bb61e71714f7e8
Nouman Tayyab <nouman@geekschicago.com>
2012-12-10 14:10:33 +0500

    reorder

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -150,7 +150,9 @@ class Instantiations extends MY_Controller
             $user_id = $this->user_id;
             $settings = $this->input->post('settings');
             $freeze_columns = $this->input->post('frozen_column');
-            $data = array('view_settings' => json_encode($settings), 'frozen_column' => $freeze_columns);
+            echo '<pre>';print_r($settings);
+            
+//            $data = array('view_settings' => json_encode($settings), 'frozen_column' => $freeze_columns);
             $this->user_settings->update_setting($user_id, 'instantiation', $data);
             echo json_decode(array('success' => true));
             exit;

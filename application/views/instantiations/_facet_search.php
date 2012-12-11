7cc37830a10544853b14dca61990b6240df6bbb7
aliraza001 <ali.raza@pureloigcs.net>
2012-12-11 15:08:23 +0500

    Fix header changes for simple table

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -797,9 +797,52 @@
             </div>
         <?php } ?>
         <!-- Event Type End      -->
+        <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+            	<i class="icon-calendar icon-large"></i>
+        	<span></span> <b class="caret" style="margin-top: 8px"></b>
+        </div>
     </form>
 </div>
 <script type="text/javascript">
+ $(document).ready(function() {
+                  $('#reportrange').daterangepicker(
+                     {
+                        ranges: {
+                           'Today': ['today', 'today'],
+                           'Yesterday': ['yesterday', 'yesterday'],
+                           'Last 7 Days': [Date.today().add({ days: -6 }), 'today'],
+                           'Last 30 Days': [Date.today().add({ days: -29 }), 'today'],
+                           'This Month': [Date.today().moveToFirstDayOfMonth(), Date.today().moveToLastDayOfMonth()],
+                           'Last Month': [Date.today().moveToFirstDayOfMonth().add({ months: -1 }), Date.today().moveToFirstDayOfMonth().add({ days: -1 })]
+                        },
+                        opens: 'left',
+                        format: 'MM/dd/yyyy',
+                        separator: ' to ',
+                        startDate: Date.today().add({ days: -29 }),
+                        endDate: Date.today(),
+                        minDate: '01/01/2012',
+                        maxDate: '<?php echo date("mm/dd/YY");?>',
+                        locale: {
+                            applyLabel: 'Submit',
+                            fromLabel: 'From',
+                            toLabel: 'To',
+                            customRangeLabel: 'Custom Range',
+                            daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+                            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                            firstDay: 1
+                        },
+                        showWeekNumbers: true,
+                        buttonClasses: ['btn-danger']
+                     }, 
+                     function(start, end) {
+                        $('#reportrange span').html(start.toString('MMMM d, yyyy') + ' - ' + end.toString('MMMM d, yyyy'));
+                     }
+                  );
+
+                  //Set the initial state of the picker label
+                  $('#reportrange span').html(Date.today().add({ days: -29 }).toString('MMMM d, yyyy') + ' - ' + Date.today().toString('MMMM d, yyyy'));
+
+               });
     function add_token(name,type,isRemoved){
         if(type=='keyword_field_main'){
             

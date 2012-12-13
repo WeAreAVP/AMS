a0a5f00b992b12c141d23356aeac4b50e094ef8b
aliraza001 <ali.raza@pureloigcs.net>
2012-12-13 18:47:23 +0500

    Column freezing debuging

--- application/views/records/index.php
+++ application/views/records/index.php
@@ -130,10 +130,10 @@ if (!$isAjax)
 
                                             if (!($this->frozen_column > $key))
                                                 $class = 'drap-drop';
-                                            if (in_array($type, array("AA_GUID", "Local_ID", "Titles_Type", "Titles_Ref", "Titles_Source", "Description_Type", "Subjects", "Subjects_Ref", "Subjects_Source", "Genre", "Genre_Source", "Genre_Ref", "Creator_Name", "Creator_Affiliation", "Creator_Source", "Creator_Ref", "Creator_Role", "Creator_Role_Source", "Contributor_Name", "Contributor_Affiliation", "Contributor_Source", "Contributor_Ref", "Contributor_Role", "Contributor_Role_Source", "Contributor_Role_Ref", "Publisher_Name", "Publisher_Affiliation", "Publisher_Ref", "Publisher_Role", "Publisher_Role_Source", "Publisher_Role_Source_Ref", "Assets_Date", "Date_Type", "Coverage", "Coverage_Type", "Audience_Level", "Audience_Level_Source", "Audience_Level_Ref", "Audience_Rating", "Audience_Rating_Source", "Audience_Rating_Ref", "Annotation", "Annotation_Type", "Annotation_Ref", "Rights", "Rights_Link")))
+                                            if (in_array($type, array( "Local_ID", "Titles_Type", "Titles_Ref", "Titles_Source", "Description_Type", "Subjects", "Subjects_Ref", "Subjects_Source", "Genre", "Genre_Source", "Genre_Ref", "Creator_Name", "Creator_Affiliation", "Creator_Source", "Creator_Ref", "Creator_Role", "Creator_Role_Source", "Contributor_Name", "Contributor_Affiliation", "Contributor_Source", "Contributor_Ref", "Contributor_Role", "Contributor_Role_Source", "Contributor_Role_Ref", "Publisher_Name", "Publisher_Affiliation", "Publisher_Ref", "Publisher_Role", "Publisher_Role_Source", "Publisher_Role_Source_Ref", "Assets_Date", "Date_Type", "Coverage", "Coverage_Type", "Audience_Level", "Audience_Level_Source", "Audience_Level_Ref", "Audience_Rating", "Audience_Rating_Source", "Audience_Rating_Ref", "Annotation", "Annotation_Type", "Annotation_Ref", "Rights", "Rights_Link")))
                                             {
                                                 $width = 'min-width:100px;';
-                                            } else if ($type == 'Titles' || $type == 'Description')
+                                            } else if ($type == 'Titles' || $type == 'Description' || $type =="AA_GUID")
                                             {
                                                 $width = 'min-width:150px;';
                                             }
@@ -209,10 +209,10 @@ if (!$isAjax)
                                         }
                                         $column = $des;
                                     }
-                                    if (in_array($type, array("AA_GUID", "Local_ID", "Titles_Type", "Titles_Ref", "Titles_Source", "Description_Type", "Subjects", "Subjects_Ref", "Subjects_Source", "Genre", "Genre_Source", "Genre_Ref", "Creator_Name", "Creator_Affiliation", "Creator_Source", "Creator_Ref", "Creator_Role", "Creator_Role_Source", "Contributor_Name", "Contributor_Affiliation", "Contributor_Source", "Contributor_Ref", "Contributor_Role", "Contributor_Role_Source", "Contributor_Role_Ref", "Publisher_Name", "Publisher_Affiliation", "Publisher_Ref", "Publisher_Role", "Publisher_Role_Source", "Publisher_Role_Source_Ref", "Assets_Date", "Date_Type", "Coverage", "Coverage_Type", "Audience_Level", "Audience_Level_Source", "Audience_Level_Ref", "Audience_Rating", "Audience_Rating_Source", "Audience_Rating_Ref", "Annotation", "Annotation_Type", "Annotation_Ref", "Rights", "Rights_Link")))
+                                    if (in_array($type, array( "Local_ID", "Titles_Type", "Titles_Ref", "Titles_Source", "Description_Type", "Subjects", "Subjects_Ref", "Subjects_Source", "Genre", "Genre_Source", "Genre_Ref", "Creator_Name", "Creator_Affiliation", "Creator_Source", "Creator_Ref", "Creator_Role", "Creator_Role_Source", "Contributor_Name", "Contributor_Affiliation", "Contributor_Source", "Contributor_Ref", "Contributor_Role", "Contributor_Role_Source", "Contributor_Role_Ref", "Publisher_Name", "Publisher_Affiliation", "Publisher_Ref", "Publisher_Role", "Publisher_Role_Source", "Publisher_Role_Source_Ref", "Assets_Date", "Date_Type", "Coverage", "Coverage_Type", "Audience_Level", "Audience_Level_Source", "Audience_Level_Ref", "Audience_Rating", "Audience_Rating_Source", "Audience_Rating_Ref", "Annotation", "Annotation_Type", "Annotation_Ref", "Rights", "Rights_Link")))
                                     {
                                         $width = 'min-width:100px;';
-                                    } else if ($type == 'Titles' || $type == 'Description')
+                                    } else if ($type == 'Titles' || $type == 'Description' || $type =="AA_GUID")
                                     {
                                         $width = 'min-width:150px;';
                                     }

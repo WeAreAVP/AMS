876b03db9027a61dadd5c8e647eecc344fd1a2d5
Nouman Tayyab <nouman@geekschicago.com>
2012-12-13 17:53:11 +0500

    facet search date range design

--- application/views/messages/_materials_received_digitization_vendor.php
+++ application/views/messages/_materials_received_digitization_vendor.php
@@ -14,7 +14,19 @@
         </div>
     </div>
 </div>
+<div class="modal hide" id="error_window" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3>Tracking Information</h3>
+    </div>
+    <div class="modal-body">
+        One or more stations don't have their tracking information and no media received date.
+    </div>
+    <div class="modal-footer">
+        <button class="btn btn-primary" data-dismiss="modal" aria-hidden="true" data-dismiss="modal">Close</button>
 
+    </div>
+</div>
 <script type="text/javascript">
     $(function() {
         //        console.log(to);
--- application/views/stations/_messages.php
+++ application/views/stations/_messages.php
@@ -16,13 +16,13 @@
 <div class="modal hide" id="error_window" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3>Error</h3>
+        <h3>Tracking Information</h3>
     </div>
     <div class="modal-body">
-        One or more stations don't have their tracking information.
+        One or more stations don't have their tracking information and no media received date.
     </div>
     <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true" data-dismiss="modal">Hide</button>
+        <button class="btn btn-primary" data-dismiss="modal" aria-hidden="true" data-dismiss="modal">Close</button>
 
     </div>
 </div>

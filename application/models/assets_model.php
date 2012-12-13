64f43774549b16636cc269aaa6290c807810f61e
Nouman Tayyab <nouman@geekschicago.com>
2012-12-13 17:04:29 +0500

    facet search date range design

--- application/models/assets_model.php
+++ application/models/assets_model.php
@@ -290,11 +290,8 @@ class Assets_Model extends CI_Model
 								LEFT JOIN {$this->stations} ON {$this->stations}.id = {$this->_assets_table}.stations_id
 						AND assets.id='".$asset_id."'";
 		$res=$this->db->query($sql);
-        echo '<pre>';print_r($res);exit;
-		if(isset($res) && !empty($res))
-		{
-			return $res->row();
-		}return false;
+        return $res->row();
+		
     }
 	/**
 	* Get asset by asset_id

d2cb4bbdf4503bea8a4d445efe6bb918b5a8625d
Nouman Tayyab <nouman@geekschicago.com>
2012-12-20 11:29:46 +0500

    table view changes

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -102,6 +102,7 @@ class	Instantiations	extends	MY_Controller
 								$config['total_rows']	=	$data['total'];
 								$config['per_page']	=	100;
 								$data['records']	=	$records['records'];
+								echo '<pre>';print_r($data['records']);exit;
 								$data['count']	=	count	($data['records']);
 								if	($data['count']	>	0	&&	$offset	===	0)
 								{

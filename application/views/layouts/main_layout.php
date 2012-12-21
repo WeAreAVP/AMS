b5a6ecb94a8446229fae6ff862095b3c92a1fc8a
Nouman Tayyab <nouman@geekschicago.com>
2012-12-21 18:15:39 +0500

    instantiations changes

--- application/views/layouts/main_layout.php
+++ application/views/layouts/main_layout.php
@@ -154,7 +154,7 @@
 																								if($this->can_compose_alert)
 																								{
 																												?>
-																												<li class="<?php	echo	active_anchor('templatemanager',	array('add',	'lists',	'edit',	'details',	'readmessage',	'manage_crawford'));	?>"><a href="<?php	echo	site_url('templatemanager/lists');	?>" >Email Template</a></li>
+																												<li class="<?php	echo	active_anchor('templatemanager',	array('add',	'lists',	'edit',	'details',	'readmessage'));	?>"><a href="<?php	echo	site_url('templatemanager/lists');	?>" >Email Template</a></li>
 																								<?php	}	?>
 																								<li class="<?php	echo	active_anchor('templatemanager',	'manage_crawford');	?>"><a href="<?php	echo	site_url('templatemanager/manage_crawford');	?>">Crawford Contact Detail</a></li> 
 																								<li class="<?php	echo	active_anchor('settings',	'index');	?>"><a href="<?php	echo	site_url('settings/index');	?>">Users</a></li>

eaeb0b72d7557c948506a22f6dcc0c038676a6e9
Nouman Tayyab <nouman@geekschicago.com>
2012-12-26 18:38:56 +0500

    sphnix changes

--- application/controllers/messages.php
+++ application/controllers/messages.php
@@ -1,7 +1,7 @@
 <?php
 
-if	(!defined	('BASEPATH'))
-				exit	('No direct script access allowed');
+if(	!	defined('BASEPATH'))
+				exit('No direct script access allowed');
 
 /**
 	* Messages
@@ -17,9 +17,9 @@ class	Messages	extends	MY_Controller
 					* constructor. Load layout,Model,Library and helpers
 					* 
 					*/
-				function	__construct	()
+				function	__construct()
 				{
-								parent::__construct	();
+								parent::__construct();
 								$this->layout	=	'main_layout.php';
 				}
 
@@ -27,41 +27,41 @@ class	Messages	extends	MY_Controller
 					* Redirect to inbox
 					*  
 					*/
-				public	function	index	()
+				public	function	index()
 				{
-								redirect	('messages/inbox',	'location');
+								redirect('messages/inbox',	'location');
 				}
 
 				/**
 					* List Received Message 
 					*  
 					*/
-				public	function	inbox	()
+				public	function	inbox()
 				{
 								$where	=	'';
-								if	($_POST)
+								if($_POST)
 								{
-												if	($_POST['message_type'])
+												if(isset($_POST['message_type']))
 												{
 																$where['msg_type']	=	$_POST['message_type'];
 												}
-												if	(isset($_POST['stations']))
+												if(isset($_POST['stations']))
 												{
 																$where['receiver_id']	=	$_POST['stations'];
 												}
 								}
-								$data['results']	=	$this->msgs->get_inbox_msgs	($this->station_id,	$where);
-								$data['station_records']	=	$this->station_model->get_all	();
-								if	(isAjax	())
+								$data['results']	=	$this->msgs->get_inbox_msgs($this->station_id,	$where);
+								$data['station_records']	=	$this->station_model->get_all();
+								if(isAjax())
 								{
 												$data['is_ajax']	=	true;
-												echo	$this->load->view	('messages/inbox',	$data,	true);
+												echo	$this->load->view('messages/inbox',	$data,	true);
 												exit;
 								}
 								else
 								{
 												$data['is_ajax']	=	false;
-												$this->load->view	('messages/inbox',	$data);
+												$this->load->view('messages/inbox',	$data);
 								}
 				}
 
@@ -69,39 +69,39 @@ class	Messages	extends	MY_Controller
 					* List Sent Message 
 					*  
 					*/
-				public	function	sent	()
+				public	function	sent()
 				{
-								if	(in_array	($this->role_id,	array	(1,	2,	5)))
+								if(in_array($this->role_id,	array(1,	2,	5)))
 								{
 												$where	=	'';
-												if	($_POST)
+												if($_POST)
 												{
-																if	($_POST['message_type'])
+																if($_POST['message_type'])
 																{
 																				$where['msg_type']	=	$_POST['message_type'];
 																}
-																if	($_POST['stations'])
+																if($_POST['stations'])
 																{
 																				$where['receiver_id']	=	$_POST['stations'];
 																}
 												}
-												$data['station_records']	=	$this->station_model->get_all	();
-												$data['results']	=	$this->msgs->get_sent_msgs	($this->user_id,	$where);
-												if	(isAjax	())
+												$data['station_records']	=	$this->station_model->get_all();
+												$data['results']	=	$this->msgs->get_sent_msgs($this->user_id,	$where);
+												if(isAjax())
 												{
 																$data['is_ajax']	=	true;
-																echo	$this->load->view	('messages/sent',	$data,	true);
+																echo	$this->load->view('messages/sent',	$data,	true);
 																exit;
 												}
 												else
 												{
 																$data['is_ajax']	=	false;
-																$this->load->view	('messages/sent',	$data);
+																$this->load->view('messages/sent',	$data);
 												}
 								}
 								else
 								{
-												show_404	();
+												show_404();
 								}
 				}
 
@@ -112,20 +112,20 @@ class	Messages	extends	MY_Controller
 					* @return html view for message type
 					*  
 					*/
-				public	function	get_message_type	()
+				public	function	get_message_type()
 				{
-								if	(isAjax	())
+								if(isAjax())
 								{
-												$type	=	$this->input->post	('type');
-												$messagesType	=	$this->config->item	('messages_type');
-												$messageType	=	'_'	.	str_replace	(' ',	'_',	strtolower	($messagesType[$type]));
-												$messageTypeFields	=	str_replace	(' ',	'_',		$messagesType[$type]);
-												$data['record']=$this->email_template->get_template_by_sys_id($messageTypeFields);
+												$type	=	$this->input->post('type');
+												$messagesType	=	$this->config->item('messages_type');
+												$messageType	=	'_'	.	str_replace(' ',	'_',	strtolower($messagesType[$type]));
+												$messageTypeFields	=	str_replace(' ',	'_',	$messagesType[$type]);
+												$data['record']	=	$this->email_template->get_template_by_sys_id($messageTypeFields);
 												$data['is_ajax']	=	true;
-												echo	$this->load->view	('messages/'	.	$messageType,	$data,	TRUE);
+												echo	$this->load->view('messages/'	.	$messageType,	$data,	TRUE);
 												exit;
 								}
-								show_404	();
+								show_404();
 				}
 
 				/**
@@ -138,41 +138,41 @@ class	Messages	extends	MY_Controller
 					* @param $extaras receive the remaining fields as an array
 					*  
 					*/
-				public	function	compose	()
+				public	function	compose()
 				{
-								if	($this->input->post	()	&&	isAjax	())
+								if($this->input->post()	&&	isAjax())
 								{
-												$alerts_array	=	$this->config->item	('messages_type');
-												$html	=	$this->input->post	('html');
-												$type	=	$this->input->post	('type');
-												$same_dsd	=	($this->input->post	('same_dsd'))	?	$this->input->post	('same_dsd')	:	false;
-												$template	=	str_replace	(" ",	"_",	$alerts_array[$type]);
-												$template_data	=	$this->email_template->get_template_by_sys_id	($template);
-												$multiple_station	=	$this->input->post	('to');
-												$extra	=	$this->input->post	('extras');
-												if	(isset	($template_data)	&&	!empty	($template_data))
+												$alerts_array	=	$this->config->item('messages_type');
+												$html	=	$this->input->post('html');
+												$type	=	$this->input->post('type');
+												$same_dsd	=	($this->input->post('same_dsd'))	?	$this->input->post('same_dsd')	:	false;
+												$template	=	str_replace(" ",	"_",	$alerts_array[$type]);
+												$template_data	=	$this->email_template->get_template_by_sys_id($template);
+												$multiple_station	=	$this->input->post('to');
+												$extra	=	$this->input->post('extras');
+												if(isset($template_data)	&&	!	empty($template_data))
 												{
-																if	(isset	($multiple_station)	&&	!empty	($multiple_station))
+																if(isset($multiple_station)	&&	!	empty($multiple_station))
 																{
-																				$this->compose_station_message	($multiple_station,	$template_data,	$extra,	$template,	$type);
-																				echo	json_encode	(array	('success'	=>	TRUE));
+																				$this->compose_station_message($multiple_station,	$template_data,	$extra,	$template,	$type);
+																				echo	json_encode(array('success'	=>	TRUE));
 																				exit;
 																}
 																else
 																{
-																				echo	json_encode	(array	('success'		=>	FALSE,	"error_id"	=>	1));
+																				echo	json_encode(array('success'		=>	FALSE,	"error_id"	=>	1));
 																				exit;
 																}
 												}
 												else
 												{
-																echo	json_encode	(array	('success'		=>	FALSE,	"error_id"	=>	2));
+																echo	json_encode(array('success'		=>	FALSE,	"error_id"	=>	2));
 																exit;
 												}
 								}
 								else
 								{
-												show_404	();
+												show_404();
 								}
 				}
 
@@ -184,26 +184,26 @@ class	Messages	extends	MY_Controller
 					* @param string $template
 					* @param integer $type 
 					*/
-				function	compose_station_message	($multiple_station,	$template_data,	$extra,	$template,	$type)
+				function	compose_station_message($multiple_station,	$template_data,	$extra,	$template,	$type)
 				{
 								$material_received_date	=	0;
-								foreach	($multiple_station	as	$to)
+								foreach($multiple_station	as	$to)
 								{
 
-												$station_details	=	$this->station_model->get_station_by_id	($to);
+												$station_details	=	$this->station_model->get_station_by_id($to);
 												$subject	=	$template_data->subject;
 
-												if	($template	==	'Digitization_Start_Date')
+												if($template	==	'Digitization_Start_Date')
 												{
 																$extra['ship_date']	=	$station_details->start_date;
 												}
-												else	if	($template	==	'Materials_Received_Digitization_Vendor')
+												else	if($template	==	'Materials_Received_Digitization_Vendor')
 												{
 
-																$tracking_info	=	$this->tracking->get_last_tracking_info	($to);
-																if	(count	($tracking_info)	>	0)
+																$tracking_info	=	$this->tracking->get_last_tracking_info($to);
+																if(count($tracking_info)	>	0)
 																{
-																				if	(empty	($tracking_info->media_received_date)	||	$tracking_info->media_received_date	==	null)
+																				if(empty($tracking_info->media_received_date)	||	$tracking_info->media_received_date	==	null)
 																				{
 																								$material_received_date	=	1;
 																				}
@@ -214,18 +214,18 @@ class	Messages	extends	MY_Controller
 																				}
 																}
 												}
-												if	($material_received_date	==	0)
+												if($material_received_date	==	0)
 												{
-																foreach	($extra	as	$key	=>	$value)
+																foreach($extra	as	$key	=>	$value)
 																{
-																				$replacebale[$key]	=	(isset	($value)	&&	!empty	($value))	?	$value	:	'';
+																				$replacebale[$key]	=	(isset($value)	&&	!	empty($value))	?	$value	:	'';
 																}
 
-																$replacebale['station_name']	=	isset	($station_details->station_name)	?	$station_details->station_name	:	'';
-																if	($this->config->item	('demo')	==	TRUE)
+																$replacebale['station_name']	=	isset($station_details->station_name)	?	$station_details->station_name	:	'';
+																if($this->config->item('demo')	==	TRUE)
 																{
-																				$to_email	=	$this->config->item	('to_email');
-																				$from_email	=	$this->config->item	('from_email');
+																				$to_email	=	$this->config->item('to_email');
+																				$from_email	=	$this->config->item('from_email');
 																				$replacebale['user_name']	=	'AMS';
 																				$this->emailtemplates->sent_now	=	TRUE;
 																}
@@ -236,15 +236,15 @@ class	Messages	extends	MY_Controller
 																				$replacebale['user_name']	=	$this->user_detail->first_name	.	' '	.	$this->user_detail->last_name;
 																}
 																$replacebale['inform_to']	=	'ssapienza@cpb.org';
-																$email_queue_id	=	$this->emailtemplates->queue_email	($template,	$to_email,	$replacebale);
+																$email_queue_id	=	$this->emailtemplates->queue_email($template,	$to_email,	$replacebale);
 
-																$data	=	array	('sender_id'			=>	$this->user_id,	'receiver_id'	=>	$to,	'msg_type'				=>	$type,	'subject'					=>	$subject,	'msg_extras'		=>	json_encode	($extra),	'created_at'		=>	date	('Y-m-d h:m:i'));
-																if	(isset	($email_queue_id)	&&	$email_queue_id)
+																$data	=	array('sender_id'			=>	$this->user_id,	'receiver_id'	=>	$to,	'msg_type'				=>	$type,	'subject'					=>	$subject,	'msg_extras'		=>	json_encode($extra),	'created_at'		=>	date('Y-m-d h:m:i'));
+																if(isset($email_queue_id)	&&	$email_queue_id)
 																{
 																				$data['email_queue_id']	=	$email_queue_id;
 																}
-																$this->msgs->add_msg	($data);
-																$this->session->set_userdata	('sent',	'Message Sent');
+																$this->msgs->add_msg($data);
+																$this->session->set_userdata('sent',	'Message Sent');
 												}
 								}
 				}
@@ -255,73 +255,73 @@ class	Messages	extends	MY_Controller
 					* @param $message_id msg id
 					*  Display Message details
 					*/
-				public	function	readmessage	($message_id	=	'')
+				public	function	readmessage($message_id	=	'')
 				{
-								if	(isAjax	())
+								if(isAjax())
 								{
-												$rslt["total_unread_text"]	=	'<a href="'	.	site_url	('messages/inbox')	.	'">Messages</a>';
+												$rslt["total_unread_text"]	=	'<a href="'	.	site_url('messages/inbox')	.	'">Messages</a>';
 												$rslt["error"]	=	true;
 												$rslt["reset_row"]	=	false;
-												if	($message_id	!=	'')
+												if($message_id	!=	'')
 												{
-																$data['result']	=	$this->msgs->get_inbox_msgs	($this->station_id,	array	("id"	=>	$message_id));
-																if	(isset	($data['result'])	&&	!empty	($data['result'])	&&	$data['result'][0]->msg_status	==	'unread'	&&	!$this->can_compose_alert)
+																$data['result']	=	$this->msgs->get_inbox_msgs($this->station_id,	array("id"	=>	$message_id));
+																if(isset($data['result'])	&&	!	empty($data['result'])	&&	$data['result'][0]->msg_status	==	'unread'	&&	!	$this->can_compose_alert)
 																{
-																				$this->msgs->update_msg_by_id	($message_id,	array	("msg_status"	=>	'read',	"read_at"				=>	date	('Y-m-d H:i:s')));
-																				$this->total_unread	=	$this->msgs->get_unread_msgs_count	($this->user_id);
-																				if	(isset	($this->total_unread)	&&	$this->total_unread	>	0	&&	$this->is_station_user)
+																				$this->msgs->update_msg_by_id($message_id,	array("msg_status"	=>	'read',	"read_at"				=>	date('Y-m-d H:i:s')));
+																				$this->total_unread	=	$this->msgs->get_unread_msgs_count($this->user_id);
+																				if(isset($this->total_unread)	&&	$this->total_unread	>	0	&&	$this->is_station_user)
 																				{
-																								$rslt["total_unread_text"]	=	'<a class="btn large message" href="'	.	site_url	('messages/inbox')	.	'">Messages<span class="badge label-important message-alert">'	.	$this->total_unread	.	'</span></a>';
+																								$rslt["total_unread_text"]	=	'<a class="btn large message" href="'	.	site_url('messages/inbox')	.	'">Messages<span class="badge label-important message-alert">'	.	$this->total_unread	.	'</span></a>';
 																								$rslt["reset_row"]	=	true;
 																				}
 																}
 																$rslt["error"]	=	false;
-																$rslt["msg_data"]	=	$this->load->view	('messages/read_msg',	$data,	true);
-																echo	json_encode	($rslt);
+																$rslt["msg_data"]	=	$this->load->view('messages/read_msg',	$data,	true);
+																echo	json_encode($rslt);
 																exit;
 												}
 												else
 												{
-																echo	json_encode	($rslt);
+																echo	json_encode($rslt);
 																exit;
 												}
 								}
 								else
 								{
-												show_404	();
+												show_404();
 								}
 				}
 
-				public	function	readsentmessage	($message_id	=	'')
+				public	function	readsentmessage($message_id	=	'')
 				{
-								if	(isAjax	())
+								if(isAjax())
 								{
 												$rslt["error"]	=	true;
-												if	($this->can_compose_alert)
+												if($this->can_compose_alert)
 												{
-																if	($message_id	!=	'')
+																if($message_id	!=	'')
 																{
-																				$data['result']	=	$this->msgs->get_sent_msgs	($this->user_id,	array	("id"														=>	$message_id));
+																				$data['result']	=	$this->msgs->get_sent_msgs($this->user_id,	array("id"														=>	$message_id));
 																				$rslt["error"]	=	false;
-																				$rslt["msg_data"]	=	$this->load->view	('messages/read_msg',	$data,	true);
-																				echo	json_encode	($rslt);
+																				$rslt["msg_data"]	=	$this->load->view('messages/read_msg',	$data,	true);
+																				echo	json_encode($rslt);
 																				exit;
 																}
 																else
 																{
-																				echo	json_encode	($rslt);
+																				echo	json_encode($rslt);
 																				exit;
 																}
 												}
 												else
 												{
-																echo	json_encode	($rslt);
+																echo	json_encode($rslt);
 																exit;
 												}
 								}
 								else
 								{
-												show_404	();
+												show_404();
 								}
 				}
 
--- application/views/settings/edit_user.php
+++ application/views/settings/edit_user.php
@@ -1,28 +1,28 @@
 <?php
-$email = array(
-    'name' => 'email',
-    'id' => 'email',
-    'value' => $user_info->email,
+$email	=	array(
+				'name'				=>	'email',
+				'id'						=>	'email',
+				'value'			=>	$user_info->email,
 );
-$password = array(
-    'name' => 'password',
-    'id' => 'password',
-    'value' => set_value('password'),
+$password	=	array(
+				'name'						=>	'password',
+				'id'								=>	'password',
+				'value'					=>	set_value('password'),
 );
-$first_name = array(
-    'name' => 'first_name',
-    'id' => 'first_name',
-    'value' => $user_info->first_name,
+$first_name	=	array(
+				'name'					=>	'first_name',
+				'id'							=>	'first_name',
+				'value'				=>	$user_info->first_name,
 );
-$last_name = array(
-    'name' => 'last_name',
-    'id' => 'last_name',
-    'value' => $user_info->last_name,
+$last_name	=	array(
+				'name'				=>	'last_name',
+				'id'						=>	'last_name',
+				'value'			=>	$user_info->last_name,
 );
-$phone_no = array(
-    'name' => 'phone_no',
-    'id' => 'phone_no',
-    'value' => $user_info->phone_no,
+$phone_no	=	array(
+				'name'		=>	'phone_no',
+				'id'				=>	'phone_no',
+				'value'	=>	$user_info->phone_no,
 );
 
 //$title = array(
@@ -30,109 +30,130 @@ $phone_no = array(
 //    'id' => 'title',
 //    'value' => $user_info->title,
 //);
-$fax = array(
-    'name' => 'fax',
-    'id' => 'fax',
-    'value' => $user_info->fax,
+$fax	=	array(
+				'name'			=>	'fax',
+				'id'					=>	'fax',
+				'value'		=>	$user_info->fax,
 );
-$address = array(
-    'name' => 'address',
-    'id' => 'address',
-    'value' => $user_info->address,
+$address	=	array(
+				'name'		=>	'address',
+				'id'				=>	'address',
+				'value'	=>	$user_info->address,
 );
-$role = array(
-    'name' => 'role',
-    'id' => 'role',
-    'value' => $user_info->role_id,
+$role	=	array(
+				'name'			=>	'role',
+				'id'					=>	'role',
+				'value'		=>	$user_info->role_id,
 );
-$station = array(
-    'name' => 'station',
-    'id' => 'station',
-    'value' => $user_info->station_id,
+$station	=	array(
+				'name'						=>	'station',
+				'id'								=>	'station',
+				'value'					=>	$user_info->station_id,
 );
-$attributes=null;
-if(!isset($profile_edit))
-$attributes = array('onsubmit' => 'return false;', 'id' => 'edit_from');
+$attributes	=	null;
+if(	!	isset($profile_edit))
+				$attributes	=	array('onsubmit'	=>	'return false;',	'id'							=>	'edit_from');
 ?>
 
 <center>
-    <?php echo form_open_multipart($this->uri->uri_string(), $attributes); ?>
+				<?php	echo	form_open_multipart($this->uri->uri_string(),	$attributes);	?>
     <table class="table no_border">
 
 
 
         <tr>
-            <td class="_label"><?php echo form_label('Email:', $email['id']); ?></td>
-            <td><?php echo form_input($email); ?><span style="color: red;"><?php echo form_error($email['name']); ?></span></td>
+            <td class="_label"><?php	echo	form_label('Email:',	$email['id']);	?></td>
+            <td><?php	echo	form_input($email);	?><span style="color: red;"><?php	echo	form_error($email['name']);	?></span></td>
         </tr>
 
 
         <tr>
-            <td class="_label"><?php echo form_label('Password:', $password['id']); ?></td>
-            <td><?php echo form_password($password); ?><div style="clear: both;"></div>Leave blank if you not want to change<span style="color: red;"><?php echo form_error($password['name']); ?></span></td>
+            <td class="_label"><?php	echo	form_label('Password:',	$password['id']);	?></td>
+            <td><?php	echo	form_password($password);	?><div style="clear: both;"></div>Leave blank if you not want to change<span style="color: red;"><?php	echo	form_error($password['name']);	?></span></td>
 
         </tr>
 
 
         <tr>
-            <td class="_label""><?php echo form_label('First Name:', $first_name['id']); ?></td>
-            <td><?php echo form_input($first_name); ?><span style="color: red;"><?php echo form_error($first_name['name']); ?></span></td>
+            <td class="_label""><?php	echo	form_label('First Name:',	$first_name['id']);	?></td>
+            <td><?php	echo	form_input($first_name);	?><span style="color: red;"><?php	echo	form_error($first_name['name']);	?></span></td>
         </tr>
 
 
 
 
         <tr>
-            <td class="_label"><?php echo form_label('Last Name:', $last_name['id']); ?></td>
-            <td><?php echo form_input($last_name); ?><span style="color: red;"><?php echo form_error($last_name['name']); ?></span></td>
+            <td class="_label"><?php	echo	form_label('Last Name:',	$last_name['id']);	?></td>
+            <td><?php	echo	form_input($last_name);	?><span style="color: red;"><?php	echo	form_error($last_name['name']);	?></span></td>
         </tr>
 
 
         <tr>
-            <td class="_label"><?php echo form_label('Phone #:', $phone_no['id']); ?></td>
-            <td><?php echo form_input($phone_no); ?><span style="color: red;"><?php echo form_error($phone_no['name']); ?></span></td>
+            <td class="_label"><?php	echo	form_label('Phone #:',	$phone_no['id']);	?></td>
+            <td><?php	echo	form_input($phone_no);	?><span style="color: red;"><?php	echo	form_error($phone_no['name']);	?></span></td>
         </tr>
 <!--        <tr>
-            <td class="_label"><?php echo form_label('Title:', $title['id']); ?></td>
-            <td><?php echo form_input($title); ?><span style="color: red;"><?php echo form_error($title['name']); ?></span></td>
+            <td class="_label"><?php	echo	form_label('Title:',	$title['id']);	?></td>
+            <td><?php	echo	form_input($title);	?><span style="color: red;"><?php	echo	form_error($title['name']);	?></span></td>
         </tr>-->
         <tr>
-            <td class="_label"><?php echo form_label('Fax :', $fax['id']); ?></td>
-            <td><?php echo form_input($fax); ?><span style="color: red;"><?php echo form_error($fax['name']); ?></span></td>
+            <td class="_label"><?php	echo	form_label('Fax :',	$fax['id']);	?></td>
+            <td><?php	echo	form_input($fax);	?><span style="color: red;"><?php	echo	form_error($fax['name']);	?></span></td>
         </tr>
         <tr>
-            <td class="_label"><?php echo form_label('Address:', $address['id']); ?></td>
-            <td><?php echo form_input($address); ?><span style="color: red;"><?php echo form_error($address['name']); ?></span></td>
+            <td class="_label"><?php	echo	form_label('Address:',	$address['id']);	?></td>
+            <td><?php	echo	form_input($address);	?><span style="color: red;"><?php	echo	form_error($address['name']);	?></span></td>
         </tr>
 
 
-        <?php if(!isset($profile_edit)){ ?>
-        <tr>
-            <td class="_label"><?php echo form_label('Role:', $role['id']); ?></td>
-            <td><?php echo form_dropdown($role['id'], $roles, array($user_info->role_id),'id="role" onchange="checkRole();"'); ?><span style="color: red;"><?php echo form_error($role['name']); ?></span></td>
-        </tr>
-        <?php } ?>
+								<?php
+								if(	!	isset($profile_edit))
+								{
+												?>
+												<tr>
+																<td class="_label"><?php	echo	form_label('Role:',	$role['id']);	?></td>
+																<td><?php	echo	form_dropdown($role['id'],	$roles,	array($user_info->role_id),	'id="role" onchange="checkRole();"');	?><span style="color: red;"><?php	echo	form_error($role['name']);	?></span></td>
+												</tr>
+								<?php	}	?>
         <tr id="station_row"  style="display: none;">
-            <td class="_label"><?php echo form_label('Station:', $station['id']); ?></td>
-            <td><?php echo form_dropdown($station['id'], $stations_list, array($user_info->station_id)); ?><span style="color: red;"><?php echo form_error($station['name']); ?></span></td>
-        </tr>
+												<?php
+												if($this->is_station_user)
+												{
+																?>
+																<td><?php	echo	form_hidden($station['name'],	$this->station_id);	?></td>
+																<?php
+												}
+												else
+												{
+																?>
+																<td class="_label"><?php	echo	form_label('Station:',	$station['id']);	?></td>
+																<td><?php	echo	form_dropdown($station['id'],	$stations_list,	array($user_info->station_id));	?><span style="color: red;"><?php	echo	form_error($station['name']);	?></span></td>
+												<?php	}	?>
+								</tr>
         <tr>
-            <?php if (isset($profile_edit)) { ?>
-            <td colspan="2">
-                <?php echo form_submit('save', 'Save', 'class="btn btn-primary"'); ?>
-            </td>
-                
-            <?php } else { ?>
-                <td colspan="2" style="text-align: right;">
+												<?php
+												if(isset($profile_edit))
+												{
+																?>
+																<td colspan="2">
+																				<?php	echo	form_submit('save',	'Save',	'class="btn btn-primary"');	?>
+																</td>
 
-                    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>&nbsp;<?php echo form_submit('save', 'Update', 'class="btn btn-primary btn-custom" onclick="manageUser(\'post\',\'edit_user/' . $user_info->id . '\');" '); ?>
+																<?php
+												}
+												else
+												{
+																?>
+																<td colspan="2" style="text-align: right;">
 
-                </td>
-            <?php } ?>
+																				<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>&nbsp;<?php	echo	form_submit('save',	'Update',	'class="btn btn-primary btn-custom" onclick="manageUser(\'post\',\'edit_user/'	.	$user_info->id	.	'\');" ');	?>
+
+																</td>
+												<?php	}	?>
         </tr>
 
 
     </table>
 
-    <?php echo form_close(); ?>
+				<?php	echo	form_close();	?>
 </center>

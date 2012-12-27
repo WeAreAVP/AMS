26aaff3241588cdeb48118e5e45f810c2e499e82
Nouman Tayyab <nouman@geekschicago.com>
2012-12-27 16:57:17 +0500

    fafa

--- application/views/instantiations/detail.php
+++ application/views/instantiations/detail.php
@@ -1,22 +1,22 @@
 <div class="row-fluid">
     <h2>Instantiation Details: <?php	echo	$asset_details->title	?></h2>
-    <div class="span12 form-row">
+   
 
 								<?php	$this->load->view('records/_list');	?>
-    </div>
+   
     <div class="span9"  style="margin-left: 285px;">
-								<div class="span12 button-after-player">
+								<div class="span9 button-after-player">
             <button class="btn btn-large"><span class="icon-pencil"></span>Edit Instantiation</button>
             <button class="btn btn-large"><span class="icon-download-alt"></span>Export Instantiation</button>
         </div>
 
 
 
-        <div class="my-navbar span12">
+        <div class="my-navbar span9">
             <div>Instantiation Details</div>
         </div>
 
-        <div class="span12 form-row">
+        <div class="span9 form-row">
             <div class="span2 form-label">
                 <label><i class="icon-question-sign"></i>* Organization:</label>
             </div>
@@ -28,7 +28,7 @@
             </div>
             <!--end of span9--> 
         </div>
-        <div class="span12 form-row">
+        <div class="span9 form-row">
             <div class="span2 form-label">
                 <label><i class="icon-question-sign"></i>* Instantiation:</label>
             </div>
@@ -60,7 +60,7 @@
 								if($instantiation_detail->media_type)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>* Media Type:</label>
 																</div>
@@ -84,7 +84,7 @@
 								if($instantiation_detail->format_type	&&	$instantiation_detail->format_type	!=	'')
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>Format:</label>
 																</div>
@@ -118,7 +118,7 @@
 								if($instantiation_detail->generation)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>Generation:</label>
 																</div>
@@ -141,7 +141,7 @@
 								if($instantiation_detail->actual_duration	||	$instantiation_detail->projected_duration)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>Duration:</label>
 																</div>
@@ -173,7 +173,7 @@
 								if($instantiation_detail->dates	||	$instantiation_detail->date_type)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>Date:</label>
 																</div>
@@ -205,7 +205,7 @@
 								if($instantiation_detail->location)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>* Location:</label>
 																</div>
@@ -227,7 +227,7 @@
 								if($instantiation_detail->file_size	||	$instantiation_detail->file_size_unit_of_measure)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>File Size:</label>
 																</div>
@@ -260,7 +260,7 @@
 								if($instantiation_detail->color)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>Color:</label>
 																</div>
@@ -281,7 +281,7 @@
 								if($instantiation_detail->language)
 								{
 												?>
-												<div class="span12 form-row">
+												<div class="span9 form-row">
 																<div class="span2 form-label">
 																				<label><i class="icon-question-sign"></i>Language:</label>
 																</div>

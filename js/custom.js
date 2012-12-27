bc6b011c5bb74d80c7a9b466b6c4d5d5be5c7b89
Nouman Tayyab <nouman@geekschicago.com>
2012-12-27 14:21:52 +0500

    add new filters in facet browse

--- js/custom.js
+++ js/custom.js
@@ -322,7 +322,7 @@ function updateDatabase()
 												table_type:current_table_type
 								},
 								success: function (result){
-											updateDataTable();
+												facet_search('0');
 								}
 				});
 }

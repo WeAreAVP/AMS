106dcff5f1c10a60fc99602f576f22724ecc0085
Nouman Tayyab <nouman@geekschicago.com>
2012-12-28 13:13:46 +0500

    menu changes

--- application/views/instantiations/detail.php
+++ application/views/instantiations/detail.php
@@ -3,7 +3,7 @@
 								<h2>Instantiation Details: <?php	echo	$asset_details->title;	?></h2>
 				</div>
     <div class="clearfix"></div>
-				<div class="span3 detail-menu">
+				<div class="span4 detail-menu">
 								<?php
 								$class	=	'';
 								if(	!	isset($inst_id)	&&	empty($inst_id))

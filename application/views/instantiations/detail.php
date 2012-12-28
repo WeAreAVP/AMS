de0b9ae26ce9bdb27ab98d5e6fb0c102dc2959a3
Nouman Tayyab <nouman@geekschicago.com>
2012-12-28 17:37:52 +0500

    menu changes

--- application/views/instantiations/detail.php
+++ application/views/instantiations/detail.php
@@ -6,110 +6,115 @@
 				<?php	$this->load->view('partials/_list');	?>
 
 				<div class="span9" style="margin-left: 285px;">
-								<table  cellPadding="8">
-												<!--				Instantiation ID	Start		-->
-												<?php
-												if($instantiation_detail->instantiation_identifier	||	$instantiation_detail->instantiation_source)
-												{
-																?>
-																<tr>
-																				<td class="record-detail-page">
-																								<label><i class="icon-question-sign"></i><b>* Instantiation ID:</b></label>
-																				</td>
-																				<td>
-																								<?php
-																								if($instantiation_detail->instantiation_identifier)
-																								{
-																												?>
-																												<span><?php	echo	$instantiation_detail->instantiation_identifier;	?></span>
+								<div style="float: left;">
+												<table  cellPadding="8">
+																<!--				Instantiation ID	Start		-->
+																<?php
+																if($instantiation_detail->instantiation_identifier	||	$instantiation_detail->instantiation_source)
+																{
+																				?>
+																				<tr>
+																								<td class="record-detail-page">
+																												<label><i class="icon-question-sign"></i><b>* Instantiation ID:</b></label>
+																								</td>
+																								<td>
 																												<?php
-																												if($instantiation_detail->instantiation_source)
+																												if($instantiation_detail->instantiation_identifier)
 																												{
 																																?>
-																																<span>	<?php	echo	' ('	.	$instantiation_detail->instantiation_source	.	')';	?></span>
+																																<span><?php	echo	$instantiation_detail->instantiation_identifier;	?></span>
 																																<?php
+																																if($instantiation_detail->instantiation_source)
+																																{
+																																				?>
+																																				<span>	<?php	echo	' ('	.	$instantiation_detail->instantiation_source	.	')';	?></span>
+																																				<?php
+																																}
 																												}
-																								}
-																								?>
-																				</td>
-																</tr>
-												<?php	}	?>
-												<!--				Instantiation ID	End		-->
-												<!--				Date 	Start		-->
-												<?php
-												if($instantiation_detail->dates	||	$instantiation_detail->date_type)
-												{
-																?>
-																<tr>
-																				<td class="record-detail-page">
-																								<label><i class="icon-question-sign"></i><b>  Date:</b></label>
-																				</td>
-																				<td>
-																								<?php
-																								if($instantiation_detail->date_type)
-																								{
 																												?>
-																												<span><?php	echo	$instantiation_detail->date_type	.	':';	?></span>
+																								</td>
+																				</tr>
+																<?php	}	?>
+																<!--				Instantiation ID	End		-->
+																<!--				Date 	Start		-->
+																<?php
+																if($instantiation_detail->dates	||	$instantiation_detail->date_type)
+																{
+																				?>
+																				<tr>
+																								<td class="record-detail-page">
+																												<label><i class="icon-question-sign"></i><b>  Date:</b></label>
+																								</td>
+																								<td>
 																												<?php
-																												if($instantiation_detail->dates)
+																												if($instantiation_detail->date_type)
 																												{
 																																?>
-																																<span><?php	echo	date('Y-m-d',	$instantiation_detail->dates);	?></span>
+																																<span><?php	echo	$instantiation_detail->date_type	.	':';	?></span>
+																																<?php
+																																if($instantiation_detail->dates)
+																																{
+																																				?>
+																																				<span><?php	echo	date('Y-m-d',	$instantiation_detail->dates);	?></span>
+
+																																<?php	}	?>
 
 																												<?php	}	?>
-
-																								<?php	}	?>
-																				</td>
-																</tr>
-												<?php	}	?>
-												<!--				Date 	End		-->
-												<!--				Media Type 	Start		-->
-												<?php
-												if($instantiation_detail->media_type)
-												{
-																$media_type	=	explode(' | ',	$instantiation_detail->media_type);
-																?>
-																<tr>
-																				<td class="record-detail-page">
-																								<label><i class="icon-question-sign"></i><b>* Media Type:</b></label>
-																				</td>
-																				<td>
-																								<?php
-																								foreach($media_type	as	$value)
-																								{
-																												?>
-																												<p><?php	echo	$value;	?></p>
-																								<?php	}
-																								?>
-																				</td>
-																</tr>
-												<?php	}	?>
-												<!--				Media Type	End		-->
-												<!--				Format 	Start		-->
-												<?php
-												if($instantiation_detail->format_name)
-												{
-
-																$format	=	'Format: ';
-																if(isset($instantiation_detail->format_type)	&&	($instantiation_detail->format_type	!=	NULL))
+																								</td>
+																				</tr>
+																<?php	}	?>
+																<!--				Date 	End		-->
+																<!--				Media Type 	Start		-->
+																<?php
+																if($instantiation_detail->media_type)
 																{
-																				if($instantiation_detail->format_type	===	'physical')
-																								$format	=	'Physical Format: ';
-																				if($instantiation_detail->format_type	===	'digital')
-																								$format	=	'Digital Format: ';
-																}
-																?>	
-																<tr>
-																				<td class="record-detail-page">
-																								<label><i class="icon-question-sign"></i><b>  <?php	echo	$format;	?></b></label>
-																				</td>
-																				<td>
-																								<span>	<?php	echo	$instantiation_detail->format_name;	?></span>
-																				</td>
-																</tr>
-												<?php	}	?>
-												<!--				Format	End		-->
-								</table>
+																				$media_type	=	explode(' | ',	$instantiation_detail->media_type);
+																				?>
+																				<tr>
+																								<td class="record-detail-page">
+																												<label><i class="icon-question-sign"></i><b>* Media Type:</b></label>
+																								</td>
+																								<td>
+																												<?php
+																												foreach($media_type	as	$value)
+																												{
+																																?>
+																																<p><?php	echo	$value;	?></p>
+																												<?php	}
+																												?>
+																								</td>
+																				</tr>
+																<?php	}	?>
+																<!--				Media Type	End		-->
+																<!--				Format 	Start		-->
+																<?php
+																if($instantiation_detail->format_name)
+																{
+
+																				$format	=	'Format: ';
+																				if(isset($instantiation_detail->format_type)	&&	($instantiation_detail->format_type	!=	NULL))
+																				{
+																								if($instantiation_detail->format_type	===	'physical')
+																												$format	=	'Physical Format: ';
+																								if($instantiation_detail->format_type	===	'digital')
+																												$format	=	'Digital Format: ';
+																				}
+																				?>	
+																				<tr>
+																								<td class="record-detail-page">
+																												<label><i class="icon-question-sign"></i><b>  <?php	echo	$format;	?></b></label>
+																								</td>
+																								<td>
+																												<span>	<?php	echo	$instantiation_detail->format_name;	?></span>
+																								</td>
+																				</tr>
+																<?php	}	?>
+																<!--				Format	End		-->
+												</table>
+								</div>
+								<div style="float: right;">
+												fadfadfdafadffasdfdsf
+								</div>
 				</div>
 
 </div>

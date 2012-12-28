657023ce9bbd3d36d67027670e64bceab6f139b6
Nouman Tayyab <nouman@geekschicago.com>
2012-12-28 15:24:06 +0500

    menu changes

--- application/views/instantiations/detail.php
+++ application/views/instantiations/detail.php
@@ -1,8 +1,8 @@
-<div class="row" style="height: 600px;">
+<div class="row">
 				<div style="margin-left: 10px;">
-				<h2>Instantiation Details: <?php	echo	$asset_details->title;	?></h2>
-</div>
-<div class="clearfix"></div>
+								<h2>Instantiation Details: <?php	echo	$asset_details->title;	?></h2>
+				</div>
+				<div class="clearfix"></div>
 				<?php	$this->load->view('partials/_list');	?>
 
 

760c2116e1d431f1ba89d1ac59974beac9a9e93a
Nouman Tayyab <nouman@geekschicago.com>
2012-12-28 15:30:06 +0500

    menu changes

--- application/views/instantiations/detail.php
+++ application/views/instantiations/detail.php
@@ -5,7 +5,28 @@
 				<div class="clearfix"></div>
 				<?php	$this->load->view('partials/_list');	?>
 
+				<div class="span9">
 
+								<table>
+												<tr>
+																<td>
+																				<label><i class="icon-question-sign"></i>* Instantiation ID:</label>
+																</td>
+																<td>
+																				<?php
+																				if($instantiation_detail->instantiation_identifier)
+																				{
+																								echo	$instantiation_detail->instantiation_identifier;
+																								if($instantiation_detail->instantiation_source)
+																								{
+																												echo	' ('	.	$instantiation_detail->instantiation_source	.	')';
+																								}
+																				}
+																				?>
+																</td>
+												</tr>
+								</table>
+				</div>
 
 </div>
 

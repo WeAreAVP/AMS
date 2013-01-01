9a2a3be6c14680776f6bd672300739b350b4a4b2
Nouman Tayyab <nouman@geekschicago.com>
2013-01-01 13:59:22 +0500

    some minor changes

--- application/models/assets_model.php
+++ application/models/assets_model.php
@@ -286,7 +286,8 @@ class	Assets_Model	extends	CI_Model
                                 GROUP_CONCAT(local.identifier SEPARATOR ' | ') AS local_identifier, 
 								GROUP_CONCAT(local.identifier_source SEPARATOR ' | ') AS local_identifier_source, 
 								GROUP_CONCAT(local.identifier_ref SEPARATOR ' | ') AS local_identifier_ref, 
-                                $this->_table_asset_descriptions.description, $this->_table_asset_titles.title ,
+                                $this->_table_asset_descriptions.description,
+																GROUP_CONCAT($this->_table_asset_titles.title SEPARATOR ' | ') AS title,
 								$this->_table_asset_titles.title_source ,$this->_table_asset_titles.title_ref ,$this->_table_asset_title_types.title_type,
 								GROUP_CONCAT($this->_table_asset_types.asset_type SEPARATOR ' | ') AS asset_type
 								FROM (`$this->_assets_table`) 

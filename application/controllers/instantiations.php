2d9046f18d6c01eafa18645fc723a9a4a64a989a
Nouman Tayyab <nouman@geekschicago.com>
2013-01-01 14:35:36 +0500

    some minor changes

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -154,7 +154,7 @@ class	Instantiations	extends	MY_Controller
 																$data['instantiation_detail']	=	$this->sphinx->instantiations_list(array('asset_id'	=>	$detail->assets_id,	'search'=>	''));
 																$data['instantiation_detail']	=	$data['instantiation_detail']['records'][0];
 																$data['asset_details']	=	$this->assets_model->get_asset_by_asset_id($detail->assets_id);
-//																debug($data['asset_details']);
+																debug($data['asset_details']);
 																$data['asset_instantiations']	=	$this->sphinx->instantiations_list(array('asset_id'	=>	$detail->assets_id,	'search'			=>	''));
 																$this->load->view('instantiations/detail',	$data);
 												}

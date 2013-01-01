cf3c697ae3117ab7e17c884d8a7efc890f1f7c95
Nouman Tayyab <nouman@geekschicago.com>
2013-01-01 13:35:50 +0500

    some minor changes

--- application/views/instantiations/detail.php
+++ application/views/instantiations/detail.php
@@ -160,8 +160,8 @@
 																												<label><i class="icon-question-sign"></i><b>* Duration</b></label>
 																								</td>
 																								<td>
-																												<?php echo $instantiation_detail->projected_duration;?>
-																												<p>	<?php	echo	date('H:i:s',	$instantiation_detail->projected_duration);	?></p>
+
+																												<p><?php	echo	$instantiation_detail->projected_duration;	?></p>
 
 																								</td>
 																				</tr>

120ad6cdda40f2706722d28a3a35d069cacd5d10
Nouman Tayyab <nouman@geekschicago.com>
2013-01-03 13:27:23 +0500

    changes

--- application/controllers/records.php
+++ application/controllers/records.php
@@ -156,7 +156,6 @@ class	Records	extends	MY_Controller
 								if($asset_id)
 								{
 												$data['asset_id']	=	$asset_id;
-												
 												$data['asset_details']	=	$this->assets_model->get_asset_by_asset_id($asset_id);
 												$data['asset_guid']	=	$this->assets_model->get_guid_by_asset_id($asset_id);
 												$data['asset_localid']	=	$this->assets_model->get_localid_by_asset_id($asset_id);

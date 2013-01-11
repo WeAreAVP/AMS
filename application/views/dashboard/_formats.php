ac3407d6032bd892c976d718a4b43b724d79dab4
Nouman Tayyab <nouman@geekschicago.com>
2013-01-11 18:45:27 +0500

    station user changes

--- application/views/dashboard/_formats.php
+++ application/views/dashboard/_formats.php
@@ -16,12 +16,7 @@
 
 
 <script type="text/javascript">
-  
-  $(function () {
-    
-    var chart2;
-    var chart3;
-    $(document).ready(function() {
+				$(document).ready(function() {
 								digitized_format_name=<?php echo json_encode($digitized_format_name); ?>;
 								digitized_total=<?php echo json_encode($digitized_total); ?>;
 								scheduled_format_name=<?php echo json_encode($scheduled_format_name); ?>;
@@ -84,90 +79,90 @@
         }
       };
       var highchartsOptions = Highcharts.setOptions(Highcharts.theme);
-						console.log(digitized_format_name);
+						
 						if(digitized_format_name!=null){
-      chart2 = new Highcharts.Chart({
-        chart: {
-          renderTo: 'digitized',
-          type: 'column'
-          //        margin: [ 50, 50, 100, 80]
-          
-        },
-        
-        title: {
-          text: ''
-        },
-        credits: {
-          enabled: false,
-          href: "",
-          text: "AMS"
-        },
-        plotOptions:{
-          column:{
-            borderWidth:0,
-            shadow:false
-          }
-        },
-        xAxis: {
-          categories: digitized_format_name,
-          gridLineWidth: 0,
-          labels: {
-            rotation: -90,
-            align: 'right',
-            style: {
-              fontSize: '11px',
-              fontFamily: 'Verdana, sans-serif'
-            }
-          },
-          lineWidth: 0,
-          tickWidth:0
-        },
-        yAxis: {
-//          tickInterval:100,
-          gridLineWidth: 0,
-          min: 0,
-//          max:1000,
-          title: {
-            text: ''
-          }
-        
-        },
-        legend: {
-          enabled: false
-        },
-      
-        tooltip: {
-          formatter: function() {
-            return '<b>'+ this.x +'</b><br/>'+
-              Highcharts.numberFormat(this.y, 0)+' Digitized';
-          }
-        },
-        series: [{
-            name: 'Total Digitized Assets',
-            pointWidth: 10,
-            borderWidth: 0,
-          
-            data: digitized_total,
-            dataLabels: {
-              enabled: true,
-														rotation: 0,
-              color: '#000000',
-              align: 'center',
-              x: 0,
-              y: 0,
-              style: {
-                fontSize: '9px',
-                fontFamily: 'Verdana, sans-serif'
-              }
-            }
-          }]
-      });
+								new Highcharts.Chart({
+												chart: {
+												renderTo: 'digitized',
+												type: 'column'
+												//        margin: [ 50, 50, 100, 80]
+
+												},
+
+												title: {
+												text: ''
+												},
+												credits: {
+												enabled: false,
+												href: "",
+												text: "AMS"
+												},
+												plotOptions:{
+												column:{
+																borderWidth:0,
+																shadow:false
+												}
+												},
+												xAxis: {
+												categories: digitized_format_name,
+												gridLineWidth: 0,
+												labels: {
+																rotation: -90,
+																align: 'right',
+																style: {
+																fontSize: '11px',
+																fontFamily: 'Verdana, sans-serif'
+																}
+												},
+												lineWidth: 0,
+												tickWidth:0
+												},
+												yAxis: {
+				//          tickInterval:100,
+												gridLineWidth: 0,
+												min: 0,
+				//          max:1000,
+												title: {
+																text: ''
+												}
+
+												},
+												legend: {
+												enabled: false
+												},
+
+												tooltip: {
+												formatter: function() {
+																return '<b>'+ this.x +'</b><br/>'+
+																Highcharts.numberFormat(this.y, 0)+' Digitized';
+												}
+												},
+												series: [{
+																name: 'Total Digitized Assets',
+																pointWidth: 10,
+																borderWidth: 0,
+
+																data: digitized_total,
+																dataLabels: {
+																enabled: true,
+																rotation: 0,
+																color: '#000000',
+																align: 'center',
+																x: 0,
+																y: 0,
+																style: {
+																				fontSize: '9px',
+																				fontFamily: 'Verdana, sans-serif'
+																}
+																}
+												}]
+								});
     
     }
 				else{
-				$('#digitized').html('<center>No digitized format available</center>');
+								$('#digitized').html('<center>No digitized format available</center>');
 				}
-      chart3 = new Highcharts.Chart({
+      new Highcharts.Chart({
         chart: {
           renderTo: 'scheduled',
           type: 'column'
@@ -244,7 +239,7 @@
       });
     });
     
-  });
+  
 		
 		function abbreviateNumber(value) {
     var newValue = value;

e5b228d6c2a697b48bc12e246408fabb144173ff
Nouman Tayyab <nouman@geekschicago.com>
2013-01-15 14:20:25 +0500

    query changes

--- application/models/instantiations_model.php
+++ application/models/instantiations_model.php
@@ -613,15 +613,16 @@ class	Instantiations_Model	extends	CI_Model
 				function	get_scheduled_formats()
 				{
 
-								$this->db->select("count(DISTINCT $this->table_instantiations.id) AS total",	FALSE);
+								
+								$this->db->select("count(DISTINCT $this->table_instantiation_formats.instantiations_id) AS total",	FALSE);
 								$this->db->select("$this->table_instantiation_formats.format_name",	FALSE);
 
-								$this->db->join($this->table_instantiation_formats,	"$this->table_instantiation_formats.instantiations_id = $this->table_instantiations.id");
-								$this->db->join($this->table_nominations,	"$this->table_nominations.instantiations_id = $this->table_instantiations.id");
+								$this->db->join($this->table_instantiations,	"$this->table_instantiation_formats.instantiations_id = $this->table_instantiations.id");
+								$this->db->join($this->table_nominations,	"$this->table_nominations.instantiations_id = $this->table_instantiation_formats.instantiations_id");
 								$this->db->where("$this->table_instantiations.digitized",	'0');
 								$this->db->or_where("$this->table_instantiations.digitized IS NULL");
 								$this->db->group_by("$this->table_instantiation_formats.format_name");
-								$result	=	$this->db->get($this->table_instantiations);
+								$result	=	$this->db->get($this->table_instantiation_formats);
 								
 								return	$result->result();
 				}

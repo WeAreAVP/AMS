1b63348b26feaad320de5fe591c226ad10266b21
Nouman Tayyab <nouman@geekschicago.com>
2013-01-15 16:24:45 +0500

    query changes

--- application/controllers/stations.php
+++ application/controllers/stations.php
@@ -52,7 +52,7 @@ class	Stations	extends	MY_Controller
 				public	function	index()
 				{
 
-								$param	=	array('search_kewords'	=>	'',	'certified'						=>	'',	'agreed'									=>	'');
+								$param	=	array('search_keywords'	=>	'',	'certified'						=>	'',	'agreed'									=>	'');
 								$value	=	$this->form_validation;
 								$value->set_rules('search_keyword',	'Search Keyword',	'trim|xss_clean');
 								$value->set_rules('certified',	'Certified',	'trim|xss_clean');
@@ -66,7 +66,7 @@ class	Stations	extends	MY_Controller
 //            $param['start_date'] = $this->input->post('start_date');
 //            $param['end_date'] = $this->input->post('end_date');
 
-												$param['search_kewords']	=	str_replace(',',	' & ',	trim($this->input->post('search_words')));
+												$param['search_keywords']	=	str_replace(',',	' & ',	trim($this->input->post('search_words')));
 												$records	=	$this->sphinx->search_stations($param);
 												$data['stations']	=	$records['records'];
 								}

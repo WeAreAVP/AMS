ad2d6e716e4d8f494c41f3ceef99bacb37580b1c
Nouman Tayyab <nouman@geekschicago.com>
2013-01-24 12:43:34 +0500

    csv changes

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -238,7 +238,7 @@ class	Instantiations	extends	MY_Controller
 				public	function	export_csv()
 				{
 								$params	=	array('search'	=>	'');
-								$records	=	$this->sphinx->instantiations_list($params);
+								$records	=	$this->sphinx->instantiations_list($params,0,286805);
 								$this->load->library('excel');
 								$this->excel->getActiveSheetIndex();
 								$this->excel->getActiveSheet()->setTitle('Limited CSV');

6cc76a7aacce16a6011c7195c63a48a727bb936a
Nouman Tayyab <nouman@geekschicago.com>
2013-01-24 19:41:56 +0500

    csv changes

--- application/views/templatemanager/edit_template.php
+++ application/views/templatemanager/edit_template.php
@@ -9,10 +9,12 @@ $subject	=	array	(
 				'id'								=>	'subject',
 				'value'					=>	$template_detail->subject,
 );
+$plain=str_replace	('<br />\n<br />',	"\n",	$template_detail->body_plain);
+$plain=str_replace	('<br />',	"\n",	$template_detail->body_plain);
 $body_plain	=	array	(
 				'name'					=>	'body_plain',
 				'id'							=>	'body_plain',
-				'value'				=>	str_replace	(array('<br />\n<br />','<br />'),	"\n",	$template_detail->body_plain),
+				'value'				=>	$plain,
 );
 $body_html	=	array	(
 				'name'											=>	'body_html',

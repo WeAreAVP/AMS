37067c1ff3010a9ec505a1f8a328184dbf9c3ca1
Nouman Tayyab <nouman@geekschicago.com>
2013-01-25 16:33:09 +0500

    test

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -267,14 +267,14 @@ class	Instantiations	extends	MY_Controller
 												$row	++;
 								}
 								$filename	=	'csv_export.csv';	//save our workbook as this file name
-								header('Content-Type: application/csv');	//mime type
-								header('Content-Disposition: attachment;filename="'	.	$filename	.	'"');	//tell browser what's the file name
-								header('Cache-Control: max-age=0');	//no cache
+//								header('Content-Type: application/csv');	//mime type
+//								header('Content-Disposition: attachment;filename="'	.	$filename	.	'"');	//tell browser what's the file name
+//								header('Cache-Control: max-age=0');	//no cache
 //save it to Excel5 format (excel 2003 .XLS file), change this to 'Excel2007' (and adjust the filename extension, also the header mime type)
 //if you want to save it as .XLSX Excel 2007 format
 								$objWriter	=	PHPExcel_IOFactory::createWriter($this->excel,	'Excel2007');
 //force user to download the Excel file without writing it to server's HD
-								$objWriter->save('php://output');
+								$objWriter->save('php://output',"assets/$filename");
 								exit;
 				}
 

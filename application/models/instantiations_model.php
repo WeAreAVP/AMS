537eef241cbb1583bb5605fac3849a46838fee14
Nouman Tayyab <nouman@geekschicago.com>
2013-01-25 17:29:17 +0500

    csv export

--- application/models/instantiations_model.php
+++ application/models/instantiations_model.php
@@ -762,7 +762,7 @@ class	Instantiations_Model	extends	CI_Model
 								$query=$this->db->group_by("$this->table_instantiations.id");
 
 								if($real_time){
-												return $query;
+												return $this->db->get_compiled_select();
 								}
 								$result	=	$this->db->get($this->table_instantiations);
 								

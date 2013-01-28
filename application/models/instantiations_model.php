74e69bc3c8b9ac51878aa740b0429deb9fae277d
Nouman Tayyab <nouman@geekschicago.com>
2013-01-28 14:56:33 +0500

    cron

--- application/models/instantiations_model.php
+++ application/models/instantiations_model.php
@@ -657,10 +657,46 @@ class	Instantiations_Model	extends	CI_Model
 								$this->db->join($this->table_instantiation_formats,	"$this->table_instantiation_formats.instantiations_id = $this->table_instantiations.id",	'left');
 								$this->db->join($this->table_nominations,	"$this->table_nominations.instantiations_id = $this->table_instantiations.id",	'left');
 								$this->db->join($this->table_nomination_status,	"$this->table_nomination_status.id = $this->table_nominations.nomination_status_id",	'left');
+								$this->db->join('assets_subjects',	"assets_subjects.assets_id = assets.id",	'left');
+								$this->db->join('subjects',	"subjects.id = assets_subjects.subjects_id",	'left');
+								$this->db->join('coverages',	"coverages.assets_id = assets.id",	'left');
+
+								$this->db->join('assets_genres',	"assets_genres.assets_id = assets.id",	'left');
+								$this->db->join('genres',	"genres.id = assets_genres.genres_id",	'left');
+								$this->db->join('assets_publishers_role',	"assets_publishers_role.assets_id = assets.id",	'left');
+								$this->db->join('publisher_roles',	"assets_publishers_role.publisher_roles_id = publisher_roles.id",	'left');
+								$this->db->join('publishers',	"assets_publishers_role.publishers_id = publishers.id",	'left');
+								$this->db->join('asset_descriptions',	"asset_descriptions.assets_id = assets.id",	'left');
+								$this->db->join('description_types',	"description_types.id = asset_descriptions.description_types_id",	'left');
+								$this->db->join('assets_creators_roles',	"assets_creators_roles.assets_id = assets.id",	'left');
+								$this->db->join('creator_roles',	"assets_creators_roles.creator_roles_id = creator_roles.id",	'left');
+								$this->db->join('creators',	"assets_creators_roles.creators_id = creators.id",	'left');
+								$this->db->join('assets_contributors_roles',	"assets_contributors_roles.assets_id = assets.id",	'left');
+								$this->db->join('contributor_roles',	"assets_contributors_roles.contributor_roles_id = contributor_roles.id",	'left');
+								$this->db->join('contributors',	"assets_contributors_roles.contributors_id = contributors.id",	'left');
+								$this->db->join('instantiation_identifier',	"instantiations.id = instantiation_identifier.instantiations_id",	'left');
+								$this->db->join('instantiation_dimensions',	"instantiation_dimensions.instantiations_id = instantiations.id",	'left');
+								$this->db->join('essence_tracks',	"essence_tracks.instantiations_id = instantiations.id",	'left');
+								$this->db->join('essence_track_encodings',	"essence_track_encodings.essence_tracks_id = essence_tracks.id",	'left');
+								$this->db->join('essence_track_frame_sizes',	"essence_track_frame_sizes.id = essence_tracks.essence_track_frame_sizes_id",	'left');
+								$this->db->join('essence_track_annotations',	"essence_track_annotations.essence_tracks_id = essence_tracks.id",	'left');
+								$this->db->join('instantiation_annotations',	"instantiation_annotations.instantiations_id = instantiations.id",	'left');
 
 
 
-//								$this->db->join($this->_table_asset_descriptions,	"$this->_table_asset_descriptions.assets_id = $this->_assets_table.id",	'left');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 								$this->db->join($this->asset_titles,	"$this->asset_titles.assets_id	 = $this->table_instantiations.assets_id",	'left');
 								$this->db->join($this->stations,	"$this->stations.id = $this->_assets_table.stations_id",	'left');
@@ -724,9 +760,66 @@ class	Instantiations_Model	extends	CI_Model
 
 								if(isset($session['custom_search'])	&&	$session['custom_search']	!=	'')
 								{
+												$facet_columns	=	array(
+												'asset_title'																			=>	'titles',
+												'asset_subject'																	=>	'subjects.subject',
+												'asset_coverage'																=>	'coverages.coverage',
+												'asset_genre'																			=>	'genres.genre',
+												'asset_publisher_name'										=>	'publishers.publisher',
+												'asset_description'													=>	'description_types.description_type',
+												'asset_creator_name'												=>	'creators.creator_name',
+												'asset_creator_affiliation'					=>	'creators.creator_affiliation',
+												'asset_contributor_name'								=>	'contributors.contributor_name',
+												'asset_contributor_affiliation'	=>	'contributors.contributor_affiliation',
+												'instantiation_identifier'						=>	'instantiation_identifier.instantiation_identifier',
+												'instantiation_source'										=>	'instantiation_identifier.instantiation_source',
+												'instantiation_dimension'							=>	'instantiation_dimensions.instantiation_dimension',
+												'unit_of_measure'															=>	'instantiation_dimensions.unit_of_measure',
+												'standard'																						=>	'instantiations.standard',
+												'location'																						=>	'instantiations.location',
+												'file_size'																					=>	'instantiations.file_size',
+												'actual_duration'															=>	'instantiations.actual_duration',
+												'track_duration'																=>	'essence_tracks.duration',
+												'data_rate'																					=>	'instantiations.data_rate',
+												'track_data_rate'															=>	'essence_tracks.data_rate',
+												'tracks'																								=>	'instantiations.tracks',
+												'channel_configuration'									=>	'instantiations.channel_configuration',
+												'language'																						=>	'instantiations.language',
+												'track_language'																=>	'essence_tracks.language',
+												'alternative_modes'													=>	'instantiations.alternative_modes',
+												'ins_annotation'																=>	'instantiation_annotations.annotation',
+												'track_annotation'														=>	'essence_track_annotations.annotation',
+												'ins_annotation_type'											=>	'instantiation_annotations.annotation_type',
+												'track_essence_track_type'						=>	'essence_track_annotations.annotation_type',
+												'track_encoding'																=>	'essence_track_encodings.encoding',
+												'track_standard'																=>	'essence_tracks.standard',
+												'track_frame_rate'														=>	'essence_tracks.frame_rate',
+												'track_playback_speed'										=>	'essence_tracks.playback_speed',
+												'track_sampling_rate'											=>	'essence_tracks.sampling_rate',
+												'track_bit_depth'															=>	'essence_tracks.bit_depth',
+												'track_width'																			=>	'essence_track_frame_sizes.width',
+												'track_height'																		=>	'essence_track_frame_sizes.height',
+												'track_aspect_ratio'												=>	'essence_tracks.aspect_ratio',
+												);
 												$custom_search	=	str_replace('|||',	'"',	trim($session['custom_search']));
 												$custom_search	=	explode('@',	$custom_search);
-												debug($custom_search);
+												$first_where	=	TRUE;
+												if(count($custom_search)	==	1)
+												{
+																debug($custom_search);
+												}
+												else
+												{
+																debug($custom_search);
+//																if($first_where	==	TRUE)
+//																{
+//																				$first_where	=	FALSE;
+//																}
+//																else
+//																{
+//																				
+//																}
+												}
 								}
 								if(isset($session['date_range'])	&&	$session['date_range']	!=	'')
 								{

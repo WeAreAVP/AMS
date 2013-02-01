9e8cc614cf33e9a9a737d13b3dacbe8d3c511e36
Nouman Tayyab <nouman@geekschicago.com>
2013-02-01 13:08:06 +0500

    some cron changes

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -145,6 +145,7 @@ class	MY_Controller	extends	CI_Controller
 								else	if($this->session->userdata['DX_email']	===	'cstephenson@mail.crawford.com')
 								{
 												$this->total_unread	=	$this->msgs->get_unread_msgs_count($this->session->userdata['DX_user_id']);
+												echo $this->total_unread;exit;
 								}
 								$this->user_detail	=	$this->users->get_user_detail($this->user_id)->row();
 								$this->can_compose_alert	=	false;

df608c0ef3d85d7b00381f180b79307fc874545a
Nouman Tayyab <nouman@geekschicago.com>
2013-02-04 16:13:09 +0500

    facet bar changes

--- application/controllers/records.php
+++ application/controllers/records.php
@@ -1,7 +1,7 @@
 <?php
 
-if	(	!	defined	('BASEPATH'))
-				exit	('No direct script access allowed');
+if(	!	defined('BASEPATH'))
+				exit('No direct script access allowed');
 
 /**
 	* Records controller.
@@ -17,16 +17,15 @@ class	Records	extends	MY_Controller
 					* 
 					*/
 
-				function	Records	()
+				function	Records()
 				{
-								parent::__construct	();
-								$this->load->model	('assets_model');
-								$this->load->model	('sphinx_model',	'sphinx');
-								$this->load->model	('instantiations_model',	'instantiation');
+								parent::__construct();
+								$this->load->model('assets_model');
+								$this->load->model('sphinx_model',	'sphinx');
+								$this->load->model('instantiations_model',	'instantiation');
 								$this->load->model('nomination_model',	'mix');
-								$this->load->library	('pagination');
-								$this->load->library	('Ajax_pagination');
-								
+								$this->load->library('pagination');
+								$this->load->library('Ajax_pagination');
 				}
 
 				/*
@@ -35,64 +34,67 @@ class	Records	extends	MY_Controller
 					*
 					*/
 
-				function	index	()
+				function	index()
 				{
-								$offset	=	($this->uri->segment	(3))	?	$this->uri->segment	(3)	:	0;
-								if	(isAjax	())
+								$offset	=	($this->uri->segment(3))	?	$this->uri->segment(3)	:	0;
+								if(isAjax())
 								{
-												$this->unset_facet_search	();
-												$search['custom_search']	=	$this->input->post	('keyword_field_main_search');
-												$search['organization']	=	$this->input->post	('organization_main_search');
-												$search['nomination']	=	$this->input->post	('nomination_status_main_search');
-												$search['media_type']	=	$this->input->post	('media_type_main_search');
-												$search['physical_format']	=	$this->input->post	('physical_format_main_search');
-												$search['digital_format']	=	$this->input->post	('digital_format_main_search');
-												$search['generation']	=	$this->input->post	('generation_main_search');
-												$search['date_range']	=	$this->input->post	('date_range');
-												$search['date_type']	=	$this->input->post	('date_type');
-												if	($this->input->post	('digitized')	&&	$this->input->post	('digitized')	===	'1')
+												$this->unset_facet_search();
+												$search['custom_search']	=	$this->input->post('keyword_field_main_search');
+												$search['organization']	=	$this->input->post('organization_main_search');
+												$search['nomination']	=	$this->input->post('nomination_status_main_search');
+												$search['media_type']	=	$this->input->post('media_type_main_search');
+												$search['physical_format']	=	$this->input->post('physical_format_main_search');
+												$search['digital_format']	=	$this->input->post('digital_format_main_search');
+												$search['generation']	=	$this->input->post('generation_main_search');
+												$search['date_range']	=	$this->input->post('date_range');
+												$search['date_type']	=	$this->input->post('date_type');
+												if($this->input->post('digitized')	&&	$this->input->post('digitized')	===	'1')
 												{
-																$search['digitized']	=	$this->input->post	('digitized');
+																$search['digitized']	=	$this->input->post('digitized');
 												}
-												if	($this->input->post	('migration_failed')	&&	$this->input->post	('migration_failed')	===	'1')
+												if($this->input->post('migration_failed')	&&	$this->input->post('migration_failed')	===	'1')
 												{
-																$search['migration_failed']	=	$this->input->post	('migration_failed');
+																$search['migration_failed']	=	$this->input->post('migration_failed');
 												}
-												$this->set_facet_search	($search);
+												$this->set_facet_search($search);
 								}
-								$this->session->set_userdata	('page_link',	'records/index');
-								$data['facet_search_url']	=	site_url	('records/index');
+								$this->session->set_userdata('page_link',	'records/index');
+								$data['facet_search_url']	=	site_url('records/index');
 								$data['current_tab']	=	'simple';
-								if	(isset	($this->session->userdata['current_tab'])	&&	!	empty	($this->session->userdata['current_tab']))
+								if(isset($this->session->userdata['current_tab'])	&&	!	empty($this->session->userdata['current_tab']))
 								{
 												$data['current_tab']	=	$this->session->userdata['current_tab'];
 								}
-								$this->session->set_userdata	('current_tab',	$data['current_tab']);
-								$data['get_column_name']	=	$this->make_array	();
-								$data['stations']	=	$this->station_model->get_asset_facet_stations();
-								$data['nomination_status']	=	$this->mix->get_assets_nomination	();
-								$data['media_types']	=	$this->mix->get_asset_media_types();
-								$data['physical_formats']	=	$this->mix->get_asset_physical_formats	();
-								$data['digital_formats']	=	$this->mix->get_asset_digital_formats	();
-								$data['generations']	=	$this->mix->get_asset_generations	();
-								$data['date_types']	=	$this->instantiation->get_date_types	();
-								$is_hidden	=	array	();
-								$data['table_type']	=	'assets';
-								foreach	($this->column_order	as	$key	=>	$value)
+								$this->session->set_userdata('current_tab',	$data['current_tab']);
+								$data['get_column_name']	=	$this->make_array();
+								if(	!	isAjax())
 								{
-												if	($value['hidden']	==	1)
+												$data['stations']	=	$this->station_model->get_asset_facet_stations();
+												$data['nomination_status']	=	$this->mix->get_assets_nomination();
+												$data['media_types']	=	$this->mix->get_asset_media_types();
+												$data['physical_formats']	=	$this->mix->get_asset_physical_formats();
+												$data['digital_formats']	=	$this->mix->get_asset_digital_formats();
+												$data['generations']	=	$this->mix->get_asset_generations();
+												$data['date_types']	=	$this->instantiation->get_date_types();
+								}
+								$is_hidden	=	array();
+								$data['table_type']	=	'assets';
+								foreach($this->column_order	as	$key	=>	$value)
+								{
+												if($value['hidden']	==	1)
 																$is_hidden[]	=	$key;
 								}
 								$data['hidden_fields']	=	$is_hidden;
 								$data['isAjax']	=	FALSE;
-								$param	=	array	('index'	=>	'assets_list');
-								$records	=	$this->sphinx->assets_listing	($param,	$offset);
+								$param	=	array('index'															=>	'assets_list');
+								$records	=	$this->sphinx->assets_listing($param,	$offset);
 								$data['total']	=	$records['total_count'];
 								$config['total_rows']	=	$data['total'];
 								$config['per_page']	=	100;
 								$data['records']	=	$records['records'];
-								$data['count']	=	count	($data['records']);
-								if	($data['count']	>	0	&&	$offset	==	0)
+								$data['count']	=	count($data['records']);
+								if($data['count']	>	0	&&	$offset	==	0)
 								{
 												$data['start']	=	1;
 												$data['end']	=	$data['count'];
@@ -100,7 +102,7 @@ class	Records	extends	MY_Controller
 								else
 								{
 												$data['start']	=	$offset;
-												$data['end']	=	intval	($offset)	+	intval	($data['count']);
+												$data['end']	=	intval($offset)	+	intval($data['count']);
 								}
 								$config['prev_link']	=	'<i class="icon-chevron-left"></i>';
 								$config['next_link']	=	'<i class="icon-chevron-right"></i>';
@@ -111,21 +113,21 @@ class	Records	extends	MY_Controller
 								$config['js_method']	=	'facet_search';
 								$config['postVar']	=	'page';
 
-								$this->ajax_pagination->initialize	($config);
-								if	(isAjax	())
+								$this->ajax_pagination->initialize($config);
+								if(isAjax())
 								{
 												$data['isAjax']	=	TRUE;
-												echo	$this->load->view	('records/index',	$data,	TRUE);
+												echo	$this->load->view('records/index',	$data,	TRUE);
 												exit;
 								}
-								$this->load->view	('records/index',	$data);
+								$this->load->view('records/index',	$data);
 				}
 
-				function	set_current_tab	($current_tab)
+				function	set_current_tab($current_tab)
 				{
-								if	(isAjax	())
+								if(isAjax())
 								{
-												$this->session->set_userdata	('current_tab',	$current_tab);
+												$this->session->set_userdata('current_tab',	$current_tab);
 												exit;
 								}
 				}
@@ -136,10 +138,10 @@ class	Records	extends	MY_Controller
 					*
 					*/
 
-				function	flagged	()
+				function	flagged()
 				{
-								show_404	();
-								exit	();
+								show_404();
+								exit();
 //			$this->load->view('records/flagged');
 				}
 
@@ -148,47 +150,47 @@ class	Records	extends	MY_Controller
 					*
 					*/
 
-				function	details	($asset_id)
+				function	details($asset_id)
 				{
-								if	($asset_id)
+								if($asset_id)
 								{
 												$data['asset_id']	=	$asset_id;
 												$data['list_assets']	=	$this->instantiation->get_instantiations_by_asset_id($asset_id);
-												$data['asset_details']	=	$this->assets_model->get_asset_by_asset_id	($asset_id);
-												$data['asset_guid']	=	$this->assets_model->get_guid_by_asset_id	($asset_id);
-												$data['asset_localid']	=	$this->assets_model->get_localid_by_asset_id	($asset_id);
-												$data['asset_subjects']	=	$this->assets_model->get_subjects_by_assets_id	($asset_id);
-												$data['asset_dates']	=	$this->assets_model->get_assets_dates_by_assets_id	($asset_id);
-												$data['asset_genres']	=	$this->assets_model->get_assets_genres_by_assets_id	($asset_id);
-												$data['asset_creators_roles']	=	$this->assets_model->get_assets_creators_roles_by_assets_id	($asset_id);
-												$data['asset_contributor_roles']	=	$this->assets_model->get_assets_contributor_roles_by_assets_id	($asset_id);
-												$data['asset_publishers_roles']	=	$this->assets_model->get_assets_publishers_role_by_assets_id	($asset_id);
-												$data['asset_coverages']	=	$this->assets_model->get_coverages_by_asset_id	($asset_id);
-												$data['rights_summaries']	=	$this->assets_model->get_rights_summaries_by_asset_id	($asset_id);
-												$data['asset_audience_levels']	=	$this->assets_model->get_audience_level_by_asset_id	($asset_id);
-												$data['asset_audience_ratings']	=	$this->assets_model->get_audience_rating_by_asset_id	($asset_id);
-												$data['annotations']	=	$this->assets_model->get_annotations_by_asset_id	($asset_id);
-												$search_results_data	=	$this->sphinx->assets_listing	(array	('index'	=>	'assets_list'),	0,	1000);
+												$data['asset_details']	=	$this->assets_model->get_asset_by_asset_id($asset_id);
+												$data['asset_guid']	=	$this->assets_model->get_guid_by_asset_id($asset_id);
+												$data['asset_localid']	=	$this->assets_model->get_localid_by_asset_id($asset_id);
+												$data['asset_subjects']	=	$this->assets_model->get_subjects_by_assets_id($asset_id);
+												$data['asset_dates']	=	$this->assets_model->get_assets_dates_by_assets_id($asset_id);
+												$data['asset_genres']	=	$this->assets_model->get_assets_genres_by_assets_id($asset_id);
+												$data['asset_creators_roles']	=	$this->assets_model->get_assets_creators_roles_by_assets_id($asset_id);
+												$data['asset_contributor_roles']	=	$this->assets_model->get_assets_contributor_roles_by_assets_id($asset_id);
+												$data['asset_publishers_roles']	=	$this->assets_model->get_assets_publishers_role_by_assets_id($asset_id);
+												$data['asset_coverages']	=	$this->assets_model->get_coverages_by_asset_id($asset_id);
+												$data['rights_summaries']	=	$this->assets_model->get_rights_summaries_by_asset_id($asset_id);
+												$data['asset_audience_levels']	=	$this->assets_model->get_audience_level_by_asset_id($asset_id);
+												$data['asset_audience_ratings']	=	$this->assets_model->get_audience_rating_by_asset_id($asset_id);
+												$data['annotations']	=	$this->assets_model->get_annotations_by_asset_id($asset_id);
+												$search_results_data	=	$this->sphinx->assets_listing(array('index'																	=>	'assets_list'),	0,	1000);
 												$data['next_result_id']	=	FALSE;
 												$data['prev_result_id']	=	FALSE;
-												if	(isset	($search_results_data['records'])	&&	!	is_empty	($search_results_data['records']))
+												if(isset($search_results_data['records'])	&&	!	is_empty($search_results_data['records']))
 												{
 																$search_results	=	$search_results_data['records'];
-																$search_results_array	=	array	();
+																$search_results_array	=	array();
 																$num_search_results	=	0;
-																if	($search_results)
+																if($search_results)
 																{
-																				foreach	($search_results	as	$search_result)
+																				foreach($search_results	as	$search_result)
 																				{
 																								$search_results_array[]['id']	=	$search_result->id;
 																				}
-																				$num_search_results	=	count	($search_results);
+																				$num_search_results	=	count($search_results);
 																}
 																# Get result number of current asset
 																$search_result_pointer	=	0;
-																foreach	($search_results_array	as	$search_res)
+																foreach($search_results_array	as	$search_res)
 																{
-																				if	($search_res['id']	==	$asset_id)
+																				if($search_res['id']	==	$asset_id)
 																								break;
 																				$search_result_pointer	++;
 																}
@@ -198,28 +200,28 @@ class	Records	extends	MY_Controller
 																$data['num_results']	=	$num_search_results;
 
 																# Get result number of next listings
-																if	($search_result_pointer	>=	($num_search_results	-	1))
+																if($search_result_pointer	>=	($num_search_results	-	1))
 																				$data['next_result_id']	=	FALSE;
 																else
 																				$data['next_result_id']	=	$search_results_array[$search_result_pointer	+	1]['id'];
 
 																# Get result number of previous listings
-																if	($search_result_pointer	<=	0	||	$num_search_results	==	1)
+																if($search_result_pointer	<=	0	||	$num_search_results	==	1)
 																				$data['prev_result_id']	=	FALSE;
 																else
 																				$data['prev_result_id']	=	$search_results_array[$search_result_pointer	-	1]['id'];
 												}
 												$data['last_page']	=	'';
-												if	(isset	($this->session->userdata['page_link'])	&&	!	is_empty	($this->session->userdata['page_link']))
+												if(isset($this->session->userdata['page_link'])	&&	!	is_empty($this->session->userdata['page_link']))
 												{
 																$data['last_page']	=	$this->session->userdata['page_link'];
 												}
 
-												$this->load->view	('records/assets_details',	$data);
+												$this->load->view('records/assets_details',	$data);
 								}
 								else
 								{
-												show_404	();
+												show_404();
 								}
 				}
 

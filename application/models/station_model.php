184f8b29008cc10fd21aa1baff4cf6de9419cf87
Nouman Tayyab <nouman@geekschicago.com>
2013-02-04 12:13:55 +0500

    facet bar changes

--- application/models/station_model.php
+++ application/models/station_model.php
@@ -48,7 +48,7 @@ class	Station_Model	extends	CI_Model
 								$this->db->select("COUNT($this->_instantiations_table.id) as total,$this->_table.station_name",	FALSE);
 								$this->db->join($this->_assets_table,	"$this->_assets_table.id = $this->_instantiations_table.assets_id");
 								$this->db->join($this->_table,	"$this->_table.id = $this->_assets_table.stations_id");
-								$this->db->group_by("$this->_assets_table.stations_id");
+								$this->db->group_by("$this->_table.id");
 								return	$query	=	$this->db->get($this->_instantiations_table)->result();
 				}
 

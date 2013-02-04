60d00ca216d4502eac310dbef47345eecbdea7e9
Nouman Tayyab <nouman@geekschicago.com>
2013-02-04 15:22:29 +0500

    facet bar changes

--- application/models/sphinx_model.php
+++ application/models/sphinx_model.php
@@ -170,13 +170,13 @@ class	Sphinx_Model	extends	CI_Model
 								}
 								if(isset($this->session->userdata['physical_format'])	&&	$this->session->userdata['physical_format']	!=	'')
 								{
-												$this->sphinxsearch->set_match_mode(SPH_MATCH_FULLSCAN);
+												$this->sphinxsearch->set_match_mode(SPH_MATCH_PHRASE);
 												$physical_format	=	str_replace('|||',	'" | "',	trim($this->session->userdata['physical_format']));
 												$where	.=" @format_name \"$physical_format\" @format_type \"physical\"";
 								}
 								if(isset($this->session->userdata['digital_format'])	&&	$this->session->userdata['digital_format']	!=	'')
 								{
-												$this->sphinxsearch->set_match_mode(SPH_MATCH_FULLSCAN);
+												$this->sphinxsearch->set_match_mode(SPH_MATCH_PHRASE);
 												$digital_format	=	str_replace('|||',	'" | "',	trim($this->session->userdata['digital_format']));
 												$where	.=" @format_name \"$digital_format\" @format_type \"digital\"";
 								}

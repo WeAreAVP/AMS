a9c4a16a8aae0f9745036f0a4f7e992780ed1f3f
Nouman Tayyab <nouman@geekschicago.com>
2013-02-04 12:59:32 +0500

    facet bar changes

--- application/models/nomination_model.php
+++ application/models/nomination_model.php
@@ -38,10 +38,10 @@ class	Nomination_Model	extends	CI_Model
 				{
 								$this->db->select("COUNT($this->_table.id) as total,$this->_nomination_table.status",	FALSE);
 								$this->db->join($this->_nomination_table,	"$this->_nomination_table.id = $this->_table.nomination_status_id");
-								$this->db->join($this->_instantiations_table,	"$this->_instantiations_table.id = $this->_nomination_table.instantiations_id");
+								$this->db->join($this->_instantiations_table,	"$this->_instantiations_table.id = $this->_table.instantiations_id");
 								$this->db->group_by("$this->_nomination_table.id");
-								$query	=	$this->db->get($this->_nomination_table);
-																echo $this->db->last_query();exit;
+								return	$this->db->get($this->_nomination_table)->result();
+																
 				}
 
 }

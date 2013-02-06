7837bea836a483716a2a1ceead2ff1753f6edb4d
Nouman Tayyab <nouman@geekschicago.com>
2013-02-06 16:35:04 +0500

    csv cron changes

---
+++ crons/export_csv.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#####################################
+#                                   #
+#  Shell Script for remove images   #
+#  from local server as images      #
+#  are moved on Amazon S3 Bucket    #
+#  This script will prevent         #
+#  from running method more         #
+#  than one time                    #
+#  Developer : Nouman Tayyab        #
+#  nouman@geekschicago.com          #
+#                                   #
+#####################################
+
+source /root/.bash_profile
+
+HOST=`/bin/hostname`
+PID_FILE="PIDs/export_csv.pid"
+OUTPUT_FILE="cronlog/export_csv.log"
+BASEDIR="/var/www/html/"
+DATE=`date`
+
+PHP_PATH=`which php`
+PID_FULLPATH=$BASEDIR$PID_FILE
+OUTPUT_FILE=$BASEDIR$OUTPUT_FILE
+
+touch $PID_FULLPATH
+touch $OUTPUT_FILE
+
+PID=`cat $PID_FULLPATH`
+PIDD=/proc/$PID 
+if [ -n "$PID" ]  && [ -d $PIDD ]
+then
+echo "Already running ...."
+else
+CMD="$PHP_PATH ${BASEDIR}index.php cron csv_export_job >> $OUTPUT_FILE 2>&1 & echo $! > $PID_FULLPATH"
+MSG="ENV [$ENVIRONMENT]"
+echo $MSG >> $OUTPUT_FILE
+MSG="Starting cron at $DATE"
+echo $MSG >> $OUTPUT_FILE
+echo $CMD >> $OUTPUT_FILE
+
+$PHP_PATH ${BASEDIR}index.php cron csv_export_job >> $OUTPUT_FILE 2>&1 & echo $! > $PID_FULLPATH
+PID=`cat $PID_FULLPATH`
+echo "Started Cron [$PID]"
+echo "To view logs Use tail -f $OUTPUT_FILE"
+fi

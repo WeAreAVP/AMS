26b36e00240aaa425795190f62f51db7bc39781d
Nouman Tayyab <nouman@geekschicago.com>
2013-02-06 17:50:55 +0500

    csv cron changes

--- application/controllers/crons.php
+++ application/controllers/crons.php
@@ -106,7 +106,7 @@ class	Crons	extends	CI_Controller
 												$objWriter->save("uploads/$filename");
 												$url	=	site_url()	.	"uploads/$filename";
 												$this->csv_job->update_job($job->id,	array('status'	=>	'1'));
-																$user=$this->users->get_user_by_id($job>user_id)->row();
+												$user=$this->users->get_user_by_id($job>user_id)->row();
 												send_email($user->email,	'ssapienza@cpb.org',	'Limited CSV Export',	$url);
 												exit;
 								}

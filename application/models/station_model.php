6b5ed71de6175ca68bea5cab4cbdfc9032fe6b2d
Nouman Tayyab <nouman@geekschicago.com>
2013-02-06 12:12:12 +0500

    csv cron changes

--- application/models/station_model.php
+++ application/models/station_model.php
@@ -215,7 +215,7 @@ class	Station_Model	extends	CI_Model
 				{
 								$this->db->select("COUNT($this->_assets_table.id) as total,$this->_table.state",	FALSE);
 								$this->db->join($this->_table,	"$this->_table.id = $this->_assets_table.stations_id");
-								$this->db->group_by("$this->_assets_table.state");
+								$this->db->group_by("$this->_table.state");
 								return	$query	=	$this->db->get($this->_assets_table)->result();
 				}
 
@@ -223,8 +223,8 @@ class	Station_Model	extends	CI_Model
 				{
 									$this->db->select("COUNT($this->_instantiations_table.id) as total,$this->_table.state",	FALSE);
 								$this->db->join($this->_assets_table,	"$this->_assets_table.id = $this->_instantiations_table.assets_id");
-								$this->db->join($this->_table,	"$this->_table.id = $this->_assets_table.state");
-								$this->db->group_by("$this->_table.id");
+								$this->db->join($this->_table,	"$this->_table.id = $this->_assets_table.stations_id");
+								$this->db->group_by("$this->_table.state");
 								return	$query	=	$this->db->get($this->_instantiations_table)->result();
 				}
 

f26f0873d3b87515543cd50a69a0ccd5add29607
Nouman Tayyab <nouman@geekschicago.com>
2013-02-07 14:55:43 +0500

    csv cron changes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -22,7 +22,7 @@
 												datepickerOptions: {
 																changeMonth: true,
 																changeYear: true,
-																yearRange: '-1000:+15'
+																yearRange: '-500:+15'
 																
 												},
 												onOpen:function(){ if(inframe){ $(window.parent.document).find('iframe:eq(1)').width(700).height('35em');} }, 

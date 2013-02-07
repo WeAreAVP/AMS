916696a57c9a887349c7cc4215ee66bdf8a70c07
Nouman Tayyab <nouman@geekschicago.com>
2013-02-07 13:01:32 +0500

    csv cron changes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,35 +1,30 @@
 <script type="text/javascript">
 			$(function(){
-					$('#rangeA').daterangepicker();
-					$('#rangeBa, #rangeBb').daterangepicker();
-					$('#rangeC').daterangepicker({arrows: true});
-					$('#rangeD').daterangepicker();
-					$('#rangeE').daterangepicker({constrainDates: true});
+				if($(window.parent.document).find('iframe').size()){
+					var inframe = true;
+				}
+				 $('input').daterangepicker({
+				 	presetRanges: [
+						{text: 'Ad Campaign', dateStart: 'Today', dateEnd: '03/07/09' },
+						{text: 'Spring Vacation', dateStart: '03/04/09', dateEnd: '03/08/09' },
+						{text: 'Office Closed', dateStart: '04/04/09', dateEnd: '04/08/09' }
+					], 
+					posX: null,
+				 	posY: null,
+				 	arrows: true, 
+				 	dateFormat: 'M d, yy',
+				 	rangeSplitter: 'to',
+				 	datepickerOptions: {
+				 		changeMonth: true,
+				 		changeYear: true
+				 	},
+				 	onOpen:function(){ if(inframe){ $(window.parent.document).find('iframe:eq(1)').width(700).height('35em');} }, 
+				  	onClose: function(){ if(inframe){ $(window.parent.document).find('iframe:eq(1)').width('100%').height('5em');} }
+				 }); 
 			 });
 		</script>
 		
 		
 		<div>
-			<input type="text" value="4/23/99" id="rangeA" />
-		</div>
-
-		<h2>2 inputs Rangepicker</h2>
-		<div>
-			<input type="text" value="4/23/99" id="rangeBa" />
-			<input type="text" value="4/23/99" id="rangeBb" />
-		</div>
-
-		<h2>Rangepicker with arrows</h2>
-		<div>
-			<input type="text" value="4/23/99" id="rangeC" />
-		</div>
-
-		<h2>Rangepicker opening to the right</h2>
-		<div style="float: right;">
-			<input type="text" value="4/23/99" id="rangeD" />
-		</div>
-
-		<h2>Rangepicker with contraints</h2>
-		<div>
-			<input type="text" value="4/23/99" id="rangeE" />
+		<input type="text" value="Choose a Date" id="dateRange" />
 		</div>

8f176e6b6c5681b94f1cc49ae445809001ecce0d
Nouman Tayyab <nouman@geekschicago.com>
2013-02-07 17:17:47 +0500

    csv cron changes

--- application/models/sphinx_model.php
+++ application/models/sphinx_model.php
@@ -246,13 +246,13 @@ class	Sphinx_Model	extends	CI_Model
 												if(isset($date_range[0])	&&	trim($date_range[0])	!=	'')
 												{
 																$start_date	=	strtotime(trim($date_range[0]));
-																var_dump($start_date);exit;
+																
 												}
 												if(isset($date_range[1])	&&	trim($date_range[1])	!=	'')
 												{
 																$end_date	=	strtotime(trim($date_range[1]));
 												}
-												if($start_date	!=	''	&&	isset($end_date)	&&	is_numeric($end_date)	&&	$end_date	>=	$start_date)
+												if($start_date	!=	'' && is_numeric($start_date)	&&	isset($end_date)	&&	is_numeric($end_date)	&&	$end_date	>=	$start_date)
 												{
 																$this->sphinxsearch->set_filter_range("dates",	$start_date,	$end_date);
 												}

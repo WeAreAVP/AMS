3a327de439bed52240461e84d9e5db1c66a057d7
Nouman Tayyab <nouman@geekschicago.com>
2013-02-08 19:37:23 +0500

    design changes

--- application/views/stations/list.php
+++ application/views/stations/list.php
@@ -121,7 +121,7 @@ if(	!	$is_ajax)
 																												<td><input type='checkbox' name='all' value='' id='check_all'  class="check-all" onclick='javascript:checkAll();' /></td>
 																								<?php	}	?>
 																								<th>CPB ID</th>
-																								<th>Station Name</th>
+																								<th style="width:230px;">Station Name</th>
 																								<th>Total Allocated Hours</th>
 																								<th>Certified</th>
 																								<th>Agreed</th>

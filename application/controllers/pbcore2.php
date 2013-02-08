65c7a39ef921875e5eae47baeb3bb932e66b990a
Nouman Tayyab <nouman@geekschicago.com>
2013-02-08 12:15:01 +0500

    design changes

---
+++ application/controllers/pbcore2.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+	* AMS Archive Management System
+	* 
+	* PHP version 5
+	* 
+	* @category AMS
+	* @package  CI
+	* @author   Nouman Tayyab <nouman@geekschicago.com>
+	* @license  CPB http://ams.avpreserve.com
+	* @version  GIT: <$Id>
+	* @link     http://ams.avpreserve.com
+
+	*/
+
+/**
+	* Pbcore2 Class
+	*
+	* @category   AMS
+	* @package    CI
+	* @subpackage Controller
+	* @author     Nouman Tayyab <nouman@geekschicago.com>
+	* @license    CPB http://ams.avpreserve.com
+	* @link       http://ams.avpreserve.com
+	*/
+class	Pbcore2	extends	CI_Controller
+{
+
+				function	index()
+				{
+								echo	'here';
+								exit;
+				}
+
+}
+// END Pbcore2 Controller
+
+// End of file pbcore2.php 
+/* Location: ./application/controllers/pbcore2.php */

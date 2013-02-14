efb449a418b02da780da942e94907f76aec3da9b
Nouman Tayyab <nouman@geekschicago.com>
2013-02-14 14:27:14 +0500

    pbcore crons changes

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,108 @@
 // JavaScript Document
 tinyMCE.init({
-        // General options
-        	mode : "exact",
+				// General options
+				mode : "exact",
 				elements : "body_html",
-        theme : "advanced",
-        plugins : "table,inlinepopups",
+				theme : "advanced",
+				plugins : "table,inlinepopups",
 
-        // Theme options
-        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,|,table,removeformat,code",
-        theme_advanced_buttons2 : "",
-        theme_advanced_buttons3 : "",
-        theme_advanced_buttons4 : "",
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "left",
-        theme_advanced_statusbar_location : "bottom",
-        theme_advanced_resizing : true,
+				// Theme options
+				theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,|,table,removeformat,code",
+				theme_advanced_buttons2 : "",
+				theme_advanced_buttons3 : "",
+				theme_advanced_buttons4 : "",
+				theme_advanced_toolbar_location : "top",
+				theme_advanced_toolbar_align : "left",
+				theme_advanced_statusbar_location : "bottom",
+				theme_advanced_resizing : true,
 
 
 
-        // Style formats
-        style_formats : [
-                {title : 'Bold text', inline : 'b'},
-                {title : 'Red text', inline : 'span', styles : {color : '#ff0000'}},
-                {title : 'Red header', block : 'h1', styles : {color : '#ff0000'}},
-                {title : 'Example 1', inline : 'span', classes : 'example1'},
-                {title : 'Example 2', inline : 'span', classes : 'example2'},
-                {title : 'Table styles'},
-                {title : 'Table row 1', selector : 'tr', classes : 'tablerow1'}
-        ],
+				// Style formats
+				style_formats : [
+				{
+								title : 'Bold text', 
+								inline : 'b'
+				},
 
-        formats : {
-                alignleft : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'left'},
-                aligncenter : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'center'},
-                alignright : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'right'},
-                alignfull : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'full'},
-                bold : {inline : 'span', 'classes' : 'bold'},
-                italic : {inline : 'span', 'classes' : 'italic'},
-                underline : {inline : 'span', 'classes' : 'underline', exact : true},
-                strikethrough : {inline : 'del'},
-                customformat : {inline : 'span', styles : {color : '#00ff00', fontSize : '20px'}, attributes : {title : 'My custom format'}}
-        }
+				{
+								title : 'Red text', 
+								inline : 'span', 
+								styles : {
+												color : '#ff0000'
+								}
+				},
+{
+				title : 'Red header', 
+				block : 'h1', 
+				styles : {
+								color : '#ff0000'
+				}
+},
+{
+				title : 'Example 1', 
+				inline : 'span', 
+				classes : 'example1'
+},
+{
+				title : 'Example 2', 
+				inline : 'span', 
+				classes : 'example2'
+},
+{
+				title : 'Table styles'
+},
+{
+				title : 'Table row 1', 
+				selector : 'tr', 
+				classes : 'tablerow1'
+}
+],
+
+formats : {
+				alignleft : {
+								selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 
+								classes : 'left'
+				},
+				aligncenter : {
+								selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 
+								classes : 'center'
+				},
+				alignright : {
+								selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 
+								classes : 'right'
+				},
+				alignfull : {
+								selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 
+								classes : 'full'
+				},
+				bold : {
+								inline : 'span', 
+								'classes' : 'bold'
+				},
+				italic : {
+								inline : 'span', 
+								'classes' : 'italic'
+				},
+				underline : {
+								inline : 'span', 
+								'classes' : 'underline', 
+								exact : true
+				},
+				strikethrough : {
+								inline : 'del'
+				},
+				customformat : {
+								inline : 'span', 
+								styles : {
+												color : '#00ff00', 
+												fontSize : '20px'
+								}, 
+								attributes : {
+												title : 'My custom format'
+								}
+				}
+}
 });
 function rand (min, max) {
 				var argc = arguments.length;
@@ -244,15 +307,18 @@ function updateDataTable()
 												'bInfo':false,
 												'bFilter': false,
 												"bSort": false,
-												"sScrollY": $(window).height()-180,
-												"sScrollX": "100%",	
-												"bDeferRender": true,
-												"bDestroy": is_destroy,
-												"bRetrieve": true,
-												"bAutoWidth": true
+								//												"sScrollY": $(window).height()-180,
+								//												"sScrollX": "100%",	
+								//												"bDeferRender": true,
+								//												"bDestroy": is_destroy,
+								//												"bRetrieve": true,
+								//												"bAutoWidth": true
 												
 												
 								});
+								new FixedHeader( oTable, {
+												"offsetTop": 60
+								} );
 								if(frozen>0)
 								{
 												new FixedColumns( oTable, {
@@ -264,7 +330,7 @@ function updateDataTable()
 								$.extend( $.fn.dataTableExt.oStdClasses,{
 												"sWrapper": "dataTables_wrapper form-inline"
 								});
-												$('.dataTable th').css('background','#EBEBEB');
+								$('.dataTable th').css('background','#EBEBEB');
 												
 								
 				}
@@ -299,7 +365,7 @@ function updateDatabase(refresh)
 												table_type:current_table_type
 								},
 								success: function (result){
-//												$('#listing_table').css('width','100%');
+												//												$('#listing_table').css('width','100%');
 												if(refresh==1)
 																window.location.reload();
 											

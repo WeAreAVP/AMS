7ebb42fb1b3429c57cff5c9d7d3b15dc8f71393f
Nouman Tayyab <nouman@geekschicago.com>
2013-02-15 12:21:21 +0500

    simple table change

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -451,6 +451,7 @@
 
 
 																				</div>
+																				<div class="clearfix"></div>
 																				<div id="limit_btn">
 																								<div><input type="button"  style="margin-top: 10px;" id="add_keyword" name="add_keyword" value="Add Keyword" class="btn btn-primary" onclick="add_token('','keyword_field_main');"/></div>
 																				</div>

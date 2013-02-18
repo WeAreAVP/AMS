81ef06e087724c4472606e623407e1b6197f4c10
Nouman Tayyab <nouman@geekschicago.com>
2013-02-18 19:21:59 +0500

    sorting

--- application/views/records/index.php
+++ application/views/records/index.php
@@ -54,21 +54,21 @@ if(	!	$isAjax)
 																				<?php	echo	$this->ajax_pagination->create_links();	?>
 																</div>
 												</div>
-								<div class="clearfix" style="width: 710px;overflow: hidden;"></div>
+												<div class="clearfix"></div>
 												<?php
-												if(	isset($current_tab)	||	$current_tab	==	'simple')
+												if(isset($current_tab)	||	$current_tab	==	'simple')
 												{
 																?>
-																<div style="" id="simple_view">
+																<div style="width: 710px;overflow: hidden" id="simple_view">
 																				<table class="table table-bordered" id="assets_table" style="border-collapse:collapse;width:710px !important;">
 																								<thead>
 																												<tr style="background: rgb(235, 235, 235);">
 																																<td><i class="icon-flag "></i></td>
-																																<th><span style="margin-right: 10px;">Organization</span></th>
-																																<th>AA GUID</th>
-																																<th>Local ID</th>
-																																<th>Titles</th>
-																																<th><span style="margin-right: 10px;">Description</span></th>
+																																<th><span style="width:100px;">Organization</span></th>
+																																<th><span style="width: 150px;">AA GUID</span></th>
+																																<th><span style="width: 150px;">Local ID</span></th>
+																																<th><span style="width: 150px;">Titles</span></th>
+																																<th><span style="width: 150px;">Description</span></th>
 																												</tr>
 																								</thead>
 																								<tbody>
@@ -84,11 +84,12 @@ if(	!	$isAjax)
 																																<tr style="cursor: pointer;">
 																																				<td style="vertical-align:middle;font-weight:bold;"><i style="margin:0px" class="unflag"></i></td>
 																																				<td>				
-																																								
+																																								<span style="width:100px;">
 																																												<?php	echo	$asset->organization	?>
-																																								
+																																								</span>
 																																				</td>
 																																				<td>
+																																								<span style="width:150px;">
 																																												<?php
 																																												if($guid_identifier)
 																																												{
@@ -96,15 +97,18 @@ if(	!	$isAjax)
 																																																<a href="<?php	echo	site_url('records/details/'	.	$asset->id)	?>" ><?php	echo	$guid_identifier;	?></a>
 																																												<?php	}
 																																												?>
-																																																
+																																								</span>		
 																																				</td>
 																																				<td>
+																																								<span style="width:150px;">
 																																												<?php
 																																												if($local_identifier)
 																																																echo	$local_identifier;
 																																												?>
+																																								</span>
 																																				</td>
 																																				<td>
+																																								<span style="width:150px;">
 																																												<?php
 																																												$asset_title_type	=	explode(' | ',	trim(str_replace('(**)',	'',	$asset->asset_title_type)));
 																																												$asset_title	=	explode(' | ',	trim(str_replace('(**)',	'',	$asset->asset_title)));
@@ -130,10 +134,12 @@ if(	!	$isAjax)
 																																												}
 																																												echo	$column;
 																																												?>
-																																								
+																																								</span>
 
 																																				</td>
 																																				<td>
+																																								<span style="width:150px;">
+
 																																												<?php
 																																												if($asset_description)
 																																												{
@@ -149,7 +155,7 @@ if(	!	$isAjax)
 																																																}
 																																												}
 																																												?>
-																																							
+																																								</span>
 																																				</td>
 																																</tr>
 																												<?php	}

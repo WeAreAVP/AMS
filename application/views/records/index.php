7c8b4bd9191fa5669ec24422234f9f9e5f1e49af
Nouman Tayyab <nouman@geekschicago.com>
2013-02-19 13:33:56 +0500

    simple table cjanges

--- application/views/records/index.php
+++ application/views/records/index.php
@@ -9,7 +9,7 @@ if(	!	$isAjax)
 								<?php	$this->load->view('instantiations/_facet_search');	?>
 				</div>
 
-								<div id="data_container" style="float:left;">
+								<div  class="span9" id="data_container" >
 
 
 								<?php	$this->load->view('layouts/_records_nav');	?>

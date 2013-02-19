205b8359c7c954fac2c8b6f3b7a8d46579e7a11a
Nouman Tayyab <nouman@geekschicago.com>
2013-02-19 13:12:29 +0500

    simple table cjanges

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -897,6 +897,7 @@
 								$('.navbar-inner').width($(window).width()-50);
 								$('#top_setting_nav').width($(window).width()-50);
 								$('body').css('overflow','hidden');
+								$('.span3').height($(window).height()-200);
 								var hash = window.location.hash.replace('#', '');
 								if($(window.parent.document).find('iframe').size()){
 												var inframe = true;

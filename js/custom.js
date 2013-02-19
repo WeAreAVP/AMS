9a8f2216f44499ed6bff9e10869063b3b86e3d18
Nouman Tayyab <nouman@geekschicago.com>
2013-02-19 12:50:06 +0500

    simple table cjanges

--- js/custom.js
+++ js/custom.js
@@ -319,7 +319,7 @@ function updateDataTable()
 												'bFilter': false,
 												"bSort": true,
 												"sScrollY": $(window).height()-180,
-												"sScrollX": "100%",	
+												"sScrollX": "101%",	
 												"bDeferRender": true, 
 												"bDestroy": is_destroy,
 												"bRetrieve": true,

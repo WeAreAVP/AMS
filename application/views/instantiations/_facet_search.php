48c8807a73e81134220a4fd54c0215c5e5b2b9be
Nouman Tayyab <nouman@geekschicago.com>
2013-02-19 18:18:25 +0500

    simple table cjanges

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -1208,7 +1208,7 @@
 																"bDeferRender": true, 
 																"bDestroy": is_destroy,
 																"bRetrieve": true,
-																"bAutoWidth": false,
+																"bAutoWidth": true,
 																"bProcessing": true,
 																"bServerSide": true,
 																"sAjaxSource": "http://amsqa.avpreserve.com/records/sort_simple_table"

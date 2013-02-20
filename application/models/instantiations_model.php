ae517e01e7166f6532f3afdce1aaa79dfd1288db
Nouman Tayyab <nouman@geekschicago.com>
2013-02-20 11:32:32 +0500

    instantiations changes

--- application/models/instantiations_model.php
+++ application/models/instantiations_model.php
@@ -977,14 +977,14 @@ class	Instantiations_Model	extends	CI_Model
 				{
 								$this->db->select("$this->table_instantiations.id,$this->table_instantiations.actual_duration,$this->table_instantiations.projected_duration",	FALSE);
 								$this->db->select("$this->table_instantiation_formats.format_name,$this->table_instantiation_formats.format_type",	FALSE);
-								$this->db->select("GROUP_CONCAT(DISTINCT $this->table_generations.generation SEPARATOR ' | ') AS generation",	FALSE);
-//								$this->db->select("GROUP_CONCAT(DISTINCT(IFNULL($this->table_instantiation_identifier.instantiation_identifier,'(**)')) SEPARATOR ' | ') AS instantiation_identifier",	FALSE);
-//								$this->db->select("GROUP_CONCAT(DISTINCT(IFNULL($this->table_instantiation_identifier.instantiation_source,'(**)')) SEPARATOR ' | ') AS instantiation_source",	FALSE);
+//								$this->db->select("GROUP_CONCAT(DISTINCT $this->table_generations.generation SEPARATOR ' | ') AS generation",	FALSE);
+								$this->db->select("GROUP_CONCAT(DISTINCT(IFNULL($this->table_instantiation_identifier.instantiation_identifier,'(**)')) SEPARATOR ' | ') AS instantiation_identifier",	FALSE);
+								$this->db->select("GROUP_CONCAT(DISTINCT(IFNULL($this->table_instantiation_identifier.instantiation_source,'(**)')) SEPARATOR ' | ') AS instantiation_source",	FALSE);
 								$this->db->where("$this->table_instantiations.assets_id",	$asset_id);
 								$this->db->join($this->table_instantiation_formats,	"$this->table_instantiation_formats.instantiations_id = $this->table_instantiations.id",	'left');
-								$this->db->join($this->table_instantiation_generations,	"$this->table_instantiation_generations.instantiations_id = $this->table_instantiations.id",	'left');
-								$this->db->join($this->table_generations,	"$this->table_generations.id = $this->table_instantiation_generations.generations_id",	'left');
-//								$this->db->join($this->table_instantiation_identifier,	"$this->table_instantiation_identifier.instantiations_id = $this->table_instantiations.id",	'left');
+//								$this->db->join($this->table_instantiation_generations,	"$this->table_instantiation_generations.instantiations_id = $this->table_instantiations.id",	'left');
+//								$this->db->join($this->table_generations,	"$this->table_generations.id = $this->table_instantiation_generations.generations_id",	'left');
+								$this->db->join($this->table_instantiation_identifier,	"$this->table_instantiation_identifier.instantiations_id = $this->table_instantiations.id",	'left');
 								return	$result	=	$this->db->get($this->table_instantiations)->result();
 				}
 

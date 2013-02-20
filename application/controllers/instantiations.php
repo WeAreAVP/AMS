c53d3e3eea151b39b0af0c4513d6d330989bb9bc
Nouman Tayyab <nouman@geekschicago.com>
2013-02-20 17:19:53 +0500

    design chages

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -479,7 +479,7 @@ class	Instantiations	extends	MY_Controller
 								$records	=	$records['records'];
 								$data['count']	=	count($records);
 								$table_view	=	instantiations_datatable_view($records,	$this->column_order);
-//								debug($table_view);
+
 								$dataTable	=	array(
 								"sEcho"																=>	$this->input->get('sEcho')	+	1,
 								"iTotalRecords"								=>	$data['count'],

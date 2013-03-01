02fab5b01d71339342c736e3874285db6fea189b
Nouman Tayyab <nouman@geekschicago.com>
2013-03-01 18:12:51 +0500

    facet ajax columns

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -34,7 +34,7 @@
 																{
 																				$column_explode[]	=	explode('==',	$keyword);
 																}
-																debug($custom_search);
+																debug($column_explode);
 																$custom_keyword	=	array();
 																foreach($column_explode	as	$key	=>	$value)
 																{

0aa29b98e3c3a942ca0ac518910be8bb135b465a
Nouman Tayyab <nouman@geekschicago.com>
2013-03-03 18:08:14 +0500

    facet chagnes

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -63,7 +63,7 @@ class	Instantiations	extends	MY_Controller
 												$this->unset_facet_search();
 												
 												$search['custom_search']	=	$this->input->post('keyword_field_main_search');
-												debug(unserialize($search['custom_search'])	);
+//												debug(unserialize($search['custom_search'])	);
 																								
 												$search['organization']	=	$this->input->post('organization_main_search');
 												$search['states']	=	$this->input->post('states_main_search');

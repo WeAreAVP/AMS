8ad3a37bf5e8c0d21108c7988035a924c5598f31
Nouman Tayyab <nouman@geekschicago.com>
2013-03-03 14:59:16 +0500

    facet chagnes

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -39,7 +39,7 @@
 																				{
 																								$column_explode[]	=	explode('=||=',	$keyword);
 																				}
-																				debug($column_explode);
+																				
 																				$custom_keyword	=	array();
 																				foreach($column_explode	as	$key	=>	$value)
 																				{

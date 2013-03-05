5fd08063bc52409f4ac6e3f1d06c9a86dff6d361
Nouman Tayyab <nouman@geekschicago.com>
2013-03-05 19:39:50 +0500

    facet query echo

--- application/models/sphinx_model.php
+++ application/models/sphinx_model.php
@@ -98,7 +98,7 @@ class	Sphinx_Model	extends	CI_Model
 												$this->sphinxsearch->set_limits((int)	$offset,	(int)	$limit,	(	$limit	>	1000	)	?	$limit	:	1000	);
 
 								$query	=	$this->make_where_clause($type);
-								echo $query;
+								echo 'ddfad '.$query.' dfad';
 								$res	=	$this->sphinxsearch->query($query,	$index_name);
 								debug($res);
 

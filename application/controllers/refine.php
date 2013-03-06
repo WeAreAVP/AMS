70ef363a5f2f250ab563c5fef133180e7ceb4b66
Nouman Tayyab <nouman@geekschicago.com>
2013-03-06 18:40:59 +0500

    google refine

--- application/controllers/refine.php
+++ application/controllers/refine.php
@@ -44,6 +44,7 @@ class	Refine	extends	MY_Controller
 				function create(){
 								
 								$project_name='Refine_'.time();
+								echo 'hee';exit;
 								$file_path='/var/www/html/uploads/Workbook7.csv';
 								$this->googlerefine->create_project($project_name,$file_path);
 				}

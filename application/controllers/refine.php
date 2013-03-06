2a888d79dd4b1e93635df956a206aaa6bc0d2827
Nouman Tayyab <nouman@geekschicago.com>
2013-03-06 18:05:45 +0500

    fact changes

---
+++ application/controllers/refine.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+	* Google Doc Controller
+	*  
+	* PHP version 5
+	* 
+	* @category   Controllers
+	* @package    AMS
+	* @subpackage Google_Documents_Controller
+	* @author     Ali Raza <ali@geekschicago.com>
+	* @author     Nouman Tayyab <nouman@avpreserve.com>
+	* @license    ams http://ams.appreserve.com
+	* @link       http://ams.appreserve.com
+	*/
+
+/**
+	* Googledoc   controller.
+	* 
+	* @category   Controllers
+	* @package    AMS
+	* @subpackage Google_Documents_Controller
+	* @author     Ali Raza <ali@geekschicago.com>
+	* @author     Nouman Tayyab <nouman@avpreserve.com>
+	* @license    ams http://ams.appreserve.com
+	* @link       http://ams.appreserve.com
+	*/
+class	Refine	extends	MY_Controller
+{
+
+				/**
+					* Constructor.
+					* 
+					* Load the Models,Library
+					* 
+					* @return 
+					*/
+				function	__construct()
+				{
+								parent::__construct();
+								$this->load->library('googlerefine');
+				}
+
+				function create(){
+								$project_name='Refine_'.time();
+								$file_path='/var/www/html/uploads/Workbook7.csv';
+								$this->googlerefine->create_project($project_name);
+				}
+
+// Location: ./controllers/googledoc.php
+}
+
+// END Google Doc Controller
+
+// End of file googledoc.php
+// Location: ./application/controllers/googledoc.php

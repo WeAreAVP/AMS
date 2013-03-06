d338bf7ce5e7f6893960382323db94a0f5aa0c61
Nouman Tayyab <nouman@geekschicago.com>
2013-03-06 18:11:01 +0500

    google refine

--- application/libraries/googlerefine.php
+++ application/libraries/googlerefine.php
@@ -18,6 +18,7 @@ class	Googlerefine
 							
 								$post_field	=	array('project-file'	=>	"@$file_path",	'project-name'	=>	$project_name);
 								$response	=	$this->send_curl_request($uri,	$post_field);
+								debug($response);
 								$pattern	=	'`.*?((http)://[\w#$&+,\/:;=?@.-]+)[^\w#$&+,\/:;=?@.-]*?`i';	//this regexp finds your url
 								if(preg_match_all($pattern,	$response,	$matches))
 												$project_url	=	$matches[1][0];	//project ID URL

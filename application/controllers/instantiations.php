6b1b57455478fff0593c76defcc0161f34f2d0e1
Nouman Tayyab <nouman@geekschicago.com>
2013-03-07 13:37:51 +0500

    pagination

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -465,7 +465,7 @@ class	Instantiations	extends	MY_Controller
 
 								$dataTable	=	array(
 								"sEcho"																=>	intval($this->input->get('sEcho')),
-								"iTotalRecords"								=>	intval($data['total']),
+								"iTotalRecords"								=>	intval($data['count']),
 								"iTotalDisplayRecords"	=>	intval($data['total']),
 								'aaData'															=>	$table_view
 								);

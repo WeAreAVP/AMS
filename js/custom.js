077de6a703a51b845badb32111ed76bbf9e1e4c3
Nouman Tayyab <nouman@geekschicago.com>
2013-03-07 11:20:50 +0500

    pagination

--- js/custom.js
+++ js/custom.js
@@ -320,7 +320,7 @@ function updateDataTable()
 																				updateDatabase(0);
 																}
 												},																		  
-												'bPaginate':true, 
+												'bPaginate':false, 
 												'bInfo':false,
 												'bFilter': false,
 												"bSort": true,

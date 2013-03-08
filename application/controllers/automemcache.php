cab5996c115e3dace101b2bbf245a7c6442fb46b
Nouman Tayyab <nouman@geekschicago.com>
2013-03-08 19:35:50 +0500

    csv

--- application/controllers/automemcache.php
+++ application/controllers/automemcache.php
@@ -63,32 +63,15 @@ class	Automemcache	extends	CI_Controller
 								$migration	=	$this->sphinx->facet_index('migration',	$index,	'migration');
 								
 								$this->memcached_library->set('ins_state',	json_encode(sortByOneKey($states['records'],	'state')),3600);
-								
-								
-								
 								$this->memcached_library->set('ins_stations',	json_encode(sortByOneKey($stations['records'],	'organization')),3600);
-								$this->myLog('Station Count '.count($this->memcached_library->get('ins_stations')));
-								
 								$this->memcached_library->set('ins_status',	json_encode(sortByOneKey($nomination['records'],	'status')),3600);
-								$this->myLog('Status Count '.count($this->memcached_library->get('ins_status')));
-								
 								$this->memcached_library->set('ins_media_type',	json_encode(sortByOneKey($media_type['records'],	'media_type',	TRUE)),3600);
-								$this->myLog('Media Type Count '.count($this->memcached_library->get('ins_media_type')));
-								
 								$this->memcached_library->set('ins_physical',	json_encode(sortByOneKey($p_format['records'],	'format_name',	TRUE)),3600);
-								$this->myLog('Physical Format Count '.count($this->memcached_library->get('ins_physical')));
-								
 								$this->memcached_library->set('ins_digital',	json_encode(sortByOneKey($d_format['records'],	'format_name',	TRUE)),3600);
-								$this->myLog('Digital Format Count '.count($this->memcached_library->get('ins_digital')));
-								
 								$this->memcached_library->set('ins_generations',	json_encode(sortByOneKey($generation['records'],	'facet_generation',	TRUE)),3600);
-								$this->myLog('Generation Count '.count($this->memcached_library->get('ins_generations')));
-								
 								$this->memcached_library->set('ins_digitized',	json_encode($digitized),3600);
-								$this->myLog('Digitized Count '.count($this->memcached_library->get('ins_digitized')));
-								
 								$this->memcached_library->set('ins_migration',	json_encode($migration),3600);
-								$this->myLog('Migration Count '.count($this->memcached_library->get('ins_migration')));
+								
 								
 								$this->myLog('Succussfully Updated Instantiations Facet Search');
 					
@@ -107,15 +90,15 @@ class	Automemcache	extends	CI_Controller
 								$digitized	=	$this->sphinx->facet_index('digitized',	$index,	'digitized');
 								$migration	=	$this->sphinx->facet_index('migration',	$index,	'migration');
 
-								$this->memcached_library->set('asset_state',	sortByOneKey($states['records'],	'state'));
-								$this->memcached_library->set('asset_stations',	sortByOneKey($stations['records'],	'organization'));
-								$this->memcached_library->set('asset_status',	sortByOneKey($nomination['records'],	'status'));
-								$this->memcached_library->set('asset_media_type',	sortByOneKey($media_type['records'],	'media_type',	TRUE));
-								$this->memcached_library->set('asset_physical',	sortByOneKey($p_format['records'],	'format_name',	TRUE));
-								$this->memcached_library->set('asset_digital',	sortByOneKey($d_format['records'],	'format_name',	TRUE));
-								$this->memcached_library->set('asset_generations',	sortByOneKey($generation['records'],	'facet_generation',	TRUE));
-								$this->memcached_library->set('asset_digitized',	$digitized);
-								$this->memcached_library->set('asset_migration',	$migration);
+								$this->memcached_library->set('asset_state',	json_encode(sortByOneKey($states['records'],	'state')),3600);
+								$this->memcached_library->set('asset_stations',	json_encode(sortByOneKey($stations['records'],	'organization')),3600);
+								$this->memcached_library->set('asset_status',	json_encode(sortByOneKey($nomination['records'],	'status')),3600);
+								$this->memcached_library->set('asset_media_type',	json_encode(sortByOneKey($media_type['records'],	'media_type',	TRUE)),3600);
+								$this->memcached_library->set('asset_physical',	json_encode(sortByOneKey($p_format['records'],	'format_name',	TRUE)),3600);
+								$this->memcached_library->set('asset_digital',	json_encode(sortByOneKey($d_format['records'],	'format_name',	TRUE)),3600);
+								$this->memcached_library->set('asset_generations',	json_encode(sortByOneKey($generation['records'],	'facet_generation',	TRUE)),3600);
+								$this->memcached_library->set('asset_digitized',	json_encode($digitized),3600);
+								$this->memcached_library->set('asset_migration',	json_encode($migration),3600);
 								$this->myLog('Succussfully Updated Assets Facet Search');
 								
 				}

27e2ea05f361b24c6de71fbdece3588ebeff3970
Nouman Tayyab <nouman@geekschicago.com>
2013-03-08 11:31:47 +0500

    memcache issue check

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -531,7 +531,7 @@ class	Instantiations	extends	MY_Controller
 																$data['physical_formats']	=	$this->memcached_library->get($key_name	.	'_physical');
 																$data['digital_formats']	=	$this->memcached_library->get($key_name	.	'_digital');
 																$data['generations']	=	$this->memcached_library->get($key_name	.	'_generations');
-																
+																count($data['generation']);
 																
 																$data['digitized']	=	$this->memcached_library->get($key_name	.	'_digitized');
 																$data['migration']	=	$this->memcached_library->get($key_name	.	'_migration');

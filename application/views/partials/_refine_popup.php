1f090949f944e78d2bcf8892cb268d5dac492f3f
Nouman Tayyab <nouman@geekschicago.com>
2013-03-12 12:41:14 +0500

    comment code

--- application/views/partials/_refine_popup.php
+++ application/views/partials/_refine_popup.php
@@ -5,15 +5,15 @@
 
     </div>
     <div class="modal-body" id="refine_body">
-							Are you sure you want to refine data.
+								Are you sure you want to refine data.
     </div>
     <div class="modal-footer" id="refine_footer">
         <button class="btn" data-dismiss="modal" aria-hidden="true">No</button>
-        <button class="btn btn-primary"  data-dismiss="modal" onclick="sentEmail();">Yes</button>
+        <button class="btn btn-primary"  data-dismiss="modal" onclick="doRefine();">Yes</button>
     </div>
     <div class="modal-footer" id="already_refine_footer">
         <button class="btn" data-dismiss="modal" aria-hidden="true">OK</button>
-        
+
     </div>
 </div>
 
@@ -21,16 +21,32 @@
 
 
 <script type="text/javascript">
-   function refineConfirm(msg,type){
-							$('#refine_body').html(msg);
-							if(type==0){
-											$('#already_refine_footer').hide();
-											$('#refine_footer').show();
-							}
-							else{
+				function refineConfirm(msg,type){
+								$('#refine_body').html(msg);
+								if(type==0){
+												$('#already_refine_footer').hide();
+												$('#refine_footer').show();
+								}
+								else{
 												$('#refine_footer').hide();
-											$('#already_refine_footer').show();
-							}
+												$('#already_refine_footer').show();
+								}
 											
-			}
+				}
+				function doRefine(){
+								$('#export_csv_msg').html('<img src="/images/ajax-loader.gif" />Creating AMS Refine Project. It might take some time...');
+								$('#refine_footer').hide();
+								$.ajax({
+												type: 'POST', 
+												url: site_url+'instantiations/export_csv',
+												dataType: 'json',
+												success: function (result) { 
+																if(result.link=='true')
+																				$('#export_csv_msg').html('<a href="'+result.msg+'">Download</a>');
+																else
+																				$('#export_csv_msg').html(result.msg);
+																																																																																								                                        
+												}
+								});
+				}
 </script>

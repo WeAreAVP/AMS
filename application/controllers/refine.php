572bd76d3b7c16532372e4952597eea9f386576c
Nouman Tayyan <noumantayyab@gmail.com>
2013-03-18 14:16:22 +0500

    format code

--- application/controllers/refine.php
+++ application/controllers/refine.php
@@ -1,190 +1,201 @@
 <?php
 
 /**
-	* Google Doc Controller
-	*  
-	* PHP version 5
-	* 
-	* @category   Controllers
-	* @package    AMS
-	* @subpackage Google_Documents_Controller
-	* @author     Ali Raza <ali@geekschicago.com>
-	* @author     Nouman Tayyab <nouman@avpreserve.com>
-	* @license    ams http://ams.appreserve.com
-	* @link       http://ams.appreserve.com
-	*/
+ * AMS Archive Management System
+ * 
+ * PHP version 5
+ * 
+ * @category AMS
+ * @package  CI
+ * @author   Nouman Tayyab <nouman@geekschicago.com>
+ * @license  CPB http://nouman.com
+ * @version  GIT: <$Id>
+ * @link     http://amsqa.avpreserve.com
+
+ */
 
 /**
-	* Googledoc   controller.
-	* 
-	* @category   Controllers
-	* @package    AMS
-	* @subpackage Google_Documents_Controller
-	* @author     Ali Raza <ali@geekschicago.com>
-	* @author     Nouman Tayyab <nouman@avpreserve.com>
-	* @license    ams http://ams.appreserve.com
-	* @link       http://ams.appreserve.com
-	*/
-class	Refine	extends	MY_Controller
+ * Refine Class
+ *
+ * @category   AMS
+ * @package    CI
+ * @subpackage Controller
+ * @author     Nouman Tayyab <nouman@geekschicago.com>
+ * @license    CPB http://nouman.com
+ * @link       http://amsqa.avpreserve.com
+ */
+class Refine extends MY_Controller
 {
 
-				/**
-					* Constructor.
-					* 
-					* Load the Models,Library
-					* 
-					* @return 
-					*/
-				function	__construct()
-				{
-								parent::__construct();
-								$this->load->library('googlerefine');
-								$this->load->model('refine_modal');
-								$this->load->model('sphinx_model',	'sphinx');
-				}
+    /**
+     * Constructor.
+     * 
+     * Load the Models,Library
+     * 
+     * @return 
+     */
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library('googlerefine');
+        $this->load->model('refine_modal');
+        $this->load->model('sphinx_model', 'sphinx');
+    }
 
-				function	create($path,	$filename,	$job_id)
-				{
+    function create($path, $filename, $job_id)
+    {
 
-								$project_name	=	$filename;
-								$file_path	=	$path;
-								$data	=	$this->googlerefine->create_project($project_name,	$file_path);
-								if($data)
-								{
-												$data['is_active']	=	1;
-												$data['project_name']	=	$filename;
-												$this->refine_modal->update_job($job_id,	$data);
-												return	$data['project_url'];
-								}
-								return	FALSE;
-				}
+        $project_name = $filename;
+        $file_path = $path;
+        $data = $this->googlerefine->create_project($project_name, $file_path);
+        if ($data)
+        {
+            $data['is_active'] = 1;
+            $data['project_name'] = $filename;
+            $this->refine_modal->update_job($job_id, $data);
+            return $data['project_url'];
+        }
+        return FALSE;
+    }
 
-				function	export($type)
-				{
-								if($type	==	'instantiation')
-								{
+    function export($type)
+    {
+        if ($type == 'instantiation')
+        {
 
-												$params	=	array('search'	=>	'');
-												$query	=	$this->refine_modal->export_refine_csv(TRUE);
-												$record	=	array('user_id'						=>	$this->user_id,	'is_active'				=>	0,	'export_query'	=>	$query);
-												$job_id	=	$this->refine_modal->insert_job($record);
-												$filename	=	'google_refine_'	.	time()	.	'.csv';
-												$fp	=	fopen("uploads/google_refine/$filename",	'a');
-												
-												$line	=	"Organization,Asset Title,Description,Instantiation ID,Instantiation ID Source,Generation,Nomination,Nomination Reason,Media Type,Language,__Ins_id\n";
-												fputs($fp,	$line);
-												fclose($fp);
-												$db_count	=	0;
-												$offset	=	0;
+            $params = array('search' => '');
+            $query = $this->refine_modal->export_refine_csv(TRUE);
+            $record = array('user_id' => $this->user_id, 'is_active' => 0, 'export_query' => $query, 'refine_type' => 'instantiation');
+            $job_id = $this->refine_modal->insert_job($record);
+            $filename = 'google_refine_' . time() . '.csv';
+            $fp = fopen("uploads/google_refine/$filename", 'a');
 
-												while	($db_count	==	0)
-												{
-																$custom_query=$query;
-																$custom_query.=' LIMIT '	.	($offset	*	15000)	.	', 15000';
+            $line = "Organization,Asset Title,Description,Instantiation ID,Instantiation ID Source,Generation,Nomination,Nomination Reason,Media Type,Language,__Ins_id\n";
+            fputs($fp, $line);
+            fclose($fp);
+            $db_count = 0;
+            $offset = 0;
 
-																$records	=	$this->refine_modal->get_csv_records($query);
+            while ($db_count == 0)
+            {
+                $custom_query = $query;
+                $custom_query.=' LIMIT ' . ($offset * 15000) . ', 15000';
 
-																$fp	=	fopen("uploads/google_refine/$filename",	'a');
-																$line	=	'';
-																foreach($records	as	$value)
-																{
-																				$line.='"'	.	str_replace('"',	'""',	$value->organization)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->asset_title)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->description)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->instantiation_identifier)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->instantiation_source)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->generation)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->status)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->nomination_reason)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->media_type)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->language)	.	'",';
-																				$line.='"'	.	str_replace('"',	'""',	$value->ins_id)	.	'"';
-																				$line	.=	"\n";
-																}
-																fputs($fp,	$line);
-																fclose($fp);
-																$offset	++;
-																if(count($records)	<	15000)
-																				$db_count	++;
-												}
+                $records = $this->refine_modal->get_csv_records($query);
 
-												$path	=	$this->config->item('path')	.	"uploads/google_refine/$filename";
-												$data	=	array('export_csv_path'	=>	$path);
-												$this->refine_modal->update_job($job_id,	$data);
-												$project_url	=	$this->create($path,	$filename,	$job_id);
-												echo	json_encode(array('project_url'	=>	$project_url));
-												exit;
-								}
-								else
-								{
-												$params	=	array('search'	=>	'');
-												$query	=	$this->refine_modal->export_asset_refine_csv(TRUE);
-												
-												$record	=	array('user_id'						=>	$this->user_id,	'is_active'				=>	0,	'export_query'	=>	$query);
-												$job_id	=	$this->refine_modal->insert_job($record);
-												$filename	=	'google_refine_'	.	time()	.	'.csv';
-												$fp	=	fopen("uploads/google_refine/$filename",	'a');
-												$line	=	"Organization,Asset Title,Description,Subject,Subject Source,Subject Ref,Genre,Genre Source,Genre Ref,Creator Name,Creator Affiliation,Creator Source,Creator Ref,";
-												$line	.="Contributors Name,Contributors Affiliation,Contributors Source,Contributors Ref,Publisher,Publisher Affiliation,Publisher Ref,Coverage,Coverage Type,";
-												$line	.="Audience Level,Audience Level Source,Audience Level Ref,";
-												$line	.="Audience Rating,Audience Rating Source,Audience Rating Ref,";
-												$line	.="Annotation,Annotation Type,Annotation Ref,";
-												$line	.="Rights,Rights Link,Asset Type,Identifier,Identifier Source,Identifier Ref,Asset Date,";
-												$line	.="__asset_id\n";
-												fputs($fp,	$line);
-												fclose($fp);
-												$db_count	=	0;
-												$offset	=	0;
-												while	($db_count	==	0)
-												{
+                $fp = fopen("uploads/google_refine/$filename", 'a');
+                $line = '';
+                foreach ($records as $value)
+                {
+                    $line.='"' . str_replace('"', '""', $value->organization) . '",';
+                    $line.='"' . str_replace('"', '""', $value->asset_title) . '",';
+                    $line.='"' . str_replace('"', '""', $value->description) . '",';
+                    $line.='"' . str_replace('"', '""', $value->instantiation_identifier) . '",';
+                    $line.='"' . str_replace('"', '""', $value->instantiation_source) . '",';
+                    $line.='"' . str_replace('"', '""', $value->generation) . '",';
+                    $line.='"' . str_replace('"', '""', $value->status) . '",';
+                    $line.='"' . str_replace('"', '""', $value->nomination_reason) . '",';
+                    $line.='"' . str_replace('"', '""', $value->media_type) . '",';
+                    $line.='"' . str_replace('"', '""', $value->language) . '",';
+                    $line.='"' . str_replace('"', '""', $value->ins_id) . '"';
+                    $line .= "\n";
+                }
+                fputs($fp, $line);
+                fclose($fp);
+                $offset ++;
+                if (count($records) < 15000)
+                    $db_count ++;
+            }
 
-																$query.=' LIMIT '	.	($offset	*	15000)	.	', 15000';
+            $path = $this->config->item('path') . "uploads/google_refine/$filename";
+            $data = array('export_csv_path' => $path);
+            $this->refine_modal->update_job($job_id, $data);
+            $project_url = $this->create($path, $filename, $job_id);
+            echo json_encode(array('project_url' => $project_url));
+            exit;
+        }
+        else
+        {
+            $params = array('search' => '');
+            $query = $this->refine_modal->export_asset_refine_csv(TRUE);
 
-																$records	=	$this->refine_modal->get_csv_records($query);
+            $record = array('user_id' => $this->user_id, 'is_active' => 0, 'export_query' => $query, 'refine_type' => 'asset');
+            $job_id = $this->refine_modal->insert_job($record);
+            $filename = 'google_refine_' . time() . '.csv';
+            $fp = fopen("uploads/google_refine/$filename", 'a');
+            $line = "Organization,Asset Title,Description,Subject,Subject Source,Subject Ref,Genre,Genre Source,Genre Ref,Creator Name,Creator Affiliation,Creator Source,Creator Ref,";
+            $line .="Contributors Name,Contributors Affiliation,Contributors Source,Contributors Ref,Publisher,Publisher Affiliation,Publisher Ref,Coverage,Coverage Type,";
+            $line .="Audience Level,Audience Level Source,Audience Level Ref,";
+            $line .="Audience Rating,Audience Rating Source,Audience Rating Ref,";
+            $line .="Annotation,Annotation Type,Annotation Ref,";
+            $line .="Rights,Rights Link,Asset Type,Identifier,Identifier Source,Identifier Ref,Asset Date,";
+            $line .="__asset_id\n";
+            fputs($fp, $line);
+            fclose($fp);
+            $db_count = 0;
+            $offset = 0;
+            while ($db_count == 0)
+            {
 
-																$fp	=	fopen("uploads/google_refine/$filename",	'a');
-																$line	=	'';
-																foreach($records	as	$value)
-																{
-																				foreach($value as $column){
-																									$line.='"'	.	str_replace('"',	'""',	$column)	.	'",';
-																				}
-																				$line	.=	"\n";
-																}
-																fputs($fp,	$line);
-																fclose($fp);
-																$offset	++;
-																if(count($records)	<	15000)
-																				$db_count	++;
-												}
+                $query.=' LIMIT ' . ($offset * 15000) . ', 15000';
 
-												$path	=	$this->config->item('path')	.	"uploads/google_refine/$filename";
-												$data	=	array('export_csv_path'	=>	$path);
-												$this->refine_modal->update_job($job_id,	$data);
-												$project_url	=	$this->create($path,	$filename,	$job_id);
-												echo	json_encode(array('project_url'	=>	$project_url));
-												exit;
-								}
-				}
+                $records = $this->refine_modal->get_csv_records($query);
 
-				function	remove($project_id)
-				{
+                $fp = fopen("uploads/google_refine/$filename", 'a');
+                $line = '';
+                foreach ($records as $value)
+                {
+                    foreach ($value as $column)
+                    {
+                        $line.='"' . str_replace('"', '""', $column) . '",';
+                    }
+                    $line .= "\n";
+                }
+                fputs($fp, $line);
+                fclose($fp);
+                $offset ++;
+                if (count($records) < 15000)
+                    $db_count ++;
+            }
 
-								$this->googlerefine->delete_project($project_id);
-								$db_detail	=	$this->refine_modal->get_by_project_id($project_id);
-								if($db_detail)
-								{
-												$data	=	array('is_active'	=>	0);
-												$this->refine_modal->update_job($db_detail->id,	$data);
-								}
+            $path = $this->config->item('path') . "uploads/google_refine/$filename";
+            $data = array('export_csv_path' => $path);
+            $this->refine_modal->update_job($job_id, $data);
+            $project_url = $this->create($path, $filename, $job_id);
+            echo json_encode(array('project_url' => $project_url));
+            exit;
+        }
+    }
 
-								redirect('records');
-				}
+    function remove($project_id)
+    {
 
-				function	save()
-				{
-								
-				}
+        $this->googlerefine->delete_project($project_id);
+        $db_detail = $this->refine_modal->get_by_project_id($project_id);
+        if ($db_detail)
+        {
+            $data = array('is_active' => 0);
+            $this->refine_modal->update_job($db_detail->id, $data);
+        }
+
+        redirect('records');
+    }
+
+    function save($project_id)
+    {
+        $project_detail = $this->refine_modal->get_by_project_id($project_id);
+        if ($project_detail)
+        {
+            $response=$this->googlerefine->export_rows($project_detail->project_name,$project_id);
+            $filename = 'google_refined_data_' . time() . '.csv';
+            $path = $this->config->item('path') . "uploads/google_refine/$filename";
+            file_put_contents($path, $response);
+            echo $path;
+            exit;
+            $data = array('is_active' => 0);
+            $this->refine_modal->update_job($db_detail->id, $data);
+        }
+    }
 
 // Location: ./controllers/refine.php
 }
--- application/libraries/googlerefine.php
+++ application/libraries/googlerefine.php
@@ -1,92 +1,94 @@
 <?php
 
-class	Googlerefine
+class Googlerefine
 {
 
-				var	$server	=	'http://amsqa.avpreserve.com:3333';
-				var	$project_id	=	FALSE;
+    var $server = 'http://amsqa.avpreserve.com:3333';
+    var $project_id = FALSE;
 
-				function	__construct()
-				{
-								
-				}
+    function __construct()
+    {
+        
+    }
 
-				function	create_project($project_name,	$file_path)
-				{
-								$data	=	NULL;
-								$uri	=	$this->server	.	'/command/core/create-project-from-upload';
-								$post_field	=	array('project-file'	=>	"@$file_path",	'project-name'	=>	$project_name);
+    function create_project($project_name, $file_path)
+    {
+        $data = NULL;
+        $uri = $this->server . '/command/core/create-project-from-upload';
+        $post_field = array('project-file' => "@$file_path", 'project-name' => $project_name);
 
-								$response	=	$this->send_curl_request($uri,	$post_field);
+        $response = $this->send_curl_request($uri, $post_field);
 
-								$pattern	=	'`.*?((http)://[\w#$&+,\/:;=?@.-]+)[^\w#$&+,\/:;=?@.-]*?`i';	//this regexp finds your url
-								if(preg_match_all($pattern,	$response,	$matches))
-												$project_url	=	$matches[1][0];	//project ID URL
-								$data['project_url']	=	$project_url;
-								if(isset($project_url))
-								{
-												$data['project_url']	=	$project_url;
-												$explode_url	=	explode('project=',	$project_url);
-												$data['project_id']	=	$explode_url[1];
-								}
+        $pattern = '`.*?((http)://[\w#$&+,\/:;=?@.-]+)[^\w#$&+,\/:;=?@.-]*?`i'; //this regexp finds your url
+        if (preg_match_all($pattern, $response, $matches))
+            $project_url = $matches[1][0]; //project ID URL
+        $data['project_url'] = $project_url;
+        if (isset($project_url))
+        {
+            $data['project_url'] = $project_url;
+            $explode_url = explode('project=', $project_url);
+            $data['project_id'] = $explode_url[1];
+        }
 
 
-								return	$data;
+        return $data;
 //								$this->export_rows($this->project_id);
-				}
+    }
 
-				function	send_curl_request($url,	$postFields	=	null,	$headers	=	1)
-				{
-								$ch	=	curl_init();
-								curl_setopt($ch,	CURLOPT_URL,	$url);
-								curl_setopt($ch,	CURLOPT_SSL_VERIFYPEER,	false);
-								curl_setopt($ch,	CURLOPT_FAILONERROR,	1);
-								curl_setopt($ch,	CURLOPT_RETURNTRANSFER,	1);
-								curl_setopt($ch,	CURLOPT_FOLLOWLOCATION,	0);
+    function send_curl_request($url, $postFields = null, $headers = 1)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
 
-								if($postFields	!=	null)
-								{
-												curl_setopt($ch,	CURLOPT_POST,	true);
-												curl_setopt($ch,	CURLOPT_POSTFIELDS,	$postFields);
-								}
-								curl_setopt($ch,	CURLOPT_HEADER,	$headers);
+        if ($postFields != null)
+        {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        }
+        curl_setopt($ch, CURLOPT_HEADER, $headers);
 
-								$page	=	curl_exec($ch);
-								$response	=	curl_getinfo($ch);
+        $page = curl_exec($ch);
+        $response = curl_getinfo($ch);
 
-								return	$page;
-				}
+        return $page;
+    }
 
-				function	prepare_post_fields($array)
-				{
-								$params	=	array();
+    function prepare_post_fields($array)
+    {
+        $params = array();
 
-								foreach($array	as	$key	=>	$value)
-								{
-												$params[]	=	$key	.	'='	.	urlencode($value);
-								}
+        foreach ($array as $key => $value)
+        {
+            $params[] = $key . '=' . urlencode($value);
+        }
 
-								return	implode('&',	$params);
-				}
+        return implode('&', $params);
+    }
 
-				function	export_rows($project_name,$project_id,	$format	=	'csv')
-				{
+    function export_rows($project_name, $project_id, $format = 'csv')
+    {
 
 
-								$uri	=	$this->server	.	'/command/core/export-rows/'	.	$project_name	.	'.'	.	$format;
-								$post_field	=	array('engine'						=>	'{"facets":[],"mode":"row-based"}',	'project'					=>	$project_id,	'format'						=>	$format,	"contentType"	=>	"application-unknown");
-								$post_field	=	$this->prepare_post_fields($post_field);
-								$response	=	$this->send_curl_request($uri,	$post_field,	0);
-								file_put_contents("/Users/noumantayyab/test.txt",	$response);
-				}
+//        $uri = $this->server . '/command/core/export-rows/' . $project_name . '.' . $format;
+        $uri = $this->server . '/command/core/export-rows/' . $project_name;
+        $post_field = array('engine' => '{"facets":[],"mode":"row-based"}', 'project' => $project_id, 'format' => $format, "contentType" => "application-unknown");
+        $post_field = $this->prepare_post_fields($post_field);
+        $response = $this->send_curl_request($uri, $post_field, 0);
+        return $response;
+        
+    }
 
-				function	delete_project($project_id)
-				{
-								$uri	=	$this->server	.	'/command/core/delete-project';
-								$post_field	=	array('project'			=>	$project_id);
-								$post_field	=	$this->prepare_post_fields($post_field);
-								$response	=	$this->send_curl_request($uri,	$post_field);
-								return TRUE;
-				}
+    function delete_project($project_id)
+    {
+        $uri = $this->server . '/command/core/delete-project';
+        $post_field = array('project' => $project_id);
+        $post_field = $this->prepare_post_fields($post_field);
+        $response = $this->send_curl_request($uri, $post_field);
+        return TRUE;
+    }
 
 }

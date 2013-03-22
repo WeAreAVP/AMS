fe7149a3eb5817ff5bdd9e2f8afb1b325dc7af7c
Nouman Tayyan <noumantayyab@gmail.com>
2013-03-22 13:41:37 +0500

    refine changes

--- application/controllers/googledoc.php
+++ application/controllers/googledoc.php
@@ -48,6 +48,7 @@ class Googledoc extends MY_Controller
 	 */
 	function parse_american_archive()
 	{
+		echo base64_encode('nouman@avps');
 		$this->load->library('google_spreadsheet', array('user' => 'nouman@avpreserve.com', 'pass' => 'nouman@avps', 'ss' => 'test_archive', 'ws' => 'Template'));
 		$spreed_sheets = $this->google_spreadsheet->getAllSpreedSheetsDetails('');
 		debug($spreed_sheets);

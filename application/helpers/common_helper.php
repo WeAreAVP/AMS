365fb477ad05b2caac1f39390c3f0b586964cfc5
Nouman Tayyan <noumantayyab@gmail.com>
2013-03-25 21:05:48 +0500

    deployment controller

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -270,7 +270,7 @@ function deployment_display($msg, $status = 'FAILED')
 	else
 		$color = 'color:green;';
 
-	echo "<body style='background:black;color:white;font-family:monaco;font-size:12pt;'><nobr><h5>$msg\t\t[ <b style='$color'>$status</b> ]</h5></nobr>/body>";
+	echo "<body style='background:black;color:white;font-family:monaco;font-size:12pt;'><nobr><$msg\t\t[ <b style='$color'>$status</b> ]</nobr></body>";
 	flush_buffers();
 }
 

767397e3595aaf5dc0adda9033a1151a0a7f7a98
Nouman Tayyan <noumantayyab@gmail.com>
2013-03-28 12:16:24 +0500

    media info import

--- application/controllers/mediainfo.php
+++ application/controllers/mediainfo.php
@@ -659,6 +659,7 @@ class Mediainfo extends CI_Controller
 	function get_asset_id_for_media_import($guid)
 	{
 		$asset_guid = explode('.', $guid);
+		debug($asset_guid);
 		if (count($asset_guid) > 0)
 		{
 			$asset_guid = $asset_guid[0];

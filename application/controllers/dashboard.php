73f3339b722503d93c39c032dead3967826e75ac
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-01 11:49:57 +0500

    error reporting one

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -52,6 +52,12 @@ class Dashboard extends MY_Controller
 	public function index()
 	{
 		
+		
+		
+		
+		
+		
+		
 		$data['digitized_format_name'] = json_decode($this->memcached_library->get('graph_digitized_format_name'), TRUE);
 		$data['digitized_total'] = json_decode($this->memcached_library->get('graph_digitized_total'), TRUE);
 		$data['scheduled_format_name'] = json_decode($this->memcached_library->get('graph_scheduled_format_name'), TRUE);

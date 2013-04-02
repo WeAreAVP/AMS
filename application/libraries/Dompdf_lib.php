e5e01c0a89708e579d20e4be00db58e34f84cd16
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-02 16:14:01 +0500

    generate pdf

--- application/libraries/Dompdf_lib.php
+++ application/libraries/Dompdf_lib.php
@@ -22,7 +22,7 @@ class Dompdf_lib
 		$html = '<center><h2>Digitization Statistics</h2></center>';
 		if (count($data['dsd_report']) > 0)
 		{
-			$html .='<div>Scheduled for Digitization</div>';
+			$html .='<div style="background:green;">Scheduled for Digitization</div>';
 		}
 		$this->_dompdf->load_html($html);
 		$this->_dompdf->render();

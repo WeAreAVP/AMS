8c15cd8426a9a3bfd1daf2cc841b80d1438d12a5
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-02 16:13:14 +0500

    generate pdf

--- application/libraries/Dompdf_lib.php
+++ application/libraries/Dompdf_lib.php
@@ -1,32 +1,39 @@
 <?php
+
 //define('DOM_PDF_HOME', dirname(dirname(__FILE__)));
 //require_once(DOM_PDF_HOME . '/third_party/dompdf/dompdf_config.inc.php');
-class Dompdf_lib {
+class Dompdf_lib
+{
 
-    var $_dompdf = NULL;
+	var $_dompdf = NULL;
 
-    function __construct() {
-        require_once("dompdf/dompdf_config.inc.php");
-        if (is_null($this->_dompdf)) {
-            $this->_dompdf = new DOMPDF();
-        }
-    }
+	function __construct()
+	{
+		require_once("dompdf/dompdf_config.inc.php");
+		if (is_null($this->_dompdf))
+		{
+			$this->_dompdf = new DOMPDF();
+		}
+	}
 
-    function convert_html_to_pdf($data, $filename = 'sample.pdf', $stream = TRUE) {
-        
-        $html='<center><h2>Digitization Statistics</h2></center>';
-        $this->_dompdf->load_html($html);
-        $this->_dompdf->render();
-        return $this->_dompdf->stream($filename, array("Attachment" => 1));
+	function convert_html_to_pdf($data, $filename = 'sample.pdf', $stream = TRUE)
+	{
+
+		$html = '<center><h2>Digitization Statistics</h2></center>';
+		if (count($data['dsd_report']) > 0)
+		{
+			$html .='<div>Scheduled for Digitization</div>';
+		}
+		$this->_dompdf->load_html($html);
+		$this->_dompdf->render();
+		return $this->_dompdf->stream($filename, array("Attachment" => 0));
 //        return $this->_dompdf->stream($filename);
 //        if ($stream) {
 //            $this->_dompdf->stream($filename);
 //        } else {
-            return $this->_dompdf->output();
+		return $this->_dompdf->output();
 //        }
-    }
-
-   
+	}
 
 }
 

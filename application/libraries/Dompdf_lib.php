e6326ef62ff366358fb6ee38471b904e1919e776
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-02 16:11:12 +0500

    generate pdf

--- application/libraries/Dompdf_lib.php
+++ application/libraries/Dompdf_lib.php
@@ -14,10 +14,10 @@ class Dompdf_lib {
 
     function convert_html_to_pdf($data, $filename = 'sample.pdf', $stream = TRUE) {
         
-        $html='<h2>Digitization Statistics</h2>';
+        $html='<center><h2>Digitization Statistics</h2></center>';
         $this->_dompdf->load_html($html);
         $this->_dompdf->render();
-        return $this->_dompdf->stream($filename, array("Attachment" => 0));
+        return $this->_dompdf->stream($filename, array("Attachment" => 1));
 //        return $this->_dompdf->stream($filename);
 //        if ($stream) {
 //            $this->_dompdf->stream($filename);

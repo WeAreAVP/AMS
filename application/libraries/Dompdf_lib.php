0767cc7b72c2c10791a8252630e8b3997c82bc65
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-02 14:32:56 +0500

    dompdf issues

--- application/libraries/Dompdf_lib.php
+++ application/libraries/Dompdf_lib.php
@@ -17,7 +17,7 @@ class Dompdf_lib {
         
         $this->_dompdf->load_html($html);
         $this->_dompdf->render();
-//        return $this->_dompdf->stream($filename, array("Attachment" => 0));
+        return $this->_dompdf->stream($filename, array("Attachment" => 0));
 //        return $this->_dompdf->stream($filename);
 //        if ($stream) {
 //            $this->_dompdf->stream($filename);

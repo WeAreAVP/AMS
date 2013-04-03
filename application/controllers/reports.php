ce21f3685a70b15e0bcc1c78f868cec453546ea6
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-03 17:22:07 +0500

    readonly report

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -84,7 +84,7 @@ class Reports extends MY_Controller
 		if ( ! empty($report_id))
 		{
 
-			$report_info = $this->report_model->get_report_by_id($report_id);
+			$report_info = $this->report_model->get_report_by_id(base64_decode($report_id));
 			if (count($report_info) > 0)
 			{
 

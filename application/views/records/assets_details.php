e91d99579c6b917f70ab0520d218ef8cd9d7c113
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-04 17:27:13 +0500

    remove jwplayer

--- application/views/records/assets_details.php
+++ application/views/records/assets_details.php
@@ -59,7 +59,7 @@
 		{
 			?>
 			
-			<video width="320" height="240" controls>
+			<video width="700px" height="320" controls>
 				<source src="<?php echo $media['url']; ?>" type="video/mp4">
 				
 				Your browser does not support the video tag.

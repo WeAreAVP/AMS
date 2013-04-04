529641c53e27521e76f027ac60061a38c784c13a
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-04 14:47:59 +0500

    proxy files

--- application/controllers/records.php
+++ application/controllers/records.php
@@ -174,7 +174,7 @@ class Records extends MY_Controller
 			$proxy_response = file_get_contents("http://cpbproxy.crawfordmedia.com/xml.php?GUID=$proxy_guid");
 			$x = @simplexml_load_string($proxy_response);
 			$data = xmlObjToArr($x);
-			debug($daata);
+			debug($data);
 			exit;
 			if (isset($search_results_data['records']) && ! is_empty($search_results_data['records']))
 			{

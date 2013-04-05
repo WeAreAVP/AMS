423a12a21ccb98e02524b7a988180bf2e6827d35
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-05 16:45:44 +0500

    files

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -180,7 +180,7 @@ class Instantiations extends MY_Controller
 				$data['asset_details'] = $this->assets_model->get_asset_by_asset_id($detail->assets_id);
 				$search_results_data = $this->sphinx->instantiations_list(array('index' => 'assets_list'), 0, 1000);
 				$data['nominations'] = $this->instantiation->get_nomination_status();
-				$records_obj=new Records();
+				$records_obj=new Records;
 				$data['media']=$records_obj->proxy_files($data['asset_guid']->guid_identifier);
 				$data['next_result_id'] = FALSE;
 				$data['prev_result_id'] = FALSE;

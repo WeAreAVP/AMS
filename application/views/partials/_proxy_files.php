142483f49194f6c0d4c40f9d8bd3db7322ca70ca
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-05 16:59:39 +0500

    files

--- application/views/partials/_proxy_files.php
+++ application/views/partials/_proxy_files.php
@@ -36,11 +36,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="jp-title">
-						<ul>
-							<li>Cro Magnon Man</li>
-						</ul>
-					</div>
+					
 					<div class="jp-no-solution">
 						<span>Update Required</span>
 						To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.

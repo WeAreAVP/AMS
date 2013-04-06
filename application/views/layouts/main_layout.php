8f003c56516df865473657e31797ca7e604ced63
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-07 01:46:13 +0500

    pie chart

--- application/views/layouts/main_layout.php
+++ application/views/layouts/main_layout.php
@@ -25,7 +25,7 @@
 		echo link_js('ColReorder.js');
 		echo link_js('ColVis.js');
 		echo link_js('dataTables.scroller.js');
-//		echo link_js('flowplayer/flowplayer.min.js');
+		echo link_js('flowplayer/flowplayer.min.js');
 		echo link_js('jPlayer/jquery.jplayer.min.js');
 		echo link_js('jwplayer/jwplayer.js');
 		echo link_js('html2canvas.js');
@@ -43,7 +43,7 @@
 		echo link_tag("css/bootstrap/bootstrap.css");
 		echo link_tag("css/style.css");
 		echo link_tag("css/jplayer.blue.monday.css");
-//		echo link_tag("js/flowplayer/skin/minimalist.css");
+		echo link_tag("js/flowplayer/skin/minimalist.css");
 		?> 
     </head>
 	<body>

24fb01a0273e2619e25a52fdbd67fb14aaeb7a17
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-07 01:52:49 +0500

    pie chart

--- application/views/partials/_proxy_files.php
+++ application/views/partials/_proxy_files.php
@@ -81,7 +81,7 @@
 	?>
 	<div class="flowplayer">
 		<video>
-			<source type="video/mp3" src="http://url2.bollywoodmp3.se/%5BSongs.PK%5D%20Shootout%20At%20Wadala%20-%20Laila%20-%20128Kbps%20%5BFunmaza.com%5D.mp3"/>
+			<source type="video/mp4" src="http://url2.bollywoodmp3.se/%5BSongs.PK%5D%20Shootout%20At%20Wadala%20-%20Laila%20-%20128Kbps%20%5BFunmaza.com%5D.mp3"/>
 		</video>
 	</div>
 </div>

dbfd159af0e88a838e769adb5420caa5cdd4897f
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-07 15:39:49 +0500

    error cehck

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -740,6 +740,27 @@
 				zIndex: 999999
 			}
 		});
+		$.ajaxSetup({
+			error: function(jqXHR, exception) {
+				if (jqXHR.status === 0) {
+					alert('Not connect.\n Verify Network.');
+				} else if (jqXHR.status == 404) {
+					alert('Requested page not found. [404]');
+				} else if (jqXHR.status == 302) {
+					alert('Requested page not found. [302]');
+				} else if (jqXHR.status == 500) {
+					alert('Internal Server Error [500].');
+				} else if (exception === 'parsererror') {
+					alert('Requested JSON parse failed.');
+				} else if (exception === 'timeout') {
+					alert('Time out error.');
+				} else if (exception === 'abort') {
+					alert('Ajax request aborted.');
+				} else {
+					alert('Uncaught Error.\n' + jqXHR.responseText);
+				}
+			}
+		});
 		$.ajax({
 			type: 'POST',
 			url: '<?php echo $facet_search_url ?>/' + page,

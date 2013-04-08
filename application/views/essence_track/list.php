6a6c5de08d100e5221e37e8fda4d5582326d880d
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-08 12:34:38 +0500

    essence tracks

--- application/views/essence_track/list.php
+++ application/views/essence_track/list.php
@@ -24,31 +24,7 @@ if ($essence_track)
 
 		<div class="tab-content">
 			<div class="tab-pane active" id="region_digitized" style=" margin: 0 auto">
-				<div class="container-map">
-					<div class="first-asset">
-						<h2><?php echo number_format($total_region_digitized['west']); ?></h2>
-						<span>Assets</span>
-					</div><!--end of first-asset-->
-					<div class="second-asset">
-						<h2><?php echo number_format($total_region_digitized['midwest']); ?></h2>
-						<span>Assets</span>
-					</div><!--end of first-asset-->
-					<div class="third-asset">
-						<h2><?php echo number_format($total_region_digitized['south']); ?></h2>
-						<span>Assets</span>
-					</div><!--end of first-asset-->
-					<div class="forth-asset">
-						<h2><?php echo number_format($total_region_digitized['northeast']); ?></h2>
-						<span>Assets</span>
-					</div><!--end of first-asset-->
-					<div class="fifth-asset">
-						<h2><?php echo number_format($total_region_digitized['other']); ?></h2>
-						<span>Assets</span>
-					</div><!--end of first-asset-->
-					<div class="other">
-						<h3>Other</h3>
-					</div><!--end of other-->
-				</div><!--end of container-map-->
+				
 			</div>
 		</div>
 	</div>

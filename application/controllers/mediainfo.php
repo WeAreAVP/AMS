f4bb57344ca01ed9c3e1c0424d0e6151c59dc651
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-08 14:22:08 +0500

    essence tracks

--- application/controllers/mediainfo.php
+++ application/controllers/mediainfo.php
@@ -734,12 +734,11 @@ class Mediainfo extends CI_Controller
 					}
 					/* Essence Track Bitdepth End */
 					/* Essence Track Duration Start */
-					debug($track['children']);
 					if (isset($track['children']['duration_string3']) && isset($track['children']['duration_string3'][0]) && isset($track['children']['duration_string3'][0]['text']) && ! empty($track['children']['duration_string3'][0]['text']))
 					{
-						$essence_track_duration=explode('.',$general_track['duration_string3'][0]['text']);
-						debug($general_track['duration_string3'][0]['text'],FALSE);
-						echo date('H:i:s', strtotime($essence_track_duration[0]));exit;
+						$essence_track_duration=explode('.',$track['children']['duration_string3'][0]['text']);
+						debug($track['children']['duration_string3'][0]['text'],FALSE);
+						echo date('H:i:s', strtotime($track['children']['duration_string3'][0]['text']));exit;
 						$dessence_track[$dessence_track_counter]['duration'] = $essence_track['duration'] = date('H:i:s', strtotime($general_track['duration_string3'][0]['text']));
 					}
 					/* Essence Track Duration End */

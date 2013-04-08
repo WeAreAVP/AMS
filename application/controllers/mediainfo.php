ddafd48f5a7d12c86c0e3866d9c00abb279a7bf1
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-08 14:00:45 +0500

    essence tracks

--- application/controllers/mediainfo.php
+++ application/controllers/mediainfo.php
@@ -787,9 +787,10 @@ class Mediainfo extends CI_Controller
 					/* Essence Track Encoding End */
 					/* Essence Track Identifier Start */
 					$essence_track_identifier = array();
+					echo 'fadfadfad';
 					if (isset($track['children']['id']) && isset($track['children']['id'][0]) && isset($track['children']['id'][0]['text']) && ! empty($track['children']['id'][0]['text']))
 					{
-						echo '1';
+						echo '1fadfadf';exit;
 						$dessence_track[$dessence_track_counter]['identifier'] = $essence_track_identifier['essence_track_identifiers'] = $track['children']['id'][0]['text'];
 						$dessence_track[$dessence_track_counter]['identifier_source'] = $essence_track_identifier['essence_track_identifier_source'] = 'mediainfo';
 					}

309a92ffc029a3f1cd25ce0f84b627f166187d84
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-09 13:46:22 +0500

    back to normal

--- application/views/partials/_proxy_files.php
+++ application/views/partials/_proxy_files.php
@@ -4,15 +4,45 @@
 	if ($media)
 	{
 		?>
-		<div class="flowplayer">
+<!--		<div class="flowplayer">
 			<video>
 				<source type="video/mp4" src="<?php echo $media['url']; ?>"/>
 
 			</video>
-		</div>
+		</div>-->
 		<div class="clearfix"></div>
 		<div style="margin-left: 20px;margin-top: 10px;"><a href="<?php echo $media['url']; ?>" target="=_blank">Open Proxy file</a></div>
+		<video id="my-video">
+			<source src="<?php echo $media['url']; ?>" type="video/mp4">
+		</video>
 
+		<script>
+			(function() {
+				var video = document.createElement("video");
+				if (typeof(video.canPlayType) == 'undefined' || // detect browsers with no <video> support
+				video.canPlayType('video/mp4') == '') { // detect the ability to play H.264/MP4
+
+					var video = $('#my-video');
+					var videoUrl = video.find('source').attr('src');
+					var flashUrl = '/js/flowplayer/flowplayer.swf';
+
+					// create flash
+					var flash = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ' +
+					'	codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" ' +
+					'	width="' + video.width() + '" height="' + video.height() + '" id="fallbackplayer">' +
+					'		<param name="allowfullscreen" value="true" /> ' +
+					'		<param name="movie" value="' + flashUrl + '?mediaUrl=' + videoUrl + '" /> ' +
+					'		<embed id="dtsplayer" width="' + video.width() + '" height="' + video.height() + '" allowfullscreen="true" allowscriptaccess="always" ' +
+					'			quality="high" name="fallbackplayer" ' + '			src="' + flashUrl + '?mediaUrl=' + videoUrl + '" ' +
+					'			type="application/x-shockwave-flash" /> ' +
+					'</object>';
+
+					// insert flash and remove video
+					video.before(flash);
+					video.detach();
+				}
+			})();
+		</script>
 		<?php
 	}
 	?>

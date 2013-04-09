a703e9e6e757560427a3d2d9c4b15b7722d466a8
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-09 13:21:54 +0500

    up to production

--- application/views/partials/_proxy_files.php
+++ application/views/partials/_proxy_files.php
@@ -12,36 +12,7 @@
 		</div>
 		<div class="clearfix"></div>
 		<div style="margin-left: 20px;margin-top: 10px;"><a href="<?php echo $media['url']; ?>" target="=_blank">Open Proxy file</a></div>
-<div id="wowza" style="width:644px;height:276px;margin:0 auto;text-align:center">
-	  <script src="http://releases.flowplayer.org/js/flowplayer-3.2.12.min.js"></script>
-    <img src="/media/img/player/splash_black.jpg" height="276" width="548" />
-</div>
-		<script>
-		$f("wowza", "http://releases.flowplayer.org/swf/flowplayer-3.2.16.swf", {
- 
-    clip: {
-        url: 'mp4:<?php echo $media['url']; ?>',
-        scaling: 'fit',
-        // configure clip to use hddn as our provider, referring to our rtmp plugin
-        provider: 'hddn'
-    },
- 
-    // streaming plugins are configured under the plugins node
-    plugins: {
- 
-        // here is our rtmp plugin configuration
-        hddn: {
-            url: "flowplayer.rtmp-3.2.12.swf",
- 
-            // netConnectionUrl defines where the streams are found
-            netConnectionUrl: 'rtmp://rtmp01.hddn.com/play'
-        }
-    },
-    canvas: {
-        backgroundGradient: 'none'
-    }
-});
-		</script>
+
 		<?php
 	}
 	?>

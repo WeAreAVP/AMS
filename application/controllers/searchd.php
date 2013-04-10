263c010f4c6c453b777de32045003b00f3352e53
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-10 13:40:05 +0500

    rt index in sphnix.conf

--- application/controllers/searchd.php
+++ application/controllers/searchd.php
@@ -49,10 +49,11 @@ class Searchd extends MY_Controller
 	 */
 	function index(){
 		
-		$data=$this->sphnixrt->select('station_sample',array('start'=>0,'limit'=>1));
-		$this->sphnixrt->update('station_sample',array('cpb_id'=>'1234'));
 		$data=$this->sphnixrt->select('station_sample',array('start'=>0,'limit'=>1000));
-		debug($data);
+		echo count($data['records']);exit;
+		$this->sphnixrt->insert('station_sample',array('cpb_id'=>'1234'),999);
+		$data=$this->sphnixrt->select('station_sample',array('start'=>0,'limit'=>1000));
+		echo count($data['records']);exit;
 	}
 	
 

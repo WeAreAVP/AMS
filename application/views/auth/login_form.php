48ff1c4a006d9f30afafb2ee47c48536c68af2f8
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-10 16:54:58 +0500

    login view chagnes

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -1,90 +1,92 @@
 <?php
 $username = array(
-    'name' => 'username',
-    'id' => 'username',
-    'size' => 30,
-    'value' => set_value('username')
+	'name' => 'username',
+	'id' => 'username',
+	'size' => 30,
+	'value' => set_value('username')
 );
 
 $password = array(
-    'name' => 'password',
-    'id' => 'password',
-    'size' => 30
+	'name' => 'password',
+	'id' => 'password',
+	'size' => 30
 );
 
 $remember = array(
-    'name' => 'remember',
-    'id' => 'remember',
-    'value' => 1,
-    'checked' => set_value('remember'),
-    'style' => 'float:left;margin-right:5px;'
+	'name' => 'remember',
+	'id' => 'remember',
+	'value' => 1,
+	'checked' => set_value('remember'),
+	'style' => 'float:left;margin-right:5px;'
 );
 
 $confirmation_code = array(
-    'name' => 'captcha',
-    'id' => 'captcha',
-    'maxlength' => 8
+	'name' => 'captcha',
+	'id' => 'captcha',
+	'maxlength' => 8
 );
 $attributes = 'class="form-horizontal"';
 ?>
-<div class="row"> 
-    <div class="span12">
-<fieldset><legend>Sign in</legend>
-    <?php echo form_open($this->uri->uri_string(), $attributes) ?>
-    <?php if($this->dx_auth->get_auth_error()){?>
-<div class="alert alert-error"><?php echo $this->dx_auth->get_auth_error(); ?></div>
+
+<div class="span12">
+	<h3>Sign in</h3>
+	<?php echo form_open($this->uri->uri_string(), $attributes) ?>
+	<?php if ($this->dx_auth->get_auth_error())
+	{
+		?>
+		<div class="alert alert-error"><?php echo $this->dx_auth->get_auth_error(); ?></div>
 <?php } ?>
-    <div class="control-group">
-        <label class="control-label" for="<?php echo $username['id']; ?>"><?php echo form_label('Email:', $username['id']); ?></label>
-        <div class="controls">
-            <?php echo form_input($username) ?>
-            <div class="help-block" style="color: #B94A48;"> <?php echo form_error($username['name']); ?></div>
-        </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="<?php echo $password['id']; ?>"><?php echo form_label('Password:', $password['id']); ?></label>
-        <div class="controls">
-            <?php echo form_password($password) ?>
-            <div class="help-block" style="color: #B94A48;"> <?php echo form_error($password['name']); ?></div>
-        </div>
-    </div>
+	<div class="control-group">
+		<label class="control-label" for="<?php echo $username['id']; ?>"><?php echo form_label('Email:', $username['id']); ?></label>
+		<div class="controls">
+<?php echo form_input($username) ?>
+			<div class="help-block" style="color: #B94A48;"> <?php echo form_error($username['name']); ?></div>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label" for="<?php echo $password['id']; ?>"><?php echo form_label('Password:', $password['id']); ?></label>
+		<div class="controls">
+<?php echo form_password($password) ?>
+			<div class="help-block" style="color: #B94A48;"> <?php echo form_error($password['name']); ?></div>
+		</div>
+	</div>
 
-    <?php if ($show_captcha): ?>
+<?php if ($show_captcha): ?>
 
-        <dt>Enter the code exactly as it appears. There is no zero.</dt>
-        <dd><?php echo $this->dx_auth->get_captcha_image(); ?></dd>
+		<dt>Enter the code exactly as it appears. There is no zero.</dt>
+		<dd><?php echo $this->dx_auth->get_captcha_image(); ?></dd>
 
-        <dt><?php echo form_label('Confirmation Code', $confirmation_code['id']); ?></dt>
-        <dd>
-            <?php echo form_input($confirmation_code); ?>
-            <?php echo form_error($confirmation_code['name']); ?>
-        </dd>
+		<dt><?php echo form_label('Confirmation Code', $confirmation_code['id']); ?></dt>
+		<dd>
+			<?php echo form_input($confirmation_code); ?>
+	<?php echo form_error($confirmation_code['name']); ?>
+		</dd>
 
-    <?php endif; ?>
+<?php endif; ?>
 
-    
-   
-<!--        <div class="controls">
-            <?php echo form_checkbox($remember); ?><?php echo form_label('Remember me', $remember['id']); ?>
-        </div>-->
-        <div class="controls">
-           <?php echo anchor($this->dx_auth->forgot_password_uri, 'Forgot password'); ?> 
 
-         
-        
-        <?php
-        if ($this->dx_auth->allow_registration) {
-            echo ' |  '.anchor($this->dx_auth->register_uri, 'Register');
 
-        };
-        ?>
-           </div>
+	<!--        <div class="controls">
+<?php echo form_checkbox($remember); ?><?php echo form_label('Remember me', $remember['id']); ?>
+			</div>-->
+	<div class="controls">
+<?php echo anchor($this->dx_auth->forgot_password_uri, 'Forgot password'); ?> 
 
-   
-        <div class="controls"><?php echo form_submit('login', 'Sign in','class="btn btn-inverse"'); ?></div>
+
+
+		<?php
+		if ($this->dx_auth->allow_registration)
+		{
+			echo ' |  ' . anchor($this->dx_auth->register_uri, 'Register');
+		};
+		?>
+	</div>
+
+
+	<div class="controls"><?php echo form_submit('login', 'Sign in', 'class="btn btn-inverse"'); ?></div>
 
 
 <?php echo form_close() ?>
-</fieldset>
+
 </div>
-</div>
+

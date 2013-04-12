43e70d9cfd623696b89398504a2ced515d700eaa
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-12 13:17:32 +0500

    asset management

--- application/views/assets/edit.php
+++ application/views/assets/edit.php
@@ -61,7 +61,7 @@
 					{
 						?>
 						<p>
-							<input id="asset_date_<?php echo $index; ?>" name="asset_date[]" value="<?php echo $dates; ?>"/>
+							<input id="asset_date_<?php echo $index; ?>" name="asset_date[]" value="<?php echo $dates; ?>" readonly="readonly"/>
 						</p>
 						<?php
 						if (isset($asset_date_types[$index]) && $dates > 0)

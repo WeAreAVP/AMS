7b3058c37f96f4f6114d4225bb71ae9a12b7c65d
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-14 12:52:28 +0500

    add buttons

--- application/views/assets/edit.php
+++ application/views/assets/edit.php
@@ -87,7 +87,7 @@
 								</p>
 
 							</div>
-							<div class="remove_element" onclick="removeElement('#remove_local_');"><img src="/images/remove-item.png"/></div>
+							<div class="remove_element" ><img src="/images/remove-item.png" onclick="removeElement('#remove_local_<?php echo $index; ?>');"/></div>
 						</div>
 						<?php
 					}

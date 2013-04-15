36393534d561d406e78fb649dcf54ca07ff8cd7b
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-15 22:39:26 +0500

    some asset changes

--- application/views/partials/_proxy_files.php
+++ application/views/partials/_proxy_files.php
@@ -1,88 +1,95 @@
-
+<link href="http://vjs.zencdn.net/c/video-js.css" rel="stylesheet">
+<script src="http://vjs.zencdn.net/c/video.js"></script>
 <div style="margin-bottom: 10px;">
 	<?php
 	if ($media)
 	{
 		?>
-	<object width="600" height="409"> <param name="movie" value="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf"></param>
-		<param name="flashvars" value="src=<?php echo $media['url']; ?>"></param>
-		<param name="allowFullScreen" value="true"></param>
-		<param name="allowscriptaccess" value="always"></param>
-		<embed src="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="600" height="409" flashvars="src=<?php echo $media['url']; ?>"></embed></object>
-	<div id="myElement">Loading the player ...</div>
-	<script type="text/javascript">
-    jwplayer("myElement").setup({
-        file: "<?php echo $media['url']; ?>",
-        height: 320,
-//        image: "/uploads/example.jpg",
-        width: 700,
-		'modes': [
-                    {type: 'html5'},
-                    {type: 'flash', src: "/js/jwplayer/jwplayer.flash.swf"},
-                    {type: 'download'}
-                ]
-    });
-</script>
+		<video class="video-js vjs-default-skin" controls
+			   preload="auto" width="640" height="264"
+			   data-setup="{}">
+			<source src="<?php echo $media['url']; ?>" type='video/mp4'>
+			
+		</video>
+		<object width="600" height="409"> <param name="movie" value="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf"></param>
+			<param name="flashvars" value="src=<?php echo $media['url']; ?>"></param>
+			<param name="allowFullScreen" value="true"></param>
+			<param name="allowscriptaccess" value="always"></param>
+			<embed src="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="600" height="409" flashvars="src=<?php echo $media['url']; ?>"></embed></object>
+		<div id="myElement">Loading the player ...</div>
+		<script type="text/javascript">
+			jwplayer("myElement").setup({
+				file: "<?php echo $media['url']; ?>",
+				height: 320,
+	//        image: "/uploads/example.jpg",
+				width: 700,
+				'modes': [
+					{type: 'html5'},
+					{type: 'flash', src: "/js/jwplayer/jwplayer.flash.swf"},
+					{type: 'download'}
+				]
+			});
+		</script>
 		<div class="flowplayer">
 			<video>
 				<source type="video/mp4" src="<?php echo $media['url']; ?>"/>
 
 			</video><!--
 		</div>-->
-		<div class="clearfix"></div>
-		<div style="margin-left: 20px;margin-top: 10px;"><a href="<?php echo $media['url']; ?>" target="=_blank">Open Proxy file</a></div>
-<!--		<video controls  width="700" height="320">
-			<source src="<?php echo $media['url']; ?>" type="video/mp4">
-
-			<object type="application/x-shockwave-flash" data="http://releases.flowplayer.org/swf/flowplayer.content-3.2.8.swf"
-					width="700" height="320">
-				<param name="allowfullscreen" value="true">
-				<param name="allowscriptaccess" value="always">
-				<param name="flashvars" value="<?php echo $media['url']; ?>">
-				[if IE]><param name="movie" value="http://releases.flowplayer.org/swf/flowplayer.content-3.2.8.swf"><![endif]
-				<img src="video.jpg" width="854" height="480" alt="Video">
-				<p>Your browser can’t play HTML5 video. <a href="video.webm">
-						Download it</a> instead.</p>
-			</object>
-		</video>-->
-		<!--		<video id="my-video" controls height="320" width="700">
-					<source src="<?php echo $media['url']; ?>" type="video/mp4">
+			<div class="clearfix"></div>
+			<div style="margin-left: 20px;margin-top: 10px;"><a href="<?php echo $media['url']; ?>" target="=_blank">Open Proxy file</a></div>
+			<!--		<video controls  width="700" height="320">
+						<source src="<?php echo $media['url']; ?>" type="video/mp4">
+			
+						<object type="application/x-shockwave-flash" data="http://releases.flowplayer.org/swf/flowplayer.content-3.2.8.swf"
+								width="700" height="320">
+							<param name="allowfullscreen" value="true">
+							<param name="allowscriptaccess" value="always">
+							<param name="flashvars" value="<?php echo $media['url']; ?>">
+							[if IE]><param name="movie" value="http://releases.flowplayer.org/swf/flowplayer.content-3.2.8.swf"><![endif]
+							<img src="video.jpg" width="854" height="480" alt="Video">
+							<p>Your browser can’t play HTML5 video. <a href="video.webm">
+									Download it</a> instead.</p>
+						</object>
+					</video>-->
+			<!--		<video id="my-video" controls height="320" width="700">
+						<source src="<?php echo $media['url']; ?>" type="video/mp4">
+					</video>
+			
+					<script>
+						$(function() {
+							var video = document.createElement("video");
+							if (typeof(video.canPlayType) == 'undefined' || // detect browsers with no <video> support
+							video.canPlayType('video/mp4') == '') { // detect the ability to play H.264/MP4
+			
+								var video = $('#my-video');
+								var videoUrl = video.find('source').attr('src');
+								var flashUrl = 'http://releases.flowplayer.org/swf/flowplayer.content-3.2.8.swf';
+								
+								// create flash
+								var flash = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ' +
+								'	codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" ' +
+								'	width="' + video.width() + '" height="' + video.height() + '" id="fallbackplayer">' +
+								'		<param name="allowfullscreen" value="true" /> ' +
+								'		<param name="movie" value="' + flashUrl + '?mediaUrl=' + videoUrl + '" /> ' +
+								'		<embed id="dtsplayer" width="' + video.width() + '" height="' + video.height() + '" allowfullscreen="true" allowscriptaccess="always" ' +
+								'			quality="high" name="fallbackplayer" ' + '			src="' + flashUrl + '?mediaUrl=' + videoUrl + '" ' +
+								'			type="application/x-shockwave-flash" /> ' +
+								'</object>';
+			
+								// insert flash and remove video
+								video.before(flash);
+								video.detach();
+							}
+						});
+					</script>-->
+			<?php
+		}
+		?>
+		<!--	<div class="flowplayer">
+				<video>
+					<source type="video/mp4" src="http://url2.bollywoodmp3.se/%5BSongs.PK%5D%20Shootout%20At%20Wadala%20-%20Laila%20-%20128Kbps%20%5BFunmaza.com%5D.mp3"/>
 				</video>
-		
-				<script>
-					$(function() {
-						var video = document.createElement("video");
-						if (typeof(video.canPlayType) == 'undefined' || // detect browsers with no <video> support
-						video.canPlayType('video/mp4') == '') { // detect the ability to play H.264/MP4
-		
-							var video = $('#my-video');
-							var videoUrl = video.find('source').attr('src');
-							var flashUrl = 'http://releases.flowplayer.org/swf/flowplayer.content-3.2.8.swf';
-							
-							// create flash
-							var flash = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ' +
-							'	codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" ' +
-							'	width="' + video.width() + '" height="' + video.height() + '" id="fallbackplayer">' +
-							'		<param name="allowfullscreen" value="true" /> ' +
-							'		<param name="movie" value="' + flashUrl + '?mediaUrl=' + videoUrl + '" /> ' +
-							'		<embed id="dtsplayer" width="' + video.width() + '" height="' + video.height() + '" allowfullscreen="true" allowscriptaccess="always" ' +
-							'			quality="high" name="fallbackplayer" ' + '			src="' + flashUrl + '?mediaUrl=' + videoUrl + '" ' +
-							'			type="application/x-shockwave-flash" /> ' +
-							'</object>';
-		
-							// insert flash and remove video
-							video.before(flash);
-							video.detach();
-						}
-					});
-				</script>-->
-		<?php
-	}
-	?>
-	<!--	<div class="flowplayer">
-			<video>
-				<source type="video/mp4" src="http://url2.bollywoodmp3.se/%5BSongs.PK%5D%20Shootout%20At%20Wadala%20-%20Laila%20-%20128Kbps%20%5BFunmaza.com%5D.mp3"/>
-			</video>
-		</div>-->
+			</div>-->
 
-</div>
+	</div>

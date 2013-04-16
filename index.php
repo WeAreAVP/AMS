bee201b1cf6f225d2789dab0beed4575c844da84
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-16 19:12:22 +0500

    asset management

--- index.php
+++ index.php
@@ -42,13 +42,12 @@ if (defined('ENVIRONMENT'))
 	{
 		case 'development':
 		case 'testing':
-			
+			case 'qatesting':
 			error_reporting(E_ALL);
 			ini_set('display_errors', 1);
 			break;
 		
 		case 'production':
-			case 'qatesting':
 			error_reporting(0);
 			ini_set('display_errors', 0);
 			break;

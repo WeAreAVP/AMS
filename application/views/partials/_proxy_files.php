9772a8f61004f5289189ded4031695daa2eb7f10
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-16 19:09:18 +0500

    asset management

--- application/views/partials/_proxy_files.php
+++ application/views/partials/_proxy_files.php
@@ -42,37 +42,7 @@
 			<div class="clearfix" style="margin-bottom: 15px;"></div>
 			<div style="margin-left: 20px;margin-top: 10px;"><a href="<?php echo $media['url']; ?>" target="=_blank">Open Proxy file</a></div>
 					
-					<video id="my-video" controls height="320" width="700">
-						<source src="<?php echo $media['url']; ?>" type="video/mp4">
-					</video>
-			
-					<script>
-						$(function() {
-							var video = document.getElementById('#my-video');
-							if (typeof(video.canPlayType) == 'undefined' || // detect browsers with no <video> support
-							video.canPlayType('video/mp4') == '') { // detect the ability to play H.264/MP4
-			
-								var video = $('#my-video');
-								var videoUrl = video.find('source').attr('src');
-								var flashUrl = 'http://releases.flowplayer.org/swf/flowplayer.content-3.2.8.swf';
-								
-								// create flash
-								var flash = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ' +
-								'	codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" ' +
-								'	width="' + video.width() + '" height="' + video.height() + '" id="fallbackplayer">' +
-								'		<param name="allowfullscreen" value="true" /> ' +
-								'		<param name="movie" value="' + flashUrl + '?mediaUrl=' + videoUrl + '" /> ' +
-								'		<embed id="dtsplayer" width="' + video.width() + '" height="' + video.height() + '" allowfullscreen="true" allowscriptaccess="always" ' +
-								'			quality="high" name="fallbackplayer" ' + '			src="' + flashUrl + '?mediaUrl=' + videoUrl + '" ' +
-								'			type="application/x-shockwave-flash" /> ' +
-								'</object>';
-			
-								// insert flash and remove video
-								video.before(flash);
-								video.detach();
-							}
-						});
-					</script>
+					 
 			<?php
 		}
 		?>

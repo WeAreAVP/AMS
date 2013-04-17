d11e9f6e918f7f69e38b83616fb3e289d36f72d6
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-17 16:52:47 +0500

    edit instantitions

--- application/views/instantiations/edit.php
+++ application/views/instantiations/edit.php
@@ -79,7 +79,7 @@
 
 				</tr>
 				<?php
-				if ( empty($instantiation_detail->digitized) || $instantiation_detail->digitized != 0)
+				if ( empty($instantiation_detail->digitized) || $instantiation_detail->digitized == 1 || $instantiation_detail->digitized==NULL)
 				{
 					?>
 
@@ -335,7 +335,7 @@
 					</td>
 				</tr>
 				<?php
-				if ( empty($instantiation_detail->digitized) || $instantiation_detail->digitized != 0)
+				if ( empty($instantiation_detail->digitized) || $instantiation_detail->digitized == 1 || $instantiation_detail->digitized==NULL)
 				{
 					?>
 					<tr>
@@ -436,7 +436,7 @@
 					</td>
 				</tr>
 				<?php
-				if ( empty($instantiation_detail->digitized) || $instantiation_detail->digitized != 0)
+				if ( empty($instantiation_detail->digitized) || $instantiation_detail->digitized == 1 || $instantiation_detail->digitized==NULL)
 				{
 					?>
 					<tr>

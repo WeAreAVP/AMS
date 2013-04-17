ae74f1e1c55f33b41a8160b6057525e1f4b9d2d9
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-17 12:13:05 +0500

    new edit view

---
+++ application/views/instantiations/edit.php
@@ -0,0 +1,86 @@
+<div class="row">
+	<div style="margin: 2px 0px 10px 0px;float:left;width: 570px;">
+
+		<?php
+		$asset_title_type = explode('|', trim(str_replace('(**)', '', $asset_detail->title_type)));
+		$asset_title = explode('|', trim(str_replace('(**)', '', $asset_detail->title)));
+		$asset_title_ref = explode('|', trim(str_replace('(**)', '', $asset_detail->title_ref)));
+		$combine_title = '';
+		foreach ($asset_title as $index => $title)
+		{
+			if (isset($asset_title_type[$index]) && $asset_title_type[$index] != '')
+				$combine_title.= $asset_title_type[$index] . ': ';
+			if (isset($asset_title_ref[$index]))
+			{
+				if ($asset_title_ref[$index] != '')
+				{
+					$combine_title.="<a target='_blank' href='$asset_title_ref[$index]'>$title</a>: ";
+					$combine_title.=' (' . $asset_title_ref[$index] . ')';
+				}
+				else
+					$combine_title.=$title;
+			}
+			else
+				$combine_title.=$title;
+			$combine_title.='<div class="clearfix"></div>';
+		}
+		?>
+		<h2><?php echo $combine_title; ?></h2>
+	</div>
+	<div class="clearfix"></div>
+	<?php $this->load->view('partials/_list'); ?>
+	<div class="span9" style="margin-left: 250px;" id="ins_view_detail">
+		<form class="form-horizontal" method="POST" action="<?php echo site_url('assets/edit/' . $asset_id); ?>" id="edit_asset_form">
+			<table cellPadding="8" class="record-detail-table">
+
+				<tr>
+					<td class="record-detail-page">
+						<label><i class="icon-question-sign"></i><b> Nomination Status:</b></label>
+					</td>
+					<td>
+						<p>
+							<select id="nomination" name="nomination">
+								<option value="">Select</option>
+								<?php
+								foreach ($nominations as $row)
+								{
+									$selected = '';
+									if (isset($ins_nomination->status) && $ins_nomination->status == $row->status)
+										$selected = 'selected="selected"'
+										?>
+									<option value="<?php echo $row->status; ?>" <?php echo $selected; ?>><?php echo $row->status; ?></option>
+								<?php }
+								?>
+							</select>
+						</p>
+
+					</td>
+				</tr>
+				<tr>
+					<td class="record-detail-page">
+						<label><i class="icon-question-sign"></i><b> Nomination Reason:</b></label>
+					</td>
+					<td>
+						<p>
+							<textarea style="width: 540px;height: 90px;" id="nomination_reason" name="nomination_reason"><?php echo (isset($ins_nomination->nomination_reason)) ? $ins_nomination->nomination_reason : ''; ?></textarea>
+						</p>
+
+					</td>
+				</tr>
+
+				<tr>
+					<td colspan="2">
+						<a class="btn" href="<?php echo site_url('instantiations/detail/' . $inst_id); ?>">Cancel</a>
+						<input type="button" onclick="validateForm();" value="Save Changes" class="btn btn-primary"/>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</div>
+<script type="text/javascript">
+
+
+
+</script>
+<!--<script type="text/javascript" src="/js/edit_asset.js"></script>-->

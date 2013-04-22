8f478cc046859a99d7207c052eba21dbe32d9499
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-22 13:44:25 +0500

    record management

--- js/edit_asset.js
+++ js/edit_asset.js
@@ -539,7 +539,7 @@ function validateForm() {
 		$('input[name="' + field[cnt] + '[]"]').each(function() {
 			if (isValid) {
 				if ($(this).length > 0) {
-					console.log($(this).val());
+					console.log($(this));
 					if ($(this).val() == '') {
 						$('body').animate({
 							scrollTop: $(this).parent().parent().offset().top - 100

7d0496bcfc2d00d2e222df11611a023e89be2fbf
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-23 15:39:24 +0500

    adding assets

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -174,6 +174,8 @@ class Instantiations extends MY_Controller
 				$data['inst_data_rate_unit'] = $this->instantiation->get_data_rate_unit_by_data_id($detail->data_rate_units_id);
 				$data['inst_color'] = $this->instantiation->get_color_by_instantiation_colors_id($detail->instantiation_colors_id);
 				$data['inst_annotation'] = $this->instantiation->get_annotation_by_instantiation_id($instantiation_id);
+				$data['inst_relation'] = $this->manage_asset->get_relation_by_instantiation_id($instantiation_id);
+				debug($data['inst_relation']);exit;
 				$data['essence_track'] = $this->essence_track->get_essence_tracks_by_instantiations_id($instantiation_id);
 //				debug($data['essence_track']);
 
@@ -975,7 +977,7 @@ class Instantiations extends MY_Controller
 				redirect('records/details/' . $asset_id);
 			}
 		}
-		$data['asset_id']=$asset_id;
+		$data['asset_id'] = $asset_id;
 		$data['pbcore_asset_date_types'] = $this->manage_asset->get_picklist_values(2);
 		$data['pbcore_media_types'] = $this->manage_asset->get_picklist_values(11);
 		$data['pbcore_generations'] = $this->manage_asset->get_picklist_values(12);

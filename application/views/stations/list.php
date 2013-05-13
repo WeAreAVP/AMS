3c94f45f694d0bf83c34025f36e150a69258d090
Nouman Tayyan <noumantayyab@gmail.com>
2013-05-13 15:41:33 +0500

    station design changes

--- application/views/stations/list.php
+++ application/views/stations/list.php
@@ -113,7 +113,7 @@ if ( ! $is_ajax)
 							<td><input type='checkbox' name='all' value='' id='check_all'  class="check-all" onclick='javascript:checkAll();' /></td>
 						<?php } ?>
 						<th>CPB ID</th>
-						<th style="width:230px;">Station Name</th>
+						<th>Station Name</th>
 						<th>Total Allocated Hours</th>
 						<th>Certified</th>
 						<th>Agreed</th>

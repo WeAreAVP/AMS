702419975cf1f92a6784dbc8e4e470cf126431ae
Nouman Tayyan <noumantayyab@gmail.com>
2013-05-13 17:03:24 +0500

    station design changes

--- application/views/partials/_proxy_files.php
+++ application/views/partials/_proxy_files.php
@@ -1,6 +1,15 @@
 <link href="http://vjs.zencdn.net/c/video-js.css" rel="stylesheet">
 <script src="http://vjs.zencdn.net/c/video.js"></script>
 <div style="margin-bottom: 10px;">
+	<style type="text/css">
+
+		/* custom player skin */
+		.flowplayer { width: 80%; background-color: #222; background-size: cover; max-width: 800px; }
+		.flowplayer .fp-controls { background-color: rgba(238, 238, 238, 1)}
+		.flowplayer .fp-timeline { background-color: rgba(204, 204, 204, 1)}
+		.flowplayer .fp-progress { background-color: rgba(17, 17, 17, 1)}
+		.flowplayer .fp-buffer { background-color: rgba(249, 249, 249, 1)}
+	</style>
 	<?php
 	if ($media)
 	{
@@ -14,12 +23,17 @@
 			</video>
 		</div>
 		<div id="flowPlayer_div" style="display: none;">
-			<div class="flowplayer">
+			<div data-swf="<?php echo site_url('js/flowplayer/flowplayer.swf'); ?>"
+				 class="flowplayer no-toggle aside-time play-button color-light"
+				 data-ratio="0.416">
 				<video>
-					<source type="video/mp4" src="<?php echo $media['url']; ?>"/>
+					<source type="video/mp4" src="<?php echo $media['url']; ?>">
 
 				</video>
+
 			</div>
+
+			
 		</div>
 
 

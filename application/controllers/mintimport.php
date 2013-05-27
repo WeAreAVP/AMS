12b5ac7e388164d84fd19ee191bd070b0237249c
Nouman Tayyan <noumantayyab@gmail.com>
2013-05-27 18:23:10 +0500

    mint

--- application/controllers/mintimport.php
+++ application/controllers/mintimport.php
@@ -40,33 +40,40 @@ class Mintimport extends CI_Controller
 		parent::__construct();
 		$this->load->model('assets_model');
 		$this->load->model('cron_model');
-		$this->assets_path = 'assets/mint_import/';
+		$this->mint_path = 'assets/mint_import/';
 	}
 
 	function get_mint_import_zip()
 	{
 		$zip = new ZipArchive;
-		$res = $zip->open($this->assets_path . 'pbcore.zip');
+		$res = $zip->open($this->mint_path . 'pbcore.zip');
 		if ($res === TRUE)
 		{
 			$zip->extractTo('temp/');
 			$zip->close();
 			$this->load->helper('directory');
 			$map = directory_map('temp/', 2);
-			$xml_files = array();
-			foreach ($map as $directory)
+			foreach ($map as $index => $directory)
 			{
 				foreach ($directory as $file)
 				{
-					$xml_files[] = $file;
+					$this->parse_xml_file($index, $file);
 				}
 			}
-			debug($xml_files);
 		}
 		else
 		{
 			log('Something went wrong  while extracting zip file.');
 		}
+	}
+
+	function parse_xml_file($index, $file)
+	{
+		$file_content = file_get_contents('temp/' . $index . '/' . $file);
+		$xml_string = @simplexml_load_string($file_content);
+		unset($file_content);
+		$xmlArray = xmlObjToArr($xml_string);
+		debug($xmlArray);
 	}
 
 }

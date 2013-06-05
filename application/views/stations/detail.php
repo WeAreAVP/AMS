19391900839d2a4696fec0135a9397102c365a24
Nouman Tayyan <noumantayyab@gmail.com>
2013-06-05 17:24:36 +0500

    design changes on statiosn list

--- application/views/stations/detail.php
+++ application/views/stations/detail.php
@@ -7,7 +7,7 @@ if (count($station_detail) > 0)
 			<li><a href="<?php echo site_url('stations/index'); ?>">Stations</a> <span class="divider">/</span></li>
 			<li class="active"><?php echo $station_detail->station_name; ?></li>
 		</ul> 
-		
+
 		<h6 class="filter_title" style="font-weight: bold;">STATION INFORMATION</h6>
 		<br/>
 		<table class="table table-bordered">
@@ -50,8 +50,12 @@ if (count($station_detail) > 0)
 				</tr>
 			</tbody> 
 		</table>
+		<h6 class="filter_title" style="font-weight: bold;">STATION ADDRESS</h6>
+		<div class="span3">
+			<?php echo $station_detail->address_primary . ' ' . $station_detail->city . ', ' . $station_detail->state . ' ' . $station_detail->zip; ?>
+		</div>
 		<h6 class="filter_title" style="font-weight: bold;">STATION CONTACTS</h6>
-		
+
 		<br/>
 		<div class="row">
 			<?php
@@ -68,8 +72,13 @@ if (count($station_detail) > 0)
 						<h4><?php echo $value->first_name . ' ' . $value->last_name; ?></h4>
 			<!--                        <h4><?php echo ($value->role_id == 3) ? 'Station Admin' : 'Station User'; ?></h4>-->
 						<h4><?php echo $value->title; ?></h4>
-						<h4><?php if ($station_detail->type == 0) echo 'Radio'; else if ($station_detail->type == 1) echo 'TV';
-			else echo 'Joint'; ?></h4>
+						<h4><?php
+							if ($station_detail->type == 0)
+								echo 'Radio'; else if ($station_detail->type == 1)
+								echo 'TV';
+							else
+								echo 'Joint';
+							?></h4>
 						<p>
 							<?php echo $value->address; ?>
 							<br/>Phone: <?php echo $value->phone_no; ?>
@@ -101,10 +110,12 @@ if (count($station_detail) > 0)
 						<th>Shipped Via</th>
 						<th>Tracking Number</th>
 						<th># Boxes Shipped</th>
-						<?php if ($this->role_id != '20')
-						{ ?>
+						<?php
+						if ($this->role_id != '20')
+						{
+							?>
 							<th style="width: 35px;"></th>
-		<?php } ?>
+						<?php } ?>
 					</tr>
 				</thead>
 				<tbody>
@@ -119,17 +130,19 @@ if (count($station_detail) > 0)
 							<td><?php echo $value->ship_via; ?></td>
 							<td><?php echo nl2br($value->tracking_no); ?></td>
 							<td><?php echo $value->no_box_shipped; ?></td>
-			<?php if ($this->role_id != '20')
-			{ ?>
+							<?php
+							if ($this->role_id != '20')
+							{
+								?>
 								<td><a href="#trackingModel" data-toggle="modal" onclick="manageTracking('get', 'edit', '<?php echo $value->id; ?>');"><i class="icon-cog"></i></a>
 									<a  href="#deleteTracingModel" data-toggle="modal" onclick="deleteTracking('<?php echo $value->id; ?>', '<?php echo $station_detail->id; ?>');"><i class="icon-remove-sign"></i></a>
 								</td>
-			<?php } ?>
+							<?php } ?>
 						</tr>
 
 
 						</div>
-		<?php } ?>
+					<?php } ?>
 				</tbody>
 			</table>
 
@@ -141,11 +154,13 @@ if (count($station_detail) > 0)
 			?>
 			<div style="text-align: center;">No Tracking Information available for station <?php echo $station_detail->station_name; ?>.</div>
 		<?php } ?>
-		<?php if ($this->role_id != '20')
-		{ ?>
+		<?php
+		if ($this->role_id != '20')
+		{
+			?>
 			<div><a href="#trackingModel" class="btn" data-toggle="modal" onclick="manageTracking('get', 'add', '<?php echo $station_detail->id; ?>');">ADD SHIPMENT</a></div>
-	<?php } ?>
-	<?php $this->load->view('stations/_edit_station'); ?>
+		<?php } ?>
+		<?php $this->load->view('stations/_edit_station'); ?>
 
 
 
@@ -160,7 +175,7 @@ if (count($station_detail) > 0)
 		<h3>The requested sation not found</h3>
 		<a href="<?php echo site_url('stations/index'); ?>">Back to Stations</a>
 
-<?php } ?>
+	<?php } ?>
 
 
     <div class="modal hide" id="trackingModel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width: 680px;" >
@@ -188,41 +203,41 @@ if (count($station_detail) > 0)
         </div>
     </div>
     <script type="text/javascript">
-																									function manageTracking(type, action, id) {
-																										data = null;
-																										method = 'GET';
+							function manageTracking(type, action, id) {
+								data = null;
+								method = 'GET';
 
-																										if (action == 'add') {
-																											$('#TrackingLabel').html('Add Shipment');
-																										}
-																										else {
-																											$('#TrackingLabel').html('Edit Shipment');
-																										}
-																										if (type == 'post') {
-																											if (action == 'add')
-																												data = $('#tracking_new_form').serialize();
-																											else
-																												data = $('#tracking_edit_form').serialize();
-																											method = 'POST';
-																										}
-																										$.ajax({
-																											type: method,
-																											url: site_url + 'tracking/' + action + '/' + id,
-																											data: data,
-																											dataType: 'html',
-																											success: function(result) {
-																												if (result == 'done') {
-																													window.location.reload();
-																												}
-																												else {
-																													$('#manage_tracking').html(result);
+								if (action == 'add') {
+									$('#TrackingLabel').html('Add Shipment');
+								}
+								else {
+									$('#TrackingLabel').html('Edit Shipment');
+								}
+								if (type == 'post') {
+									if (action == 'add')
+										data = $('#tracking_new_form').serialize();
+									else
+										data = $('#tracking_edit_form').serialize();
+									method = 'POST';
+								}
+								$.ajax({
+									type: method,
+									url: site_url + 'tracking/' + action + '/' + id,
+									data: data,
+									dataType: 'html',
+									success: function(result) {
+										if (result == 'done') {
+											window.location.reload();
+										}
+										else {
+											$('#manage_tracking').html(result);
 
-																												}
+										}
 
-																											}
-																										});
-																									}
-																									function deleteTracking(trackingID, stationID) {
-																										$('#delete_tracking_btn').attr('href', site_url + '/tracking/delete/' + trackingID + '/' + stationID);
-																									}
+									}
+								});
+							}
+							function deleteTracking(trackingID, stationID) {
+								$('#delete_tracking_btn').attr('href', site_url + '/tracking/delete/' + trackingID + '/' + stationID);
+							}
     </script>

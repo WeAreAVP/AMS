a9c98def4fc60df262b885b46a14b8bd338b5b60
Nouman Tayyan <noumantayyab@gmail.com>
2013-06-10 12:46:14 +0500

    min changes

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -149,6 +149,7 @@ class MY_Controller extends CI_Controller
 			$this->total_unread = $this->msgs->get_unread_msgs_count($this->user_id);
 		}
 		$this->user_detail = $this->users->get_user_detail($this->user_id)->row();
+		debug($this->user_detail);
 		$this->can_compose_alert = FALSE;
 		if (in_array($this->role_id, array(1, 2, 5)))
 		{

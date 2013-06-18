0ec0e2b8dbacedf5877f8e1156ef81099c733f7d
Nouman Tayyan <noumantayyab@gmail.com>
2013-06-18 21:06:12 +0500

    some minor chages

--- application/controllers/mintimport.php
+++ application/controllers/mintimport.php
@@ -258,9 +258,9 @@ class Mintimport extends CI_Controller
 		$xml_string = @simplexml_load_string($file_content);
 		unset($file_content);
 		$xmlArray = xmlObjToArr($xml_string);
-
+		echo 'here';exit;
 		$asset_id = $this->assets_model->insert_assets(array("stations_id" => $station_id, "created" => date("Y-m-d H:i:s")));
-		debug($asset_id);
+		
 		myLog('Created Asset ID ' . $asset_id);
 		$this->import_asset_info($asset_id, $station_id, $xmlArray['children']);
 

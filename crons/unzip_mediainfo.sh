1fd2b9a03ba96d74295ff767a71c2fa5283b13ed
Nouman Tayyab <noumantayyab@gmail.com>
2013-08-12 11:32:26 +0500

    mediainfo file path changes

---
+++ crons/unzip_mediainfo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#####################################
+#                                   #
+#  Shell Script for unzipping       #
+#  crawford media info files.	    #
+#  This script will prevent         #
+#  from running method more         #
+#  than one time                    #
+#  Developer : Nouman Tayyab        #
+#  nouman@avpreserve.com            #
+#                                   #
+#####################################
+
+for zip in  /home/crawford/audio_metadata_bag/*.zip ; do
+     unzip -u  "$zip" -d /var/www/html/assets/mediainfo/
+done
+
+for zip in  /home/crawford/video_metadata_bag/*.zip ; do
+     unzip -u  "$zip" -d /var/www/html/assets/mediainfo/
+done

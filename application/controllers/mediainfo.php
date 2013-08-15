c667fe811861c8bd3eaed00eaf54ef6fd6747ae4
Nouman Tayyab <noumantayyab@gmail.com>
2013-08-15 13:40:08 +0500

    help text changes

--- application/controllers/mediainfo.php
+++ application/controllers/mediainfo.php
@@ -287,9 +287,9 @@ class Mediainfo extends CI_Controller
 		$data = file_get_contents($file_path);
 		
 		$x = @simplexml_load_string($data);
-		debug($x);
-		$data = xmlObjToArr($x);
 		
+		$data = xmlObjToArr($x);
+		debug($data);
 		$tracks_data = $data['children']['file'][0]['children']['track'];
 		$db_asset_id = NULL;
 		$db_instantiation_id = NULL;

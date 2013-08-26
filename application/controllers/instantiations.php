c70982b9d0e1511a6792f3df01dbcab17ef8d052
Nouman Tayyab <noumantayyab@gmail.com>
2013-08-26 13:03:03 +0500

    detail page changes

--- application/controllers/instantiations.php
+++ application/controllers/instantiations.php
@@ -184,7 +184,7 @@ class Instantiations extends MY_Controller
 				$data['instantiation_events'] = $this->instantiation->get_events_by_instantiation_id($instantiation_id);
 
 				$data['asset_details'] = $this->assets_model->get_asset_by_asset_id($detail->assets_id);
-				$search_results_data = $this->sphinx->instantiations_list(array('index' => 'assets_list'), 0, 100,TRUE);
+				$search_results_data = $this->sphinx->instantiations_list(array('index' => 'instantiations_list'), 0, 100,TRUE);
 				$data['nominations'] = $this->instantiation->get_nomination_status();
 
 				$data['media'] = $this->proxy_files($data['asset_guid']->guid_identifier);

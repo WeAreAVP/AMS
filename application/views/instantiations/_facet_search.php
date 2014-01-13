eda910ee7345b580007a23c454ea4bf8db955e8c
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-13 18:07:28 +0500

    hour tally fix

--- application/views/instantiations/_facet_search.php
+++ application/views/instantiations/_facet_search.php
@@ -885,7 +885,7 @@
 		$('body').css('overflow', 'hidden');
 		$('.span3').css('min-height', $(window).height() - 90);
 		$('.span3').css('max-height', $(window).height() - 90);
-		$('#data_container').width($(window).width() - 300);
+		$('#data_container').width($(window).width() - 279);
 	}
 	function load_facet_columns(index, isSearch) {
 		$.ajax({

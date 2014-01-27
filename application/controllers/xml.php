089fb4814266d081f5b5dfa0a241863ba28b01f8
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-27 20:40:26 +0500

    pbcore export changes

--- application/controllers/xml.php
+++ application/controllers/xml.php
@@ -44,7 +44,7 @@ class Xml extends CI_Controller
 		if (isset($guid) && $guid !== 0)
 		{
 
-			$result = $this->pbcore_model->get_one_by($this->pbcore_model->_identifiers_table, array('identifier' => "cpb-aacip/{$guid}"));
+			$result = $this->pbcore_model->get_one_by($this->pbcore_model->table_identifers, array('identifier' => "cpb-aacip/{$guid}"));
 			if ($result)
 			{
 				debug($result);

23b63bea5dff293dee6f8dd6bc4feeed3959990d
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-27 17:50:12 +0500

    pbcore export changes

--- application/models/pbcore_model.php
+++ application/models/pbcore_model.php
@@ -90,11 +90,11 @@ class Pbcore_Model extends MY_Model
 
 	function get_asset_subject($asset_id)
 	{
-		return $this->db->select("{$this->table_subjects}.*,{$this->table_subject_types}.subject_type")
+		$this->db->select("{$this->table_subjects}.*,{$this->table_subject_types}.subject_type")
 		->join($this->table_subject_types, "{$this->table_subject_types}.id = {$this->table_subjects}.subjects_types_id", 'LEFT')
 		->where("{$this->table_subjects}.assets_id", $asset_id)
-		->get($this->table_subjects)->result();
-		
+		->get($this->table_subjects);
+		echo $this->db->last_query();exit;
 	}
 
 	function export_assets($real_time = FALSE)

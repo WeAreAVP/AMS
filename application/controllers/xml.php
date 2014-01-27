a60412bcb69ebdd28aba54872eefc3aed61dc6d3
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-27 12:34:06 +0500

    pbcore export changes

--- application/controllers/xml.php
+++ application/controllers/xml.php
@@ -9,12 +9,31 @@ class Xml extends CI_Controller
 		$this->layout = 'default.php';
 		$this->load->library('pbcore');
 		$this->load->model('pbcore_model');
+		$this->load->model('export_csv_job_model', 'csv_job');
 	}
 
-	function pbcore()
+	function export_pbcore()
 	{
-		$guid = $this->uri->segment(3, 0);
-		if ($guid !== 0)
+		$job = $this->csv_job->get_export_jobs('pbcore');
+		if (count($job) > 0)
+		{
+			myLog('CSV Job Started.');
+			for ($i = 0; $i < $job->query_loop; $i ++ )
+			{
+				myLog('Query Loop ' . $i);
+				$query = $job->export_query;
+				$query.=' LIMIT ' . ($i * 100000) . ', 100000';
+
+				$records = $this->csv_job->get_csv_records($query);
+				debug($records);exit;
+			}
+		}
+	}
+
+	function pbcore($guid)
+	{
+
+		if (isset($guid) && $guid !== 0)
 		{
 
 			$result = $this->pbcore_model->get_one_by($this->pbcore_model->_identifiers_table, array('identifier' => "cpb-aacip/{$guid}"));

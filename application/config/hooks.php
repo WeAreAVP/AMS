2cabb88611e38696c4583b6fe444a1d5841ffc31
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-29 15:27:34 +0500

    xml exporting work

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,34 +1,24 @@
-<?php
-
-if ( ! defined('BASEPATH'))
-	exit('No direct script access allowed');
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
-  | -------------------------------------------------------------------------
-  | Hooks
-  | -------------------------------------------------------------------------
-  | This file lets you define "hooks" to extend CI without hacking the core
-  | files.  Please see the user guide for info:
-  |
-  |	http://codeigniter.com/user_guide/general/hooks.html
-  |
- */
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend CI without hacking the core
+| files.  Please see the user guide for info:
+|
+|	http://codeigniter.com/user_guide/general/hooks.html
+|
+*/
 $hook['pre_controller'][] = array(
-	'class' => 'CI_Autoloader',
-	'function' => 'register',
-	'filename' => 'CI_Autoloader.php',
-	'filepath' => 'hooks',
-	'params' => array(APPPATH . 'base/')
+		'class' => 'Pear_hook',
+		'function' => 'index',
+		'filename' => 'pear_hook.php',
+		'filepath' => 'hooks'
 );
-//$hook['pre_controller'][] = array(
-//	'class' => 'Pear_hook',
-//	'function' => 'index',
-//	'filename' => 'pear_hook.php',
-//	'filepath' => 'hooks'
-//);
-$hook['display_override'][] = array('class' => 'Yield',
-	'function' => 'doYield',
-	'filename' => 'Yield.php',
-	'filepath' => 'hooks'
+$hook['display_override'][] = array('class'    => 'Yield',
+		'function' => 'doYield',
+		'filename' => 'Yield.php',
+		'filepath' => 'hooks'
 );
 
 

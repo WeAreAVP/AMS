69d7d40ce9ca5b58aa6f641307aefc26204f5258
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-29 20:07:32 +0500

    xml work on instantiations

--- application/core/MY_Instantiation_Model.php
+++ application/core/MY_Instantiation_Model.php
@@ -64,11 +64,11 @@ class MY_Instantiation_Model extends MY_Essencetrack_Model
 	 */
 	function get_instantiation_nomination($instantiation_id)
 	{
-		$this->db->select("{$this->table_nominations}.*,{$this->table_nomination_status}.*")
-		->join($this->table_nomination_status, "{$this->able_nomination_status}.id = {$this->table_nominations}.nomination_status_id", 'LEFT')
+		return $this->db->select("{$this->table_nominations}.*,{$this->table_nomination_status}.*")
+		->join($this->table_nomination_status, "{$this->table_nomination_status}.id = {$this->table_nominations}.nomination_status_id", 'LEFT')
 		->where("{$this->table_nominations}.instantiations_id", $instantiation_id)
-		->get($this->table_nominations);
-		echo $this->db->last_query();exit;
+		->get($this->table_nominations)->result();
+		
 	}
 
 }

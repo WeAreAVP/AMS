59c24aa5d7c894087897e119a8de1669bcda1e20
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-30 14:44:13 +0500

    export to xml

--- application/libraries/export_pbcore_premis.php
+++ application/libraries/export_pbcore_premis.php
@@ -32,15 +32,14 @@ class Export_pbcore_premis
 			}
 			else
 			{
-				$this->xml = new SimpleXMLElement('<premis></premis>',0,FALSE,'premis',TRUE);
-				$this->xml->registerXPathNamespace('premis','info:lc/xmlns/premis-v2');
+				$this->xml = new SimpleXMLElement('<premis:premis></premis:premis>');
+//				$this->xml->registerXPathNamespace('premis','info:lc/xmlns/premis-v2');
 				$attributes = array(
-					'xmlns:premis' => "info:lc/xmlns/premis-v2",
+//					'xmlns:premis' => "info:lc/xmlns/premis-v2",
 					'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
 					'xsi:schemaLocation' => "info:lc/xmlns/premis-v2 http://www.loc.gov/standards/premis/v2/premis.xsd",
 					'version' => "2.2");
 				$this->_add_attribute($this->xml, $attributes);
-				
 
 			}
 //			$this->_fetch_asset();

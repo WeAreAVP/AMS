1ef5b814f4aa0cafcd6f399b1612ca823806c3a8
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-30 18:08:39 +0500

    xml changes

--- application/controllers/xml.php
+++ application/controllers/xml.php
@@ -18,6 +18,7 @@ class Xml extends CI_Controller
 	function export_pbcore()
 	{
 		@ini_set("max_execution_time", 999999999999); # unlimited
+		@ini_set("memory_limit", "1000M"); # 1GB
 		$export_job = $this->csv_job->get_export_jobs('pbcore');
 		if (count($export_job) > 0)
 		{

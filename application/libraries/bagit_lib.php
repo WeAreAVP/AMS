18851c7da530d3a10ffef9abc0a2d83eca2a7deb
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-30 16:11:17 +0500

    bagit lib

--- application/libraries/bagit_lib.php
+++ application/libraries/bagit_lib.php
@@ -3,13 +3,16 @@
 if ( ! defined('BASEPATH'))
 	exit('No direct script access allowed');
 
-class Bagit_lib
+
+
+class Bagit_lib 
 {
 
 	function __construct()
 	{
-		set_include_path(dirname(dirname(__FILE__)) . '/third_party/BagIt');
-
+		
+		
+		ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . dirname(dirname(__FILE__)) . '/third_party/BagIt');
 		require_once('bagit.php');
 	}
 

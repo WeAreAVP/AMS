0f750c4aa0b71266ac579b2c24bac6e9ccf167d8
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-30 17:19:16 +0500

    xml changes

--- application/controllers/xml.php
+++ application/controllers/xml.php
@@ -29,7 +29,7 @@ class Xml extends CI_Controller
 				$query = $export_job->export_query;
 				$query.=' LIMIT ' . ($i * 100000) . ', 100000';
 				$records = $this->csv_job->get_csv_records($query);
-
+				$count=0;
 				foreach ($records as $value)
 				{
 					$this->export_pbcore_premis->asset_id = $value->id;
@@ -37,9 +37,13 @@ class Xml extends CI_Controller
 					$guid = $this->pbcore_model->get_one_by($this->pbcore_model->table_identifers, array('assets_id' => $value->id, 'identifier_source' => 'http://americanarchiveinventory.org'));
 					$file_name = str_replace('/', '-', $guid->identifier);
 					$path = "./uploads/{$file_name}.xml";
+					header("Content-Type: application/xml; charset=utf-8");
 					$this->export_pbcore_premis->xml->saveXML($path);
 
 					$bagit_lib->addFile($path, "{$file_name}/{$file_name}_pbcore.xml");
+					if($count==300)
+						break;
+					$count++;
 				}
 			}
 			$bagit_lib->update();

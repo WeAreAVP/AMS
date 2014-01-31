59ddc822e45ec0fad0fb41c4a9257c50523858b7
Nouman Tayyab <noumantayyab@gmail.com>
2014-01-31 13:25:57 +0500

    export xml changes

--- application/views/email/export_pbcore.php
+++ application/views/email/export_pbcore.php
@@ -3,7 +3,7 @@
 </div>
 <br/>
 <div>
-	Your AMS XML export is ready. You can download the file from <a href="<?php site_url('xml/download/' . base64_encode($export_id)); ?>">here</a>
+	Your AMS XML export is ready. You can download the file from <a href="<?php echo site_url('xml/download/' . base64_encode($export_id)); ?>">here</a>
 </div>
 <br/>
 <br/>

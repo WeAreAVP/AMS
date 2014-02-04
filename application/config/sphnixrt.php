9990b596003769d46e2803ec53fda8db6ee797a5
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-04 17:11:16 +0500

    config changes

--- application/config/sphnixrt.php
+++ application/config/sphnixrt.php
@@ -6,7 +6,4 @@
 $config['hostname'] = '127.0.0.1';
 
 // port, you set this in the sphinx config
-if (ENVIRONMENT == 'development')
-	$config['port'] = '9306';
-else
-	$config['port'] = '3306'; 
+$config['port'] = '9306';

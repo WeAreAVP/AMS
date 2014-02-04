b92d7e082de6d3c43805ab0f0e53875d0ccb2e62
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-04 19:29:43 +0500

    xml changes

--- application/controllers/xml.php
+++ application/controllers/xml.php
@@ -120,9 +120,14 @@ class Xml extends CI_Controller
 			{
 				$this->export_pbcore_premis->asset_id = $result->assets_id;
 				$this->export_pbcore_premis->is_pbcore_export = FALSE;
-				$this->export_pbcore_premis->make_xml();
-
-				echo $this->export_pbcore_premis->xml->asXML();
+				$result = $this->export_pbcore_premis->make_xml();
+				if ($result)
+					echo $this->export_pbcore_premis->xml->asXML();
+				else
+				{
+					$response = $this->export_pbcore_premis->xml_error('No PREMIS info available.');
+					echo $response->asXML();
+				}
 			}
 			else
 			{

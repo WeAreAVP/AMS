d69e0658fa0a3fd218f9a91185ac53327f0aa593
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-05 15:20:43 +0500

    premis xml changes

--- application/libraries/export_pbcore_premis.php
+++ application/libraries/export_pbcore_premis.php
@@ -777,7 +777,7 @@ class Export_pbcore_premis
 	private function _add_child($object, $tag_name, $value = NULL)
 	{
 		debug($tag_name, FALSE);
-		$object = $object->addChild($tag_name, str_replace("…", '&#x85;', htmlentities($value)));
+		$object = $object->addChild($tag_name, str_replace("…", '&#133;', htmlentities($value)));
 		return $object;
 	}
 

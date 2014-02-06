f620e9746b723a8afc3662923c669fcf7d0f454e
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-06 20:05:33 +0500

    changes in xml

--- application/core/MY_Asset_Model.php
+++ application/core/MY_Asset_Model.php
@@ -209,7 +209,7 @@ class MY_Asset_Model extends MY_Instantiation_Model
 		$this->db->limit(100);
 		$this->db->get($this->_assets_table);
 		echo $this->db->last_query();
-		return $this->db->get($this->_assets_table)->result();
+//		return $this->db->get($this->_assets_table)->result();
 	}
 
 }

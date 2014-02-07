192702b1b832546b11bffdfaa5c4ee336a6330f2
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-07 15:29:20 +0500

    code cleanup

--- application/core/MY_Essencetrack_Model.php
+++ application/core/MY_Essencetrack_Model.php
@@ -39,31 +39,31 @@ class MY_Essencetrack_Model extends MY_Model
 	function update_essence_track($essence_id, $data)
 	{
 		$this->db->where('id', $essence_id);
-		return $this->db->update($this->_table_essence_tracks, $data);
+		return $this->db->update($this->table_essence_tracks, $data);
 	}
 
 	function get_essence_tracks_by_instantiations_id($ins_id)
 	{
-		$this->db->select("$this->_table_essence_tracks.id");
-		$this->db->select("$this->_table_essence_tracks.standard,$this->_table_essence_tracks.frame_rate");
-		$this->db->select("$this->_table_essence_tracks.playback_speed,$this->_table_essence_tracks.sampling_rate");
-		$this->db->select("$this->_table_essence_tracks.bit_depth,$this->_table_essence_tracks.aspect_ratio");
-		$this->db->select("$this->_table_essence_tracks.time_start,$this->_table_essence_tracks.duration");
-		$this->db->select("$this->_table_essence_tracks.language,$this->_table_essence_tracks.data_rate");
-		$this->db->select("$this->_table_data_rate_units.unit_of_measure");
-		$this->db->select("$this->_table_essence_track_types.essence_track_type");
-		$this->db->select("$this->_table_essence_track_frame_sizes.width,$this->_table_essence_track_frame_sizes.height");
-		$this->db->select("$this->_table_essence_track_encodings.encoding,$this->_table_essence_track_encodings.encoding_source,$this->_table_essence_track_encodings.encoding_ref");
-		$this->db->select("$this->_table_essence_track_identifiers.essence_track_identifiers,$this->_table_essence_track_identifiers.essence_track_identifier_source");
+		$this->db->select("$this->table_essence_tracks.id");
+		$this->db->select("$this->table_essence_tracks.standard,$this->table_essence_tracks.frame_rate");
+		$this->db->select("$this->table_essence_tracks.playback_speed,$this->table_essence_tracks.sampling_rate");
+		$this->db->select("$this->table_essence_tracks.bit_depth,$this->table_essence_tracks.aspect_ratio");
+		$this->db->select("$this->table_essence_tracks.time_start,$this->table_essence_tracks.duration");
+		$this->db->select("$this->table_essence_tracks.language,$this->table_essence_tracks.data_rate");
+		$this->db->select("$this->table_data_rate_units.unit_of_measure");
+		$this->db->select("$this->table_essence_track_types.essence_track_type");
+		$this->db->select("$this->table_essence_track_frame_sizes.width,$this->table_essence_track_frame_sizes.height");
+		$this->db->select("$this->table_essence_track_encodings.encoding,$this->table_essence_track_encodings.encoding_source,$this->table_essence_track_encodings.encoding_ref");
+		$this->db->select("$this->table_essence_track_identifiers.essence_track_identifiers,$this->table_essence_track_identifiers.essence_track_identifier_source");
 
-		$this->db->join($this->_table_data_rate_units, "$this->_table_data_rate_units.id=$this->_table_essence_tracks.data_rate_units_id", 'LEFT');
-		$this->db->join($this->_table_essence_track_types, "$this->_table_essence_track_types.id=$this->_table_essence_tracks.essence_track_types_id", 'LEFT');
-		$this->db->join($this->_table_essence_track_frame_sizes, "$this->_table_essence_track_frame_sizes.id=$this->_table_essence_tracks.essence_track_frame_sizes_id", 'LEFT');
+		$this->db->join($this->table_data_rate_units, "$this->table_data_rate_units.id=$this->table_essence_tracks.data_rate_units_id", 'LEFT');
+		$this->db->join($this->table_essence_track_types, "$this->table_essence_track_types.id=$this->table_essence_tracks.essence_track_types_id", 'LEFT');
+		$this->db->join($this->table_essence_track_frame_sizes, "$this->table_essence_track_frame_sizes.id=$this->table_essence_tracks.essence_track_frame_sizes_id", 'LEFT');
 
-		$this->db->join($this->_table_essence_track_encodings, "$this->_table_essence_track_encodings.essence_tracks_id=$this->_table_essence_tracks.id", 'LEFT');
-		$this->db->join($this->_table_essence_track_identifiers, "$this->_table_essence_track_identifiers.essence_tracks_id=$this->_table_essence_tracks.id", 'LEFT');
-		$this->db->where("$this->_table_essence_tracks.instantiations_id", $ins_id);
-		return $this->db->get($this->_table_essence_tracks)->result();
+		$this->db->join($this->table_essence_track_encodings, "$this->table_essence_track_encodings.essence_tracks_id=$this->table_essence_tracks.id", 'LEFT');
+		$this->db->join($this->table_essence_track_identifiers, "$this->table_essence_track_identifiers.essence_tracks_id=$this->table_essence_tracks.id", 'LEFT');
+		$this->db->where("$this->table_essence_tracks.instantiations_id", $ins_id);
+		return $this->db->get($this->table_essence_tracks)->result();
 	}
 
 }

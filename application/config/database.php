c7085058543058d50ed8def7b73da6b98fec2ccc
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-11 14:51:17 +0500

    production changes

--- application/config/database.php
+++ application/config/database.php
@@ -70,9 +70,9 @@ else if (ENVIRONMENT == 'qatesting')
 }
 else if (ENVIRONMENT == 'production')
 {
-	$db['default']['hostname'] = 'amsdb.cnltxk49gtb0.us-east-1.rds.amazonaws.com';
+	$db['default']['hostname'] = 'localhost';
 	$db['default']['username'] = 'root';
-	$db['default']['password'] = 'GCAVPSQL098';
+	$db['default']['password'] = 'Av9S@0v4123';
 	$db['default']['database'] = 'ams_production';
 	$db['default']['dbdriver'] = 'mysql';
 	$db['default']['db_debug'] = FALSE;

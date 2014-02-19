c1b60277986706176a40be932e4936d48905af2d
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-19 16:22:43 +0500

    facet changes

--- application/models/sphinx_model.php
+++ application/models/sphinx_model.php
@@ -465,7 +465,7 @@ class Sphinx_Model extends CI_Model
 		{
 
 			$physical_format = str_replace('|||', '" | "', trim($this->session->userdata['physical_format']));
-			$where .=" @s_format_name \"$physical_format\"";
+			$where .=" @s_physical_format_name \"$physical_format\"";
 		}
 		else if ($type == 'physical'  && $sphnix_index !='assets_list')
 		{
@@ -476,7 +476,7 @@ class Sphinx_Model extends CI_Model
 		if (isset($this->session->userdata['digital_format']) && $this->session->userdata['digital_format'] != '')
 		{
 			$digital_format = str_replace('|||', '" | "', trim($this->session->userdata['digital_format']));
-			$where .=" @s_format_name \"$digital_format\"";
+			$where .=" @s_digital_format_name \"$digital_format\"";
 		}
 		else if ($type == 'digital'  && $sphnix_index !='assets_list')
 		{

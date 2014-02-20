acefec8c747ba5dd30191c54deef7fe378db281f
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-20 12:56:49 +0500

    changes

--- application/controllers/records.php
+++ application/controllers/records.php
@@ -79,6 +79,7 @@ class Records extends MY_Controller
 				$search['migration_failed'] = $this->input->post('migration_failed');
 			}
 			$this->set_facet_search($search);
+			debug($this->session->userdata,FALSE);
 		}
 		$this->session->set_userdata('page_link', 'records/index');
 		$data['facet_search_url'] = site_url('records/index');

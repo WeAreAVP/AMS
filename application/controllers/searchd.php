ee5792fd4f7693f6f46dff242c2e8461fe85503e
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-20 19:05:49 +0500

    changes

--- application/controllers/searchd.php
+++ application/controllers/searchd.php
@@ -48,6 +48,15 @@ class Searchd extends CI_Controller
 	{
 		error_reporting(E_ALL);
 		ini_set('display_errors', 1);
+		$sample_array['title']= 'Nouman Tayyab';
+		$sample_array['format_name']= '(Betacam)';
+		$sample_array['media_type']= '(Moving Image)';
+		$this->sphnixrt->insert('test', $sample_array, 1);
+		$sample_array['title']= 'Nouman Tayyab';
+		$sample_array['format_name']= '(Betacam,Betacam SP)';
+		$sample_array['media_type']= '(Moving Image)';
+		$this->sphnixrt->insert('test', $sample_array, 2);
+		exit;
 		$sphinx['start']=0;
 		$sphinx['limit']=1000;
 		$sphinx['column_name']='id';

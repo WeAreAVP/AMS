2c585ad48ae2e6e0083fa2b082d516ce5adbf10a
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-21 21:00:51 +0500

    changes

--- application/controllers/crons.php
+++ application/controllers/crons.php
@@ -166,7 +166,7 @@ class Crons extends CI_Controller
 					foreach ($result['records'] as $_row)
 					{
 
-						$exploded_facet = explode('|', $_row[$facet]);
+						$exploded_facet = explode('(;)', $_row[$facet]);
 						foreach ($exploded_facet as $single_value)
 						{
 							if (isset($make_facet[trim($single_value)]))

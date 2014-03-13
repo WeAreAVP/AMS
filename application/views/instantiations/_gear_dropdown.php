b3adbc3bdc1bc9a9520d6af07a115ec30334044f
Nouman Tayyab <noumantayyab@gmail.com>
2014-03-13 16:19:59 +0500

    changes to fix refine project issue

--- application/views/instantiations/_gear_dropdown.php
+++ application/views/instantiations/_gear_dropdown.php
@@ -97,8 +97,76 @@ if ($table_type == 'assets' && $current_tab == 'simple')
 					<li><a id="cancel_refine_data"   href="javascript://" role="button">Updating Records</a></li>
 					<?php
 				}
-			}
-			?>
+				?>
+
+				<div class="modal hide" id="refine_confirm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h3>AMS Refine</h3>
+
+					</div>
+					<div class="modal-body" id="refine_body">
+						Are you sure you want to refine data.
+					</div>
+					<div class="modal-footer" id="refine_footer">
+						<button class="btn" data-dismiss="modal" aria-hidden="true">No</button>
+						<button class="btn btn-primary" onclick="doRefine();">Yes</button>
+					</div>
+					<div class="modal-footer" id="already_refine_footer">
+						<button class="btn btn-inverse" data-dismiss="modal" aria-hidden="true" onclick="window.location.reload();">OK</button>
+
+					</div>
+				</div>
+
+
+				<div class="modal hide" id="refine_cancel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h3>AMS Refine</h3>
+
+					</div>
+					<div class="modal-body">
+						Are you sure you want to cancel refining.It will remove all your changes.
+					</div>
+					<div class="modal-footer">
+						<button class="btn" data-dismiss="modal" aria-hidden="true">No</button>
+						<a href="<?php echo site_url('refine/remove/' . $is_refine->project_id); ?>" class="btn btn-primary">Yes</a>
+					</div>
+
+				</div>
+
+
+				<script type="text/javascript">
+					var type_record;
+					function refineConfirm(msg, type, record_type) {
+						type_record = record_type;
+						$('#refine_body').html(msg);
+						if (type == 0) {
+							$('#already_refine_footer').hide();
+							$('#refine_footer').show();
+						}
+						else {
+							$('#refine_footer').hide();
+							$('#already_refine_footer').show();
+						}
+
+					}
+					function doRefine() {
+						$('#refine_body').html('<img src="/images/ajax-loader.gif" style="margin-right: 15px;" />');
+						$('#refine_footer').hide();
+
+						$.ajax({
+							type: 'POST',
+							url: site_url + 'refine/export/' + type_record,
+							dataType: 'json',
+							success: function(result) {
+								$('#refine_body').html(result.msg);
+								$('#already_refine_footer').show();
+							}
+						});
+					}
+				</script>
+			<?php } ?>
 			<li><a href="<?php echo site_url('asset/add'); ?>" >Add Asset</a></li>
 			<?php
 			if ($this->role_id == 1 || $this->role_id == 2)
@@ -112,7 +180,7 @@ if ($table_type == 'assets' && $current_tab == 'simple')
 			{
 				?>
 				<li><a href="<?php echo site_url('autocomplete/mint_login'); ?>">Import Collection</a></li>
-	<?php } ?>
+			<?php } ?>
 		</ul>
 	</div>
 	<?php
@@ -144,7 +212,7 @@ if ($table_type == 'assets' && $current_tab == 'simple')
 				<a href="<?php echo site_url('autocomplete/mint_login/' . $station_records[0]->id); ?>"  id="mint_login_url" class="btn btn-primary">Import Collection</a>
 			</div>
 		</div>
-<?php } ?>
+	<?php } ?>
 </div>
 <script type="text/javascript">
 	var hiden_column =<?php echo json_encode($hidden_fields); ?>;

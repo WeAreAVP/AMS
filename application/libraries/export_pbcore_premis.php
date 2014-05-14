b1bf50f7c39fe8fc7e91c148c26d46d8e1855ba8
Nouman Tayyab <noumantayyab@gmail.com>
2014-05-14 14:56:43 +0500

    PREMIS xml changes

--- application/libraries/export_pbcore_premis.php
+++ application/libraries/export_pbcore_premis.php
@@ -106,13 +106,15 @@ class Export_pbcore_premis
 			foreach ($records as $asset)
 			{
 				$this->asset_id = $asset->id;
-				$xml_object = $this->_add_child($xml_object, 'premis');
+				$xml_object = $this->_add_child($this->xml, 'premis');
 				$attributes = array(
-					'xmlns' => "info:lc/xmlns/premis-v2",
-					'xsi:xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
-					'xsi:xsi:schemaLocation' => "info:lc/xmlns/premis-v2 http://www.loc.gov/standards/premis/v2/premis.xsd",
-					'version' => "2.0");
-				$this->_add_attribute($this->xml, $attributes);
+//					'xmlns' => "info:lc/xmlns/premis-v2",
+					'xsi:xmlns' => "http://www.w3.org/2001/XMLSchema-instance",
+					'xsi:schemaLocation' => "info:lc/xmlns/premis-v2 http://www.loc.gov/standards/premis/v2/premis.xsd",
+				);
+				$this->_add_attribute($xml_object, array('xmlns' => "info:lc/xmlns/premis-v2"));
+				$this->_add_attribute($xml_object, $attributes, 'xsi');
+				$this->_add_attribute($xml_object, array('version' => "2.0"));
 
 				$result = $this->_fetch_events($this->xml, TRUE);
 			}
@@ -167,7 +169,7 @@ class Export_pbcore_premis
 		$events = $pbcore_model->get_instantiation_events($this->asset_id);
 		$guid = $pbcore_model->get_one_by($this->CI->pbcore_model->table_identifers, array('assets_id' => $this->asset_id, 'identifier_source' => 'http://americanarchiveinventory.org'));
 		$object_guid = $this->_add_child($xml_object, 'object');
-		$this->_add_attribute($object_guid, array('xsi:type' => 'representation'),'xsi');
+		$this->_add_attribute($object_guid, array('xsi:xsi:type' => 'representation'));
 		$object_identifier = $this->_add_child($object_guid, 'objectIdentifier');
 		$this->_add_child($object_identifier, 'objectIdentifierType', 'American Archive GUID');
 		$this->_add_child($object_identifier, 'objectIdentifierValue', $guid->identifier);

05599926a738fd043baaa12f03405b5852dceed7
Nouman Tayyab <noumantayyab@gmail.com>
2014-06-06 13:55:16 +0500

    debug the refine issue

--- application/controllers/refine.php
+++ application/controllers/refine.php
@@ -112,12 +112,9 @@ class Refine extends MY_Controller
 //			$folder_path = $this->config->item('path') . 'assets/google_refine/' . date('Y') . '/' . date('M') . '/imports/';
                         $folder_path = 'assets/google_refine/' . date('Y') . '/' . date('M') . '/imports/';
 			$file_path = $folder_path . $filename;
-			if ( ! is_dir($folder_path)){
-                            
-//				mkdir($folder_path, 0777, TRUE);
-                                exec("mkdir -p testing/nouman",$output);
-                                }
-                        debug($output);
+			if ( ! is_dir($folder_path))
+				mkdir($folder_path, 0777, TRUE);
+                        debug('here');
 			$fp = fopen($file_path, 'a');
 			file_put_contents($path, $response);
 			$this->googlerefine->delete_project($project_id);
